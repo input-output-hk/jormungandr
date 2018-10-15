7854100e36ddd9d5940c7ddd472b2640b7d78ea2
Vincent Hanquez <vincent.hanquez@iohk.io>
2018-10-15 12:38:10 +0100

    update plan

--- src/main.rs
+++ src/main.rs
@@ -16,6 +16,9 @@ extern crate jormungandr;
 use std::path::{PathBuf};
 
 use jormungandr::gclock;
+use jormungandr::tpool::TPool;
+
+use cardano::tx::{TxId, TxAux};
 
 pub struct State {
 }
@@ -27,17 +30,44 @@ impl State {
 }
 
 fn main() {
-    /// load parameters & config
+    // # load parameters & config
+    //
+    // parse the command line arguments, the config files supplied
+    // and setup the initial values
     let mut state = State::new();
 
-    /// bootstrap phase (peer discovery, download all the existing blocks)
+    // # Bootstrap phase
+    //
+    // done at every startup: we need to bootstrap from whatever local state (including nothing)
+    // to the latest network state (or close to latest). until this happen, we don't participate in the network
+    // (no block creation) and our network connection(s) is only use to download data.
+    //
+    // Various aspects to do, similar to hermes:
+    // * download all the existing blocks
+    // * verify all the downloaded blocks
+    // * network / peer discoveries (?)
+    // * gclock sync ?
 
-    /// connect to peers
-    /// core logic
+    // ** TODO **
 
+    // # Active phase
+    //
+    // now that we have caught up (or almost caught up) we download blocks from neighbor nodes,
+    // listen to announcements and actively listen to synchronous queries
+    //
+    // There's two simultaenous roles to this:
+    // * Leader: decided after global or local evaluation. Need to create and propagate a block
+    // * Non-Leader: always. receive (pushed-) blocks from other peers, investigate the correct blockchain updates
+    //
+    // Also receive synchronous connection queries:
+    // * new nodes subscribing to updates (blocks, transactions)
+    // * client GetBlocks/Headers ...
+
+    let mut tpool : TPool<TxId, TxAux> = TPool::new();
+
+    // ** TODO **
     // setup_network
-    // events:
-    //  new connection:
+    //  connection-events:
     //    poll:
     //      recv_transaction:
     //         check_transaction_valid
@@ -50,15 +80,16 @@ fn main() {
     //      get block(s):
     //         try to answer
     //
-    // periodically protocol speed:
+    // periodically (protocol speed):
     //   check elected
     //   if elected
     //     take set of transactions from pool
     //     create a block
     //     send it async to peers
     //
-    // periodically cleanup:
+    // periodically cleanup (custom):
     //   storage cleanup/packing
+    //   tpool.gc()
 
     println!("Hello, world!");
 }
