ad5d93baea60fc4a121dfbc035c36f9eede6b41d
Nicolas Di Prima <nicolas.diprima@iohk.io>
2018-10-31 11:03:19 +0000

    add BlockHeader's in the blockcfg list of reuse

--- src/blockcfg.rs
+++ src/blockcfg.rs
@@ -5,3 +5,4 @@ pub type TransactionId = cardano::tx::TxId;
 pub type Transaction = cardano::tx::TxAux;
 pub type BlockHash = cardano::block::HeaderHash;
 pub type Block = cardano::block::Block;
+pub type Header = cardano::block::BlockHeader;
