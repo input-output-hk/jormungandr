9694b45c56021f3d747026d229207d7bd4db717b
Nicolas Di Prima <nicolas.diprima@iohk.io>
2018-11-01 15:03:30 +0000

    separate BlockMsg types

--- src/blockchain/process.rs
+++ src/blockchain/process.rs
@@ -3,5 +3,12 @@ use super::super::intercom::BlockMsg;
 use super::chain;
 
 pub fn process(blockchain: &chain::BlockchainR, bquery: BlockMsg) {
-    debug!("block msg received: {:#?}", bquery);
+    match bquery {
+        BlockMsg::NetworkBlock(block) => {
+           debug!("received block from the network: {:#?}", block);
+        }
+        BlockMsg::LeadershipBlock(block) => {
+           debug!("received block from the leadership: {:#?}", block);
+        }
+    }
 }
