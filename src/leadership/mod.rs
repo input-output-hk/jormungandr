b3e2e32e14e95f90aaec5f3ae392872153ec55f9
Vincent Hanquez <vincent.hanquez@iohk.io>
2018-11-01 07:08:43 +0000

    move leadership part to own module

---
+++ src/leadership/mod.rs
@@ -0,0 +1,3 @@
+pub mod process;
+
+pub use self::process::leadership_task;
--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ extern crate serde_derive;
 extern crate serde_yaml;
 #[macro_use]
 extern crate log;
+extern crate rand;
 extern crate env_logger;
 #[macro_use]
 extern crate structopt;
@@ -20,6 +21,7 @@ pub mod clock;
 pub mod blockchain;
 pub mod transaction;
 pub mod state;
+pub mod leadership;
 pub mod network;
 pub mod utils;
 pub mod intercom;
@@ -63,42 +65,6 @@ fn client_task(_blockchain: BlockchainR, r: Receiver<ClientMsg>) {
     loop {
         let query = r.recv().unwrap();
         println!("client query received: {:?}", query)
-    }
-}
-
-fn leadership_task(tpool: TPoolR, blockchain: BlockchainR, clock: clock::Clock) {
-    loop {
-        let d = clock.wait_next_slot();
-        let (epoch, idx, next_time) = clock.current_slot().unwrap();
-        println!("slept for {:?} epoch {} slot {} next_slot {:?}", d, epoch.0, idx, next_time);
-        let len = {
-            let t = tpool.read().unwrap();
-            (*t).content.len()
-        };
-
-        // TODO: check if this node is "elected" (by design or by stake) for this slot
-        let elected = true;
-
-        if elected {
-            // create a new block to broadcast:
-            // * get the transactions to put in the transactions
-            // * mint the block
-            // * sign it
-            let latest_tip = {
-                let b = blockchain.read().unwrap();
-                b.get_tip()
-            };
-
-            println!("leadership create tpool={} transactions tip={}", len, latest_tip);
-
-            // SIMULATING busy task: take between 1 to 21 seconds.
-            {
-                let v = 1u64 + (rand::random::<u64>() % 20);
-                thread::sleep(time::Duration::from_secs(v))
-            };
-            // TODO: send it to block thread for appending/broadcasting
-        }
-
     }
 }
 
