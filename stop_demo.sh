42a154843dd032aa1232e0668d409aeedb45d0a6
amias <me@amias.net>
2018-11-23 18:16:35 +0000

    updated to match node numbering

--- stop_demo.sh
+++ stop_demo.sh
@@ -4,7 +4,8 @@ node_count=${1:-'0'}
 
 counter=0
 while [ $counter -lt $node_count ]; do
-	stub='node_'$counter
-	screen -X -S $stub quit	
 	let counter=$counter+1	
+	
+	stub='node_'$counter
+	screen -X -S $stub quit			
 done
