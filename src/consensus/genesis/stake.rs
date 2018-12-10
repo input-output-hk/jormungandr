149745d226b07d8a1d38d7d747006766ef40e18e
Vincent Hanquez <vincent.hanquez@iohk.io>
2018-12-10 19:28:05 +0000

    add stake unit/total addition helpers and stake distribution manipulation helpers function

--- src/consensus/genesis/stake.rs
+++ src/consensus/genesis/stake.rs
@@ -1,4 +1,6 @@
+use std::ops::{Add,Sub};
 use std::collections::BTreeMap;
+use std::collections::btree_map::Entry;
 
 // TODO: PublicKey
 use super::super::super::secure::crypto::{vrf, kes, sign};
@@ -10,10 +12,39 @@ use super::identity::StakerIdentity;
 /// Units of stake
 ///
 /// This should always be <= to StakeTotal
+#[derive(Clone,Copy,PartialEq,Eq,PartialOrd,Ord)]
 pub struct StakeUnits(u128);
 
+impl Add<StakeUnits> for StakeUnits {
+    type Output = StakeUnits;
+    fn add(self, rhs: StakeUnits) -> Self {
+        StakeUnits(self.0 + rhs.0)
+    }
+}
+impl Sub<StakeUnits> for StakeUnits {
+    type Output = StakeUnits;
+    fn sub(self, rhs: StakeUnits) -> Self {
+        StakeUnits(self.0 - rhs.0)
+    }
+}
+
+
 /// Total amount of unit of stake in the system
+#[derive(Clone,Copy,PartialEq,Eq,PartialOrd,Ord)]
 pub struct StakeTotal(u128);
+
+impl Add<StakeUnits> for StakeTotal {
+    type Output = StakeTotal;
+    fn add(self, rhs: StakeUnits) -> Self {
+        StakeTotal(self.0 + rhs.0)
+    }
+}
+impl Sub<StakeUnits> for StakeTotal {
+    type Output = StakeTotal;
+    fn sub(self, rhs: StakeUnits) -> Self {
+        StakeTotal(self.0 - rhs.0)
+    }
+}
 
 /// Percent Stake in the system between 0% (0.0) and 100% (1.0)
 ///
@@ -34,9 +65,35 @@ pub struct StakerPublicInformation {
     block_key: Option<<sign::Ed25519 as SignatureAlgorithm>::PublicKey>,
 }
 
-/// Distribution
+/// Distribution of stake according to identities
+#[derive(Clone)]
 pub struct StakeDistribution {
+    total: StakeTotal,
     map: BTreeMap<StakerIdentity, StakeUnits>,
+}
+
+impl StakeDistribution {
+    pub fn create() -> Self {
+        StakeDistribution { total: StakeTotal(0), map: BTreeMap::new() }
+    }
+
+    pub fn add(&mut self, id: StakerIdentity, units: StakeUnits) {
+        self.map.entry(id).and_modify(|v| *v = *v + units).or_insert(units);
+        self.total = self.total + units;
+    }
+
+    pub fn remove(&mut self, id: StakerIdentity, units: StakeUnits) {
+        match self.map.entry(id) {
+            Entry::Vacant(_) => {
+                // FIXME don't do anything for now, but it should likely be reported back.
+            },
+            Entry::Occupied(mut entry) => {
+                let mut e = entry.get_mut();
+                *e = *e - units;
+                self.total = self.total - units;
+            },
+        }
+    }
 }
 
 /// Delegation of stake from one staker to another
