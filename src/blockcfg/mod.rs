d8fbf89d0b9dba67ca7c36e76e25a10208b3b84f
Nicolas Di Prima <nicolas.diprima@iohk.io>
2018-12-13 17:15:19 +0000

    fix the coding style in the blockcfg/mod.rs file

--- src/blockcfg/mod.rs
+++ src/blockcfg/mod.rs
@@ -17,7 +17,7 @@ pub mod update;
 
 pub trait BlockConfig {
     type Block: chain::Block<Hash = Self::BlockHash>
-              + ledger::HasTransaction<Transaction = Self::Transaction>;
+        + ledger::HasTransaction<Transaction = Self::Transaction>;
     type BlockHash;
     type BlockHeader;
     type Transaction: ledger::Transaction<Id = Self::TransactionId>;
@@ -25,8 +25,7 @@ pub trait BlockConfig {
     type GenesisData;
 
     type Ledger: ledger::Ledger<Transaction = Self::Transaction>
-               + update::Update<Block = Self::Block>;
-
+        + update::Update<Block = Self::Block>;
 
     fn make_block(
         secret_key: &secure::NodeSecret,
@@ -54,64 +53,62 @@ impl BlockConfig for Cardano {
         ledger: &Self::Ledger,
         block_id: <Self::Block as chain::Block>::Id,
         transactions: Vec<Self::Transaction>,
-    ) -> Self::Block
-    {
-        use cardano::{block::*};
-        use cbor_event::{Value};
-        use blockcfg::update::{Update};
+    ) -> Self::Block {
+        use blockcfg::update::Update;
+        use cardano::block::*;
         use cardano::hash::Blake2b256;
+        use cbor_event::Value;
 
         let previous_hash = ledger.get_tip();
 
         match block_id {
             BlockDate::Boundary(_) => unimplemented!(),
             BlockDate::Normal(block_id) => {
-        let pm = ledger.protocol_magic;
-        let bver = BlockVersion::new(1,0,0);
-        let sver = SoftwareVersion::new(env!("CARGO_PKG_NAME"), 1).unwrap();
+                let pm = ledger.protocol_magic;
+                let bver = BlockVersion::new(1, 0, 0);
+                let sver = SoftwareVersion::new(env!("CARGO_PKG_NAME"), 1).unwrap();
 
-        let sig = secret_key.sign_block();
+                let sig = secret_key.sign_block();
 
-        let body = normal::Body {
-            tx: normal::TxPayload::new(transactions),
-            ssc: normal::SscPayload::fake(),
-            delegation: normal::DlgPayload(Value::U64(0)),
-            update: update::UpdatePayload {
-                proposal: None,
-                votes: Vec::new(),
-            },
-        };
-        let extra = Value::U64(0);
+                let body = normal::Body {
+                    tx: normal::TxPayload::new(transactions),
+                    ssc: normal::SscPayload::fake(),
+                    delegation: normal::DlgPayload(Value::U64(0)),
+                    update: update::UpdatePayload {
+                        proposal: None,
+                        votes: Vec::new(),
+                    },
+                };
+                let extra = Value::U64(0);
 
-        let body_proof = normal::BodyProof::generate_from_body(&body);
-        let extra_bytes = cbor!(&extra).unwrap();
+                let body_proof = normal::BodyProof::generate_from_body(&body);
+                let extra_bytes = cbor!(&extra).unwrap();
 
-        let hdr = normal::BlockHeader {
-            protocol_magic: pm,
-            previous_header: previous_hash.clone(),
-            body_proof: body_proof,
-            consensus: normal::Consensus {
-                slot_id: block_id,
-                leader_key: public_key.block_publickey.clone(),
-                chain_difficulty: ChainDifficulty::from(0u64),
-                block_signature: sig,
-            },
-            extra_data: HeaderExtraData {
-                block_version: bver,
-                software_version: sver,
-                attributes: BlockHeaderAttributes(Value::U64(0)),
-                extra_data_proof: Blake2b256::new(&extra_bytes),
-            },
-        };
-        let b = normal::Block {
-            header: hdr,
-            body: body,
-            extra: extra,
-        };
+                let hdr = normal::BlockHeader {
+                    protocol_magic: pm,
+                    previous_header: previous_hash.clone(),
+                    body_proof: body_proof,
+                    consensus: normal::Consensus {
+                        slot_id: block_id,
+                        leader_key: public_key.block_publickey.clone(),
+                        chain_difficulty: ChainDifficulty::from(0u64),
+                        block_signature: sig,
+                    },
+                    extra_data: HeaderExtraData {
+                        block_version: bver,
+                        software_version: sver,
+                        attributes: BlockHeaderAttributes(Value::U64(0)),
+                        extra_data_proof: Blake2b256::new(&extra_bytes),
+                    },
+                };
+                let b = normal::Block {
+                    header: hdr,
+                    body: body,
+                    extra: extra,
+                };
 
-        Block::MainBlock(b)
+                Block::MainBlock(b)
             }
         }
-
     }
 }
