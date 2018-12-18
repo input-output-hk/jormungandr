01fa210314e8fde71d2205d1aa1912206e09b86a
Mikhail Zabaluev <mikhail.zabaluev@iohk.io>
2018-12-18 23:02:49 +0200

    blockcfg::cardano: Rearranged the code
    Moved impl BlockConfig for Cardano to the top, as it is the root of all
    other Cardano implementations.
    Moved the impls for ChainState together at the bottom.

--- src/blockcfg/cardano.rs
+++ src/blockcfg/cardano.rs
@@ -19,6 +19,83 @@ pub type BlockHash = cardano::block::HeaderHash;
 pub type Block = cardano::block::Block;
 pub type Header = cardano::block::BlockHeader;
 
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
+pub struct Cardano;
+impl BlockConfig for Cardano {
+    type Block = Block;
+    type BlockHash = BlockHash;
+    type BlockHeader = Header;
+    type Transaction = Transaction;
+    type TransactionId = TransactionId;
+    type GenesisData = GenesisData;
+    type Ledger = cardano::block::verify_chain::ChainState;
+
+    fn make_block(
+        secret_key: &secure::NodeSecret,
+        public_key: &secure::NodePublic,
+        ledger: &Self::Ledger,
+        block_date: <Self::Block as property::Block>::Date,
+        transactions: Vec<Self::Transaction>,
+    ) -> Self::Block {
+        use crate::blockcfg::property::Update;
+        use cardano::block::*;
+        use cardano::hash::Blake2b256;
+        use cbor_event::Value;
+
+        let previous_hash = ledger.tip();
+
+        match block_date {
+            BlockDate::Boundary(_) => unimplemented!(),
+            BlockDate::Normal(block_id) => {
+                let pm = ledger.protocol_magic;
+                let bver = BlockVersion::new(1, 0, 0);
+                let sver = SoftwareVersion::new(env!("CARGO_PKG_NAME"), 1).unwrap();
+
+                let sig = secret_key.sign_block();
+
+                let body = normal::Body {
+                    tx: normal::TxPayload::new(transactions),
+                    ssc: normal::SscPayload::fake(),
+                    delegation: normal::DlgPayload(Value::U64(0)),
+                    update: update::UpdatePayload {
+                        proposal: None,
+                        votes: Vec::new(),
+                    },
+                };
+                let extra = Value::U64(0);
+
+                let body_proof = normal::BodyProof::generate_from_body(&body);
+                let extra_bytes = cbor!(&extra).unwrap();
+
+                let hdr = normal::BlockHeader {
+                    protocol_magic: pm,
+                    previous_header: previous_hash.clone(),
+                    body_proof: body_proof,
+                    consensus: normal::Consensus {
+                        slot_id: block_id,
+                        leader_key: public_key.block_publickey.clone(),
+                        chain_difficulty: ChainDifficulty::from(0u64),
+                        block_signature: sig,
+                    },
+                    extra_data: HeaderExtraData {
+                        block_version: bver,
+                        software_version: sver,
+                        attributes: BlockHeaderAttributes(Value::U64(0)),
+                        extra_data_proof: Blake2b256::new(&extra_bytes),
+                    },
+                };
+                let b = normal::Block {
+                    header: hdr,
+                    body: body,
+                    extra: extra,
+                };
+
+                Block::MainBlock(b)
+            }
+        }
+    }
+}
+
 impl property::Block for Block {
     type Id = BlockHash;
     type Date = cardano::block::BlockDate;
@@ -56,6 +133,9 @@ impl property::HasTransaction for Block {
         }
     }
 }
+
+
+
 impl property::Transaction for Transaction {
     type Input  = cardano::tx::TxoPointer;
     type Output = cardano::tx::TxOut;
@@ -149,83 +229,6 @@ impl property::Ledger for ChainState {
         }
 
         Ok(self)
-    }
-}
-
-#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
-pub struct Cardano;
-impl BlockConfig for Cardano {
-    type Block = Block;
-    type BlockHash = BlockHash;
-    type BlockHeader = Header;
-    type Transaction = Transaction;
-    type TransactionId = TransactionId;
-    type GenesisData = GenesisData;
-    type Ledger = cardano::block::verify_chain::ChainState;
-
-    fn make_block(
-        secret_key: &secure::NodeSecret,
-        public_key: &secure::NodePublic,
-        ledger: &Self::Ledger,
-        block_date: <Self::Block as property::Block>::Date,
-        transactions: Vec<Self::Transaction>,
-    ) -> Self::Block {
-        use crate::blockcfg::property::Update;
-        use cardano::block::*;
-        use cardano::hash::Blake2b256;
-        use cbor_event::Value;
-
-        let previous_hash = ledger.get_tip();
-
-        match block_date {
-            BlockDate::Boundary(_) => unimplemented!(),
-            BlockDate::Normal(block_id) => {
-                let pm = ledger.protocol_magic;
-                let bver = BlockVersion::new(1, 0, 0);
-                let sver = SoftwareVersion::new(env!("CARGO_PKG_NAME"), 1).unwrap();
-
-                let sig = secret_key.sign_block();
-
-                let body = normal::Body {
-                    tx: normal::TxPayload::new(transactions),
-                    ssc: normal::SscPayload::fake(),
-                    delegation: normal::DlgPayload(Value::U64(0)),
-                    update: update::UpdatePayload {
-                        proposal: None,
-                        votes: Vec::new(),
-                    },
-                };
-                let extra = Value::U64(0);
-
-                let body_proof = normal::BodyProof::generate_from_body(&body);
-                let extra_bytes = cbor!(&extra).unwrap();
-
-                let hdr = normal::BlockHeader {
-                    protocol_magic: pm,
-                    previous_header: previous_hash.clone(),
-                    body_proof: body_proof,
-                    consensus: normal::Consensus {
-                        slot_id: block_id,
-                        leader_key: public_key.block_publickey.clone(),
-                        chain_difficulty: ChainDifficulty::from(0u64),
-                        block_signature: sig,
-                    },
-                    extra_data: HeaderExtraData {
-                        block_version: bver,
-                        software_version: sver,
-                        attributes: BlockHeaderAttributes(Value::U64(0)),
-                        extra_data_proof: Blake2b256::new(&extra_bytes),
-                    },
-                };
-                let b = normal::Block {
-                    header: hdr,
-                    body: body,
-                    extra: extra,
-                };
-
-                Block::MainBlock(b)
-            }
-        }
     }
 }
 
