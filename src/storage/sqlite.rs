bb5c45920b1f3351d73593ce94bcc43b88bdee43
Eelco Dolstra <edolstra@gmail.com>
2019-01-04 09:51:07 +0100

    SQLiteBlockStore: Use prepared statements
    This gives a significant speedup (~53% on the staging load / iteration
    test).

--- src/storage/sqlite.rs
+++ src/storage/sqlite.rs
@@ -2,11 +2,22 @@ use super::{Hash, Block, BlockInfo, BackLink, BlockStore, Error};
 use sqlite;
 use cardano::util::try_from_slice::TryFromSlice;
 use blockchain::Date;
+use std::cell::RefCell;
 
 pub struct SQLiteBlockStore<B> where B: Block {
     genesis_hash: Hash,
-    connection: sqlite::Connection,
-    //insert_block: sqlite::Statement<'db>,
+    connection: Box<sqlite::Connection>,
+
+    // Prepared statements. Note: we currently give these a fake
+    // static lifetime to work around the issue described in
+    // https://stackoverflow.com/questions/27552670/how-to-store-sqlite-prepared-statements-for-later.
+    // We use a RefCell to allow them to be used from &self methods.
+    stmt_insert_block: RefCell<sqlite::Statement<'static>>,
+    stmt_insert_block_info: RefCell<sqlite::Statement<'static>>,
+    stmt_get_block: RefCell<sqlite::Statement<'static>>,
+    stmt_get_block_info: RefCell<sqlite::Statement<'static>>,
+    stmt_put_tag: RefCell<sqlite::Statement<'static>>,
+
     dummy: std::marker::PhantomData<B>,
     in_txn: bool,
     pending_changes: usize,
@@ -14,7 +25,8 @@ pub struct SQLiteBlockStore<B> where B: Block {
 
 impl<B> SQLiteBlockStore<B> where B: Block {
     pub fn new(genesis_hash: Hash, path: &str) -> Self {
-        let connection = sqlite::open(path).unwrap();
+
+        let connection = Box::new(sqlite::open(path).unwrap());
 
         connection.execute(r#"
           create table if not exists BlockInfo (
@@ -39,8 +51,17 @@ impl<B> SQLiteBlockStore<B> where B: Block {
           );
         "#).unwrap();
 
+        let make_statement = |connection: &Box<sqlite::Connection>, s: &str| unsafe {
+            RefCell::new(std::mem::transmute(connection.prepare(s).unwrap()))
+        };
+
         SQLiteBlockStore {
             genesis_hash,
+            stmt_insert_block: make_statement(&connection, "insert into Blocks (hash, block) values(?, ?)"),
+            stmt_insert_block_info: make_statement(&connection, "insert into BlockInfo (hash, date, depth, parent, fast_distance, fast_hash) values(?, ?, ?, ?, ?, ?)"),
+            stmt_get_block: make_statement(&connection, "select block from Blocks where hash = ?"),
+            stmt_get_block_info: make_statement(&connection, "select depth, parent, fast_distance, fast_hash, date from BlockInfo where hash = ?"),
+            stmt_put_tag: make_statement(&connection, "insert or replace into Tags (name, hash) values(?, ?)"),
             connection,
             dummy: std::marker::PhantomData,
             in_txn: false,
@@ -84,57 +105,56 @@ impl<B> BlockStore<B> for SQLiteBlockStore<B> where B: Block {
 
         // FIXME: wrap the next two statements in a transaction
 
-        let mut statement = self.connection.prepare(
-            "insert into Blocks (hash, block) values(?, ?)").unwrap();
-        statement.bind(1, &block_info.block_hash[..]).unwrap();
-        statement.bind(2, &block.serialize()[..]).unwrap();
-        statement.next().unwrap();
+        let mut stmt_insert_block = self.stmt_insert_block.borrow_mut();
+        stmt_insert_block.reset();
+        stmt_insert_block.bind(1, &block_info.block_hash[..]).unwrap();
+        stmt_insert_block.bind(2, &block.serialize()[..]).unwrap();
+        stmt_insert_block.next().unwrap();
 
-        let mut statement = self.connection.prepare(
-            "insert into BlockInfo (hash, date, depth, parent, fast_distance, fast_hash) values(?, ?, ?, ?, ?, ?)").unwrap();
-        statement.bind(1, &block_info.block_hash[..]).unwrap();
-        statement.bind(2, block_info.block_date.serialize() as i64).unwrap();
-        statement.bind(3, block_info.depth as i64).unwrap();
+        let mut stmt_insert_block_info = self.stmt_insert_block_info.borrow_mut();
+        stmt_insert_block_info.reset();
+        stmt_insert_block_info.bind(1, &block_info.block_hash[..]).unwrap();
+        stmt_insert_block_info.bind(2, block_info.block_date.serialize() as i64).unwrap();
+        stmt_insert_block_info.bind(3, block_info.depth as i64).unwrap();
         let parent = block_info.back_links.iter().find(|x| x.distance == 1).unwrap();
-        statement.bind(4, &parent.block_hash[..]).unwrap();
+        stmt_insert_block_info.bind(4, &parent.block_hash[..]).unwrap();
         match block_info.back_links.iter().find(|x| x.distance != 1) {
             Some(fast_link) => {
-                statement.bind(5, fast_link.distance as i64).unwrap();
-                statement.bind(6, &fast_link.block_hash[..]).unwrap();
+                stmt_insert_block_info.bind(5, fast_link.distance as i64).unwrap();
+                stmt_insert_block_info.bind(6, &fast_link.block_hash[..]).unwrap();
             },
             None => {
-                statement.bind(5, ()).unwrap();
-                statement.bind(6, ()).unwrap();
+                stmt_insert_block_info.bind(5, ()).unwrap();
+                stmt_insert_block_info.bind(6, ()).unwrap();
             }
         };
-        statement.next().unwrap();
+        stmt_insert_block_info.next().unwrap();
 
         Ok(())
     }
 
     fn get_block(&self, block_hash: &Hash) -> Result<(B, BlockInfo<B>), Error> {
-        let mut statement = self.connection.prepare(
-            "select block from Blocks where hash = ?"
-        ).unwrap();
-        statement.bind(1, &block_hash[..]).unwrap();
+        let mut stmt_get_block = self.stmt_get_block.borrow_mut();
+        stmt_get_block.reset();
 
-        match statement.next().unwrap() {
+        stmt_get_block.bind(1, &block_hash[..]).unwrap();
+
+        match stmt_get_block.next().unwrap() {
             sqlite::State::Done =>
                 Err(cardano_storage::Error::BlockNotFound(block_hash.clone().into())),
             sqlite::State::Row =>
-                Ok((B::deserialize(&statement.read::<Vec<u8>>(0).unwrap()),
+                Ok((B::deserialize(&stmt_get_block.read::<Vec<u8>>(0).unwrap()),
                     self.get_block_info(block_hash)?))
         }
     }
 
     fn get_block_info(&self, block_hash: &Hash) -> Result<BlockInfo<B>, Error> {
+        let mut stmt_get_block_info = self.stmt_get_block_info.borrow_mut();
+        stmt_get_block_info.reset();
 
-        let mut statement = self.connection.prepare(
-            "select depth, parent, fast_distance, fast_hash, date from BlockInfo where hash = ?"
-        ).unwrap();
-        statement.bind(1, &block_hash[..]).unwrap();
+        stmt_get_block_info.bind(1, &block_hash[..]).unwrap();
 
-        match statement.next().unwrap() {
+        match stmt_get_block_info.next().unwrap() {
             sqlite::State::Done =>
                 Err(cardano_storage::Error::BlockNotFound(block_hash.clone().into())),
             sqlite::State::Row => {
@@ -142,22 +162,22 @@ impl<B> BlockStore<B> for SQLiteBlockStore<B> where B: Block {
                 let mut back_links = vec![
                     BackLink {
                         distance: 1,
-                        block_hash: blob_to_hash(statement.read::<Vec<u8>>(1).unwrap())
+                        block_hash: blob_to_hash(stmt_get_block_info.read::<Vec<u8>>(1).unwrap())
                     }
                 ];
 
-                let fast_distance = statement.read::<i64>(2).unwrap() as u64;
+                let fast_distance = stmt_get_block_info.read::<i64>(2).unwrap() as u64;
                 if fast_distance != 0 {
                     back_links.push(BackLink {
                         distance: fast_distance,
-                        block_hash: blob_to_hash(statement.read::<Vec<u8>>(3).unwrap())
+                        block_hash: blob_to_hash(stmt_get_block_info.read::<Vec<u8>>(3).unwrap())
                     });
                 }
 
                 Ok(BlockInfo {
                     block_hash: block_hash.clone(),
-                    block_date: B::Date::deserialize(statement.read::<i64>(0).unwrap() as u64),
-                    depth: statement.read::<i64>(0).unwrap() as u64,
+                    block_date: B::Date::deserialize(stmt_get_block_info.read::<i64>(0).unwrap() as u64),
+                    depth: stmt_get_block_info.read::<i64>(0).unwrap() as u64,
                     back_links
                 })
             }
@@ -166,11 +186,11 @@ impl<B> BlockStore<B> for SQLiteBlockStore<B> where B: Block {
 
     fn put_tag(&mut self, tag_name: &str, block_hash: &Hash) -> Result<(), Error>
     {
-        let mut statement = self.connection.prepare(
-            "insert or replace into Tags (name, hash) values(?, ?)").unwrap();
-        statement.bind(1, tag_name).unwrap();
-        statement.bind(2, &block_hash[..]).unwrap();
-        statement.next().unwrap();
+        let mut stmt_put_tag = self.stmt_put_tag.borrow_mut();
+        stmt_put_tag.reset();
+        stmt_put_tag.bind(1, tag_name).unwrap();
+        stmt_put_tag.bind(2, &block_hash[..]).unwrap();
+        stmt_put_tag.next().unwrap();
         Ok(())
     }
 
