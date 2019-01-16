0a9a15679b759d05cfa95a3871c15e455ccee088
Mikhail Zabaluev <mikhail.zabaluev@iohk.io>
2019-01-16 22:29:27 +0200

    network-grpc: impl Future for Connection
    server::Connection provides a future so the application can get notified
    when the connection is torn down.

--- network-grpc/src/server.rs
+++ network-grpc/src/server.rs
@@ -193,6 +193,19 @@ where
     <T as Node>::BlockService: Clone,
     <T as Node>::TransactionService: Clone;
 
+impl<T> Future for Connection<T>
+where
+    T: Node + 'static,
+    <T as Node>::BlockService: Clone,
+    <T as Node>::TransactionService: Clone,
+{
+    type Item = ();
+    type Error = Error;
+    fn poll(&mut self) -> Poll<(), Error> {
+        self.0.poll().map_err(|e| e.into())
+    }
+}
+
 impl<T> Server<T>
 where
     T: Node + 'static,
@@ -222,15 +235,14 @@ where
 /// objects representing accepted TCP connections from clients.
 /// The TCP_NODELAY option is disabled on the returned sockets as
 /// necessary for the HTTP/2 protocol.
-pub fn listen(addr: &SocketAddr) -> Result<impl Stream<Item = TcpStream, Error = Error>, Error> {
+pub fn listen(
+    addr: &SocketAddr,
+) -> Result<impl Stream<Item = TcpStream, Error = tokio::io::Error>, tokio::io::Error> {
     let listener = TcpListener::bind(&addr)?;
-    let stream = listener
-        .incoming()
-        .and_then(|sock| {
-            sock.set_nodelay(true)?;
-            Ok(sock)
-        })
-        .map_err(|e| Error::Io(e));
+    let stream = listener.incoming().and_then(|sock| {
+        sock.set_nodelay(true)?;
+        Ok(sock)
+    });
     Ok(stream)
 }
 
@@ -238,6 +250,11 @@ pub fn listen(addr: &SocketAddr) -> Result<impl Stream<Item = TcpStream, Error =
 #[derive(Debug)]
 pub enum Error {
     Io(tokio::io::Error),
+    Http2Handshake(h2::Error),
+    Http2Protocol(h2::Error),
+    NewService(h2::Error),
+    Service(h2::Error),
+    Execute,
 }
 
 impl From<tokio::io::Error> for Error {
@@ -246,10 +263,38 @@ impl From<tokio::io::Error> for Error {
     }
 }
 
+type H2Error<T> = tower_h2::server::Error<gen::node::server::NodeServer<NodeService<T>>>;
+
+// Incorporating tower_h2::server::Error would be too cumbersome due to the
+// type parameter baggage, see https://github.com/tower-rs/tower-h2/issues/50
+// So we match it into our own variants.
+impl<T> From<H2Error<T>> for Error
+where
+    T: Node,
+    <T as Node>::BlockService: Clone,
+    <T as Node>::TransactionService: Clone,
+{
+    fn from(err: H2Error<T>) -> Self {
+        use tower_h2::server::Error::*;
+        match err {
+            Handshake(e) => Error::Http2Handshake(e),
+            Protocol(e) => Error::Http2Protocol(e),
+            NewService(e) => Error::NewService(e),
+            Service(e) => Error::Service(e),
+            Execute => Error::Execute,
+        }
+    }
+}
+
 impl fmt::Display for Error {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match self {
             Error::Io(e) => write!(f, "I/O error: {}", e),
+            Error::Http2Handshake(e) => write!(f, "HTTP/2 handshake error: {}", e),
+            Error::Http2Protocol(e) => write!(f, "HTTP/2 protocol error: {}", e),
+            Error::NewService(e) => write!(f, "service creation error: {}", e),
+            Error::Service(e) => write!(f, "error returned by service: {}", e),
+            Error::Execute => write!(f, "task execution error"),
         }
     }
 }
@@ -258,6 +303,11 @@ impl ErrorTrait for Error {
     fn source(&self) -> Option<&(dyn ErrorTrait + 'static)> {
         match self {
             Error::Io(e) => Some(e),
+            Error::Http2Handshake(e) => Some(e),
+            Error::Http2Protocol(e) => Some(e),
+            Error::NewService(e) => Some(e),
+            Error::Service(e) => Some(e),
+            Error::Execute => None,
         }
     }
 }
