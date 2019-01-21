bf1bfb95f4e46876dc915305dfb1aa160d242491
Eelco Dolstra <edolstra@gmail.com>
2019-01-21 12:21:15 +0100

    Remove storage stuff that has been moved to rust-cardano

--- src/blockchain/mod.rs
+++ src/blockchain/mod.rs
@@ -3,29 +3,3 @@ mod process;
 
 pub use self::chain::{Blockchain, BlockchainR};
 pub use self::process::process;
-
-pub type Hash = cardano::hash::Blake2b256;
-
-pub trait Block: Clone {
-    fn get_hash(&self) -> Hash;
-
-    fn get_parent(&self) -> Hash;
-
-    fn as_bytes(&self) -> Vec<u8>;
-}
-
-impl Block for cardano::block::Block {
-    fn get_hash(&self) -> Hash {
-        unimplemented!()
-        // self.header().compute_hash()
-    }
-
-    fn get_parent(&self) -> Hash {
-        unimplemented!()
-        // self.header().get_previous_header()
-    }
-
-    fn as_bytes(&self) -> Vec<u8> {
-        cbor!(self).unwrap()
-    }
-}
