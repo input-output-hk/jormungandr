8c4fd7ce6d5ade82638c221ed0a5ea7d06a38a90
Mikhail Zabaluev <mikhail.zabaluev@iohk.io>
2019-02-13 08:56:16 +0200

    blockcfg::genesis_data: Add a proper error type

--- src/blockcfg/genesis_data.rs
+++ src/blockcfg/genesis_data.rs
@@ -1,14 +1,28 @@
 //! Generic Genesis data
 
+use std::{error, fmt, io, time};
+
 #[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
 pub struct GenesisData {
-    pub start_time: std::time::SystemTime,
-    pub slot_duration: std::time::Duration,
+    pub start_time: time::SystemTime,
+    pub slot_duration: time::Duration,
     pub epoch_stability_depth: usize,
 }
 
+// TODO: details
+#[derive(Debug)]
+struct ParseError();
+
+impl error::Error for ParseError {}
+
+impl fmt::Display for ParseError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "error parsing genesis data")
+    }
+}
+
 impl GenesisData {
-    pub fn parse<R: std::io::BufRead>(reader: R) -> Result<Self, impl std::error::Error> {
+    pub fn parse<R: io::BufRead>(reader: R) -> Result<Self, ParseError> {
         unimplemented!()
     }
 }
