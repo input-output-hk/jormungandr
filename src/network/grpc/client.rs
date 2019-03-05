cddecb20f5f01923bbef37e730ee7c9403ee875f
Mikhail Zabaluev <mikhail.zabaluev@iohk.io>
2019-03-05 18:09:09 +0800

    Update to client::ProtocolConfig trait changes in network-grpc

--- src/network/grpc/client.rs
+++ src/network/grpc/client.rs
@@ -1,27 +1,19 @@
-use super::super::GlobalState;
-use crate::{blockcfg::BlockConfig, settings::start::network::Peer};
+use super::super::{GlobalState, NetworkBlockConfig};
+use crate::settings::start::network::Peer;
 
-use chain_core::property;
 use network_core::client::block::BlockService;
-use network_grpc::{
-    client::{chain_bounds, Client},
-    peer as grpc_peer,
-};
+use network_grpc::{client::Client, peer as grpc_peer};
 
 use futures::future;
 use futures::prelude::*;
 use tokio::{executor::DefaultExecutor, net::TcpStream};
-
-use std::str::FromStr;
 
 pub fn run_connect_socket<B>(
     peer: Peer,
     _state: GlobalState<B>,
 ) -> impl Future<Item = (), Error = ()>
 where
-    B: BlockConfig,
-    B::Block: chain_bounds::Block,
-    <B::Block as property::Block>::Date: FromStr,
+    B: NetworkBlockConfig,
 {
     info!("connecting to subscription peer {}", peer.connection);
     let peer = grpc_peer::TcpPeer::new(*peer.address());
@@ -31,13 +23,11 @@ where
         .map_err(move |err| {
             error!("Error connecting to peer {}: {:?}", addr, err);
         })
-        .and_then(
-            |mut client: Client<B::Block, B::Gossip, TcpStream, DefaultExecutor>| {
-                client.subscribe_to_blocks().map_err(move |err| {
-                    error!("SubscribeToBlocks request failed: {:?}", err);
-                })
-            },
-        )
+        .and_then(|mut client: Client<B, TcpStream, DefaultExecutor>| {
+            client.subscribe_to_blocks().map_err(move |err| {
+                error!("SubscribeToBlocks request failed: {:?}", err);
+            })
+        })
         .and_then(|_subscription| {
             // FIXME: do something to it
             future::ok(())
