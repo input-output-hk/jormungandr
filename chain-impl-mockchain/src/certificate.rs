98e540d69514ef880ca25507451de2d542772667
Vincent Hanquez <vincent.hanquez@iohk.io>
2019-03-06 17:38:31 +0800

    add SigningAlgorithm

--- chain-impl-mockchain/src/certificate.rs
+++ chain-impl-mockchain/src/certificate.rs
@@ -202,6 +202,7 @@ impl property::Deserialize for StakePoolRetirement {
 #[cfg(test)]
 mod test {
     use super::*;
+    use chain_crypto::SigningAlgorithm;
     use quickcheck::{Arbitrary, Gen};
 
     impl<T: Arbitrary, A: SigningAlgorithm> Arbitrary for Signed<T, A> {
