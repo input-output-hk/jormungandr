89156c1f81141eea9361e6958c8126a5b6939926
Mikhail Zabaluev <mikhail.zabaluev@iohk.io>
2019-03-07 17:14:42 +0800

    chain-crypto: make hex::encode generic for input type
    Make it work for Vec and other types that can look like a byte slice.

--- chain-crypto/src/hex.rs
+++ chain-crypto/src/hex.rs
@@ -2,7 +2,11 @@
 
 const ALPHABET: &'static [u8] = b"0123456789abcdef";
 
-pub fn encode(input: &[u8]) -> String {
+pub fn encode<D: AsRef<[u8]>>(input: D) -> String {
+    encode_bytes(input.as_ref())
+}
+
+fn encode_bytes(input: &[u8]) -> String {
     let mut v = Vec::with_capacity(input.len() * 2);
     for &byte in input.iter() {
         v.push(ALPHABET[(byte >> 4) as usize]);
