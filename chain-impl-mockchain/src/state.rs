4922005ff2556c08dd5227e10d5a57f194fb5390
Vincent Hanquez <vincent.hanquez@iohk.io>
2019-03-11 16:26:53 +0000

    change utxo and account

--- chain-impl-mockchain/src/state.rs
+++ chain-impl-mockchain/src/state.rs
@@ -1,18 +1,28 @@
 //! The global ledger/update/delegation states
 //!
 
-use crate::{account, leadership, ledger, setting};
+use crate::{account, leadership, setting, utxo};
+use cardano::address::Addr as OldAddress;
+use chain_addr::Address;
 use chain_core::property;
 
-pub(crate) type UTxOLedger = ledger::Ledger;
-pub(crate) type AccountLedger = account::Ledger;
-pub(crate) type Settings = setting::Settings;
-pub(crate) type Leadership =
-    Box<dyn property::LeaderSelection<Update = u8, Block = u8, Error = std::io::Error, LeaderId = leadership::PublicLeader>>;
+pub(crate) type Leadership = Box<
+    dyn property::LeaderSelection<
+        Update = u8,
+        Block = u8,
+        Error = std::io::Error,
+        LeaderId = leadership::PublicLeader,
+    >,
+>;
+
+pub struct Ledger {
+    pub(crate) utxos: utxo::Ledger<Address>,
+    pub(crate) oldutxos: utxo::Ledger<OldAddress>,
+    pub(crate) accounts: account::Ledger,
+}
 
 pub struct State {
-    pub(crate) utxos: UTxOLedger,
-    pub(crate) accounts: AccountLedger,
-    pub(crate) settings: Settings,
+    pub(crate) ledger: Ledger,
+    pub(crate) settings: setting::Settings,
     pub(crate) leadership: Leadership,
 }
