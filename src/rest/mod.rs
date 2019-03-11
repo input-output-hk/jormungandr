581888cf20bc4f62feb765c640a00d7650e45ba2
Mikhail Zabaluev <mikhail.zabaluev@iohk.io>
2019-03-11 18:36:45 +0200

    Eliminate PathPredicate
    Use a more natural structure of App with prefixes, invoking handlers for
    matching resources.

--- src/rest/mod.rs
+++ src/rest/mod.rs
@@ -26,9 +26,6 @@ pub fn start_rest_server(config: &Rest, context: Context) -> Result<ServerServic
     let settings = context.blockchain.read().unwrap().state.settings.clone();
     ServerService::builder(config.pkcs12.clone(), config.listen.clone(), prefix)
         .add_handler(v0::block::create_handler(context.blockchain.clone()))
-        .add_handler(v0::block::next_id::create_handler(
-            context.blockchain.clone(),
-        ))
         .add_handler(v0::node::stats::create_handler(context.stats_counter))
         .add_handler(v0::tip::create_handler(settings))
         .add_handler(v0::transaction::create_handler(context.transaction_task))
--- src/rest/server_service/mod.rs
+++ src/rest/server_service/mod.rs
@@ -2,12 +2,10 @@
 //! to be run as a background service.
 
 mod error;
-mod path_predicate;
 mod server_service;
 mod server_service_builder;
 
 pub use self::error::Error;
-pub use self::path_predicate::PathPredicate;
 pub use self::server_service::ServerService;
 pub use self::server_service_builder::ServerServiceBuilder;
 
