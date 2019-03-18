5eda707948b04bb1d3596c4f92c2ad25c13f8d00
Nicolas Di Prima <nicolas.diprima@iohk.io>
2019-03-18 21:50:43 +0000

    there is nolonger settings in the BlockchainR. Use the state

--- src/rest/mod.rs
+++ src/rest/mod.rs
@@ -23,11 +23,10 @@ pub fn start_rest_server(config: &Rest, context: Context) -> Result<Server, Sett
         .as_ref()
         .map(|prefix| prefix.as_str())
         .unwrap_or("");
-    let settings = context.blockchain.read().unwrap().state.settings.clone();
     Server::builder(config.pkcs12.clone(), config.listen.clone(), prefix)
         .add_handler(v0::block::create_handler(context.blockchain.clone()))
         .add_handler(v0::node::stats::create_handler(context.stats_counter))
-        .add_handler(v0::tip::create_handler(settings))
+        .add_handler(v0::tip::create_handler(context.blockchain.clone()))
         .add_handler(v0::transaction::create_handler(context.transaction_task))
         .add_handler(v0::utxo::create_handler(context.blockchain))
         .build()
