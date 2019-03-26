4de48873b4728dc1850b71133cc6e082d7e4d84a
Nicolas Di Prima <nicolas.diprima@iohk.io>
2019-03-26 11:44:57 +0000

    display the blockchain chain_length

--- src/main.rs
+++ src/main.rs
@@ -62,7 +62,7 @@ use futures::Future;
 use bech32::{u5, Bech32, FromBase32, ToBase32};
 use blockcfg::{genesis_data::ConfigGenesisData, genesis_data::GenesisData};
 use blockchain::{Blockchain, BlockchainR};
-use chain_core::property::Settings as _;
+use chain_core::property::{Block as _, Settings as _};
 use chain_crypto::{
     AsymmetricKey, Curve25519_2HashDH, Ed25519, Ed25519Bip32, Ed25519Extended, FakeMMM,
 };
@@ -120,11 +120,7 @@ fn startup_info(gd: &GenesisData, blockchain: &Blockchain, _settings: &settings:
         "k={} tip={} length={:?}",
         gd.epoch_stability_depth,
         blockchain.get_tip(),
-        blockchain
-            .multiverse
-            .get(&blockchain.get_tip())
-            .unwrap()
-            .chain_length()
+        blockchain.get_block_tip().unwrap().0.chain_length()
     );
 }
 
