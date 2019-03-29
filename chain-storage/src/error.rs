8567f9c41fc8f18c309042c17f5e4e3223e6cea1
Nicolas Di Prima <nicolas.diprima@iohk.io>
2019-03-29 23:41:46 +0000

    remove explicit panic and add an error instead

--- chain-storage/src/error.rs
+++ chain-storage/src/error.rs
@@ -3,12 +3,14 @@ use std::{error, fmt};
 #[derive(Debug, Clone, Eq, PartialEq)]
 pub enum Error {
     BlockNotFound, // FIXME: add BlockId
+    CannotIterate,
 }
 
 impl fmt::Display for Error {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match self {
             Error::BlockNotFound => write!(f, "block not found"),
+            Error::CannotIterate => write!(f, "cannot iterate between the 2 given blocks"),
         }
     }
 }
