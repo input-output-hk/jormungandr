12b4df33d7fe063b945dccd63281e061c0fbe825
Stewart Mackenzie <setori88@gmail.com>
2019-04-09 15:09:32 +0800

    chain-addr: allow configurable address prefixes
    set by editing the relevant environment variables:
    
    `$ TESTING_ADDRESS_PREFIX=teo PRODUCTION_ADDRESS_PREFIX=ceo cargo test`
    
    The address tests will inevitably fail as they are configured
    for ca & ta prefixes. This is an acceptable tradeoff.

---
+++ chain-addr/build.rs
@@ -0,0 +1,12 @@
+use std::env;
+use std::fs::File;
+use std::io::Write;
+use std::path::Path;
+
+fn main() {
+    let production_prefix = option_env!("PRODUCTION_ADDRESS_PREFIX").unwrap_or("ca");
+    let test_prefix = option_env!("TEST_ADDRESS_PREFIX").unwrap_or("ta");
+
+    println!("cargo:rustc-env=PRODUCTION_ADDRESS_PREFIX={}", production_prefix);
+    println!("cargo:rustc-env=TEST_ADDRESS_PREFIX={}", test_prefix);
+}
