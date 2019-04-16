9999b512c8efedc84bac367caeb1a6ff92bcf532
Nicolas Di Prima <nicolas.diprima@iohk.io>
2019-04-16 07:01:05 -0400

    simplify error code (self generate appropriate instances)

--- utils/src/certificate.rs
+++ utils/src/certificate.rs
@@ -2,53 +2,13 @@ use bech32::{Bech32, FromBase32, ToBase32};
 use chain_core::mempack::{ReadBuf, ReadError, Readable};
 use chain_core::property::Serialize;
 use chain_impl_mockchain::certificate::Certificate;
-use std::error::Error as StdError;
-use std::fmt;
 
-#[derive(Debug)]
-pub enum Error {
-    Bech32Error(bech32::Error),
-    HrpInvalid {
-        expected: &'static str,
-        actual: String,
-    },
-    ReadError(ReadError),
-}
+type StaticStr = &'static str;
 
-impl From<bech32::Error> for Error {
-    fn from(error: bech32::Error) -> Self {
-        Error::Bech32Error(error)
-    }
-}
-
-impl From<ReadError> for Error {
-    fn from(error: ReadError) -> Self {
-        Error::ReadError(error)
-    }
-}
-
-impl fmt::Display for Error {
-    fn fmt(&self, f: &mut fmt::Formatter) -> Result<(), fmt::Error> {
-        match self {
-            Error::Bech32Error(_) => write!(f, "Failed to parse bech32, invalid format"),
-            Error::ReadError(_) => write!(f, "Failed read bech32"),
-            Error::HrpInvalid { expected, actual } => write!(
-                f,
-                "Parsed bech32 has invalid HRP prefix '{}', expected '{}'",
-                expected, actual
-            ),
-        }
-    }
-}
-
-impl StdError for Error {
-    fn source(&self) -> Option<&(dyn StdError + 'static)> {
-        match self {
-            Error::Bech32Error(cause) => Some(cause),
-            Error::ReadError(cause) => Some(cause),
-            _ => None,
-        }
-    }
+custom_error! {pub Error
+    Bech32 { source: bech32::Error } = "failed to parse bech32",
+    Format { source: ReadError } = "Invalid format",
+    Hrp { expected: StaticStr, actual: String } = "Invalid bech32 prefix, expected: `{expected}'",
 }
 
 pub fn serialize_to_bech32(cert: &Certificate) -> Result<Bech32, Error> {
@@ -59,7 +19,7 @@ pub fn serialize_to_bech32(cert: &Certificate) -> Result<Bech32, Error> {
 pub fn deserialize_from_bech32(bech32_str: &str) -> Result<Certificate, Error> {
     let bech32: Bech32 = bech32_str.parse()?;
     if bech32.hrp() != "cert" {
-        return Err(Error::HrpInvalid {
+        return Err(Error::Hrp {
             expected: "cert",
             actual: bech32.hrp().to_string(),
         });
