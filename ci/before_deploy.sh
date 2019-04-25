c75777f17a1237d6a17086d3bd34d0a97733f35c
Nicolas Di Prima <nicolas.diprima@iohk.io>
2019-04-25 19:55:23 +0100

    update travis ci to include `cross` and prepare for delivery

---
+++ ci/before_deploy.sh
@@ -0,0 +1,34 @@
+# This script takes care of building your crate and packaging it for release
+
+set -ex
+
+main() {
+    local src=$(pwd) \
+          stage=
+
+    case $TRAVIS_OS_NAME in
+        linux)
+            stage=$(mktemp -d)
+            ;;
+        osx)
+            stage=$(mktemp -d -t tmp)
+            ;;
+    esac
+
+    test -f Cargo.lock || cargo generate-lockfile
+
+    # TODO Update this to build the artifacts that matter to you
+    cross rustc --bin jormungandr --target $TARGET --release -- -C lto
+    cross rustc --bin jcli        --target $TARGET --release -- -C lto
+
+    cp target/$TARGET/release/jormungandr $stage/
+    cp target/$TARGET/release/jcli $stage/
+
+    cd $stage
+    tar czf $src/$CRATE_NAME-$TRAVIS_TAG-$TARGET.tar.gz *
+    cd $src
+
+    rm -rf $stage
+}
+
+main
