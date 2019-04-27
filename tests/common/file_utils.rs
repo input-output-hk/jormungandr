3fa45f33edca3ddf03bb452772fd84d0df825398
dkijania <dariusz.kijania@iohk.io>
2019-04-27 12:58:08 +0200

    Removing not used read_file method
    Signed-off-by: dkijania <dariusz.kijania@iohk.io>

--- tests/common/file_utils.rs
+++ tests/common/file_utils.rs
@@ -28,19 +28,6 @@ pub fn create_empty_file_in_temp(file_name: &str) -> PathBuf {
     path
 }
 
-/// Gets path in temp directory (does not create it)
-///
-/// # Arguments
-///
-/// * `file_path` - A string slice that holds the path
-/// that will be glued to temp directory path
-///
-/// # Example
-///
-/// use file_utils::get_path_in_temp;
-/// let path_in_temp = "test.txt";
-/// get_path_in_temp(&path_in_temp);
-///
 pub fn get_temp_folder() -> PathBuf {
     let temp_dir = mktemp::Temp::new_dir().unwrap();
     let path = temp_dir.to_path_buf();
@@ -62,11 +49,4 @@ pub fn create_file_with_content(path: &PathBuf, content: &str) -> () {
     let mut file = File::create(&path).unwrap();
     file.write_all(content.as_bytes())
         .expect(&format!("cannot write to file {:?}", path));
-}
-
-pub fn read_file(path: &PathBuf) -> String {
-    let mut file = File::create(&path).unwrap();
-    let mut buffer = String::new();
-    std::fs::read_to_string(&mut buffer);
-    buffer
 }
