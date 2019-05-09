ada3751c78f1a6b51c6770af59410116926f0e9b
dkijania <dariusz.kijania@iohk.io>
2019-05-09 07:23:04 +0200

    unit tests for new & staging commands

--- src/bin/jcli_app/utils/io.rs
+++ src/bin/jcli_app/utils/io.rs
@@ -1,4 +1,7 @@
 use std::path::Path;
+use std::path::PathBuf;
+
+extern crate mktemp;
 
 /// open the given file path as a writable stream, or stdout if no path
 /// provided
@@ -34,4 +37,19 @@ pub fn open_file_read<P: AsRef<Path>>(path: &Option<P>) -> Box<dyn std::io::BufR
     } else {
         Box::new(std::io::BufReader::new(std::io::stdin()))
     }
+}
+
+/// get path to unique file in temp folder
+pub fn get_path_in_temp<T: Into<String>>(file_path: T) -> Result<PathBuf, std::io::Error> {
+    let mut path = get_temp_folder().unwrap();
+    path.push(file_path.into());
+    Ok(path)
+}
+
+/// get os temp folder
+pub fn get_temp_folder() -> Result<PathBuf, std::io::Error> {
+    let temp_dir = mktemp::Temp::new_dir().unwrap();
+    let path = temp_dir.to_path_buf();
+    temp_dir.release();
+    Ok(path)
 }
