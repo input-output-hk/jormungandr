8e18bf02a0d9800c50e2ab6961a31ea75fee98ce
Eelco Dolstra <edolstra@gmail.com>
2019-05-20 19:37:46 +0200

    Add some code for testing genesis leadership selection

--- chain-impl-mockchain/src/leadership/genesis/mod.rs
+++ chain-impl-mockchain/src/leadership/genesis/mod.rs
@@ -136,6 +136,130 @@ impl GenesisLeaderSelection {
     }
 }
 
+#[cfg(test)]
+mod test {
+
+    use super::*;
+    use crate::ledger::test::create_initial_fake_ledger;
+    use crate::milli::Milli;
+    use crate::stake::PoolStakeDistribution;
+    use crate::stake::StakePoolInfo;
+    use chain_addr::Discrimination;
+    use chain_time::{
+        era::{Epoch, TimeEra},
+        timeframe::Slot,
+    };
+    use std::collections::HashMap;
+
+    fn make_pool(ledger: &mut Ledger) -> (StakePoolId, SecretKey<Curve25519_2HashDH>) {
+        let mut rng = rand::thread_rng();
+
+        let pool_vrf_private_key = SecretKey::generate(&mut rng);
+
+        let pool_info = StakePoolInfo {
+            serial: 1234,
+            owners: vec![],
+            initial_key: GenesisPraosLeader {
+                vrf_public_key: pool_vrf_private_key.to_public(),
+                kes_public_key: SecretKey::generate(&mut rng).to_public(),
+            },
+        };
+
+        let pool_id = pool_info.to_id();
+
+        ledger.delegation.register_stake_pool(pool_info).unwrap();
+
+        (pool_id, pool_vrf_private_key)
+    }
+
+    #[test]
+    pub fn test_phi() {
+        let slots_per_epoch = 200000;
+
+        let (_genesis_hash, mut ledger) = create_initial_fake_ledger(
+            Discrimination::Test,
+            &vec![],
+            slots_per_epoch,
+            Milli::from_millis(100),
+        );
+
+        let mut pools = HashMap::<StakePoolId, (SecretKey<Curve25519_2HashDH>, u64, Value)>::new();
+
+        let (big_pool_id, big_pool_vrf_private_key) = make_pool(&mut ledger);
+        pools.insert(
+            big_pool_id.clone(),
+            (big_pool_vrf_private_key, 0, Value(1000)),
+        );
+
+        for _i in 0..10 {
+            let (small_pool_id, small_pool_vrf_private_key) = make_pool(&mut ledger);
+            pools.insert(
+                small_pool_id.clone(),
+                (small_pool_vrf_private_key, 0, Value(100)),
+            );
+        }
+
+        let mut selection = GenesisLeaderSelection::new(0, &ledger);
+
+        for (pool_id, (_, _, value)) in &pools {
+            selection.distribution.0.insert(
+                pool_id.clone(),
+                PoolStakeDistribution {
+                    total_stake: *value,
+                    member_stake: HashMap::new(),
+                },
+            );
+        }
+
+        let era = TimeEra::new(Slot(0), Epoch(0), slots_per_epoch);
+
+        let mut date = BlockDate::first();
+
+        let mut empty_slots = 0;
+
+        let mut times_selected_small = 0;
+
+        for _i in 0..200 {
+            let mut any_found = false;
+            let mut any_small = false;
+            for (pool_id, (pool_vrf_private_key, times_selected, value)) in pools.iter_mut() {
+                match selection
+                    .leader(&pool_id, &pool_vrf_private_key, date)
+                    .unwrap()
+                {
+                    None => {}
+                    Some(_witness) => {
+                        any_found = true;
+                        *times_selected += 1;
+                        if value.0 == 100 {
+                            any_small = true;
+                        }
+                    }
+                }
+            }
+            if !any_found {
+                empty_slots += 1;
+            }
+            if any_small {
+                times_selected_small += 1;
+            }
+            date = date.next(&era);
+        }
+
+        /*
+        println!("empty slots = {}", empty_slots);
+        for (pool_id, (_pool_vrf_private_key, times_selected, stake)) in pools.iter_mut() {
+            println!(
+                "pool id={} stake={} slots={}",
+                pool_id, stake.0, times_selected
+            );
+        }
+        println!("slots small = {}", times_selected_small);
+        */
+    }
+
+}
+
 /*
 #[cfg(test)]
 mod test {
