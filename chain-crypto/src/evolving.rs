68ebdb560ce236bf12c50bfcf76e772c117fa12e
Vincent Hanquez <vincent@typed.io>
2019-05-29 14:18:27 +0100

    Add KeyEvolvingAlgorithm

---
+++ chain-crypto/src/evolving.rs
@@ -0,0 +1,29 @@
+use crate::key::{AsymmetricKey, SecretKey};
+
+/// Evolving status
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum EvolvingStatus {
+    Success,
+    Failed,
+}
+
+pub trait KeyEvolvingAlgorithm: AsymmetricKey {
+    /// Get the period associated with this signature
+    fn get_period(key: &Self::Secret) -> usize;
+
+    /// Update the secret key to the next period
+    ///
+    /// if EvolvingStatus::Failed is returned, then the key couldn't be updated
+    fn update(key: &mut Self::Secret) -> EvolvingStatus;
+}
+
+impl<A: KeyEvolvingAlgorithm> SecretKey<A> {
+    /// Evolve the secret key to the next period
+    pub fn evolve(key: &mut Self) -> EvolvingStatus {
+        A::update(&mut key.0)
+    }
+    /// Get the period associated with the current instance of the key
+    pub fn get_period(key: &Self) -> usize {
+        A::get_period(&key.0)
+    }
+}
