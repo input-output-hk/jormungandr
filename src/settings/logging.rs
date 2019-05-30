6f27c62567638fff36c9c12ad2c19c320767e27b
CodeSandwich <igor.zuk@protonmail.com>
2019-05-30 16:55:27 +0200

    Add logging to Syslog and Journald

--- src/settings/logging.rs
+++ src/settings/logging.rs
@@ -1,4 +1,11 @@
+use crate::log::{AsyncableDrain, JsonDrain};
 use slog::{Drain, Logger};
+use slog_async::Async;
+#[cfg(feature = "systemd")]
+use slog_journald::JournaldDrain;
+#[cfg(unix)]
+use slog_syslog::Facility;
+use std::io;
 use std::str::FromStr;
 
 #[derive(Debug)]
@@ -56,22 +63,43 @@ impl FromStr for LogOutput {
 
 impl LogSettings {
     pub fn to_logger(&self) -> Result<Logger, Error> {
-        let drain = match self.format {
-            LogFormat::Plain => {
-                let decorator = slog_term::TermDecorator::new().build();
-                let drain = slog_term::FullFormat::new(decorator).build().fuse();
-                slog_async::Async::new(drain).build().fuse()
-            }
-            LogFormat::Json => {
-                let drain = slog_json::Json::default(std::io::stderr()).fuse();
-                slog_async::Async::new(drain).build().fuse()
-            }
-        };
+        let drain = self.output.to_logger(&self.format)?.fuse();
         let drain = slog::LevelFilter::new(drain, self.verbosity).fuse();
         Ok(slog::Logger::root(drain, o!()))
     }
 }
 
+impl LogOutput {
+    fn to_logger(&self, format: &LogFormat) -> Result<Async, Error> {
+        Ok(match self {
+            LogOutput::Stderr => format.decorate_stderr(),
+            #[cfg(unix)]
+            LogOutput::Syslog => format.decorate(slog_syslog::unix_3164(Facility::LOG_USER)?),
+            #[cfg(feature = "systemd")]
+            LogOutput::Journald => format.decorate(JournaldDrain),
+        })
+    }
+}
+
+impl LogFormat {
+    fn decorate_stderr(&self) -> Async {
+        match self {
+            LogFormat::Plain => slog_term::term_full().async(),
+            LogFormat::Json => slog_json::Json::default(io::stderr()).async(),
+        }
+    }
+
+    fn decorate<D: AsyncableDrain>(&self, drain: D) -> Async
+    where
+        <D as Drain>::Err: std::fmt::Debug,
+    {
+        match self {
+            LogFormat::Plain => drain.async(),
+            LogFormat::Json => JsonDrain::new(drain).async(),
+        }
+    }
+}
+
 custom_error! {pub Error
-    ImpossibleError {} = @{{ unreachable!(); "" }} // Custom_error requires at least 1 variant
+    SyslogAccessFailed { source: io::Error } = "syslog access failed",
 }
