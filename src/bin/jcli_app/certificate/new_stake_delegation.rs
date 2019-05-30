02e2908012cf4bd0f26bfc23c987f408c958f302
Nicolas Di Prima <nicolas.diprima@iohk.io>
2019-05-30 13:13:32 +0100

    update the comment in the `jcli certificate` to make it less confusing

--- src/bin/jcli_app/certificate/new_stake_delegation.rs
+++ src/bin/jcli_app/certificate/new_stake_delegation.rs
@@ -12,8 +12,8 @@ pub struct StakeDelegation {
     /// the stake pool id
     #[structopt(name = "STAKE_POOL_ID", parse(try_from_str))]
     pub pool_id: chain_crypto::Blake2b256,
-    /// the delegation key
-    #[structopt(name = "DELEGATION_ID", parse(try_from_str = "parse_pub_key"))]
+    /// the public key used in the stake key registration
+    #[structopt(name = "STAKE_KEY", parse(try_from_str = "parse_pub_key"))]
     pub stake_id: PublicKey<Ed25519Extended>,
     /// print the output signed certificate in the given file, if no file given
     /// the output will be printed in the standard output
