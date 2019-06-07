43ddfea9478d9a2260fee117dfe0a7c25a0e82ed
Stewart Mackenzie <sjm@fractalide.com>
2019-06-07 19:00:52 +0800

    Remove CLI_NAME customization.
    The implications are using documentation that doesn't
    match the newly named cli rename. This becomes untenable.

--- tests/common/configuration/mod.rs
+++ tests/common/configuration/mod.rs
@@ -33,9 +33,8 @@ pub fn get_jormungandr_app() -> PathBuf {
 
 /// Get jcli executable from current environment
 pub fn get_jcli_app() -> PathBuf {
-    const JOR_CLI_NAME: &'static str = env!("JOR_CLI_NAME");
     let mut path = get_working_directory();
-    path.push(JOR_CLI_NAME);
+    path.push("jcli");
     if cfg!(windows) {
         path.set_extension("exe");
     }
