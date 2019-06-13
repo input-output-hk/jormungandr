5b1a91f060316a9083619e9aaf7d3877ca560dae
Nicolas Di Prima <nicolas.diprima@iohk.io>
2019-06-13 08:44:04 +0200

    fix deploy script

--- ci/before_deploy.sh
+++ ci/before_deploy.sh
@@ -18,8 +18,8 @@ main() {
     test -f Cargo.lock || cargo generate-lockfile
 
     # TODO Update this to build the artifacts that matter to you
-    cross rustc --bin jormungandr --target $TARGET --release -- -C lto
-    cross rustc --bin jcli        --target $TARGET --release -- -C lto
+    cross rustc -p jormungandr --target $TARGET --release -- -C lto
+    cross rustc -p jcli        --target $TARGET --release -- -C lto
 
     cp target/$TARGET/release/jormungandr $stage/
     cp target/$TARGET/release/jcli $stage/
