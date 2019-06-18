31a62a8a8eec3b0a2bd8f0a5eec6360e1a8c18c5
Nicolas Di Prima <nicolas@primetype.co.uk>
2019-06-18 21:23:29 +0200

    Revert "New corner cases for transaction module"

--- jormungandr-integration-tests/src/jcli/transaction/mod.rs
+++ jormungandr-integration-tests/src/jcli/transaction/mod.rs
@@ -1,9 +1,3 @@
-pub mod add_account;
 pub mod e2e;
 pub mod finalize;
-pub mod id;
 pub mod input;
-pub mod new;
-pub mod output;
-pub mod seal;
-pub mod witness;
--- jormungandr-integration-tests/src/jcli/transaction/new.rs
+++ jormungandr-integration-tests/src/jcli/transaction/new.rs
@@ -1,12 +1,3 @@
-#![cfg(feature = "integration-test")]
-
-use common::file_utils;
-use common::jcli_wrapper::jcli_transaction_wrapper::JCLITransactionWrapper;
-
-const FAKE_GENESIS_HASH: &str = "19c9852ca0a68f15d0f7de5d1a26acd67a3a3251640c6066bdb91d22e2000193";
-const FAKE_INPUT_TRANSACTION_ID: &str =
-    "19c9852ca0a68f15d0f7de5d1a26acd67a3a3251640c6066bdb91d22e2000193";
-
 #[test]
 #[cfg(not(target_os = "linux"))]
 pub fn test_cannot_create_input_when_staging_file_is_readonly() {
