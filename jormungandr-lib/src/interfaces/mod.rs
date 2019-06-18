d99befd6a5a8bd304573f05f45c0c6fd4c00a12c
Nicolas Di Prima <nicolas.diprima@iohk.io>
2019-06-18 16:51:00 +0200

    add address wraping

--- jormungandr-lib/src/interfaces/mod.rs
+++ jormungandr-lib/src/interfaces/mod.rs
@@ -1,7 +1,9 @@
+mod address;
 mod blockdate;
 mod fragment_log;
 mod value;
 
+pub use self::address::Address;
 pub use self::blockdate::BlockDate;
 pub use self::fragment_log::{FragmentLog, FragmentOrigin, FragmentStatus};
 pub use self::value::Value;
