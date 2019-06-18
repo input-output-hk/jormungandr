cc6a655bfd004fabc6689cd3c3acf365b1058ded
Nicolas Di Prima <nicolas.diprima@iohk.io>
2019-06-18 16:50:43 +0200

    add value wrapping

--- jormungandr-lib/src/interfaces/mod.rs
+++ jormungandr-lib/src/interfaces/mod.rs
@@ -1,5 +1,7 @@
 mod blockdate;
 mod fragment_log;
+mod value;
 
 pub use self::blockdate::BlockDate;
 pub use self::fragment_log::{FragmentLog, FragmentOrigin, FragmentStatus};
+pub use self::value::Value;
