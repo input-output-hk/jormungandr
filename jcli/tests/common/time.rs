2d70073cfdc5ba1e68ff66445bcda6855f5cc473
dkijania <dariusz.kijania@iohk.io>
2019-06-24 08:45:55 +0200

    Fix build
    Signed-off-by: dkijania <dariusz.kijania@iohk.io>

--- jcli/tests/common/time.rs
+++ jcli/tests/common/time.rs
@@ -2,7 +2,7 @@
 
 use std::time::SystemTime;
 
-pub fn get_current_time_epoch() {
+pub fn get_current_time_epoch() -> u64 {
     SystemTime::now()
         .duration_since(std::time::SystemTime::UNIX_EPOCH)
         .unwrap_or_default()
