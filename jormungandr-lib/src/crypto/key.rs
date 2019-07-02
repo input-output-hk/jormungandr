dedceab3a590343eea9155e5788bfeee3caa17c3
Nicolas Di Prima <nicolas.diprima@iohk.io>
2019-07-02 09:28:57 +0200

    display is in bech32 by default now
    this will make the API more consistent
    
    for hexadecimal display, there is the to_hex and from_hex functions

--- jormungandr-lib/src/crypto/key.rs
+++ jormungandr-lib/src/crypto/key.rs
@@ -289,14 +289,14 @@ impl<T, A: VerificationAlgorithm> Signature<T, A> {
 
 impl<A: AsymmetricPublicKey> fmt::Display for Identifier<A> {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        self.0.fmt(f)
+        self.to_bech32_str().fmt(f)
     }
 }
 
 impl<A: AsymmetricPublicKey> FromStr for Identifier<A> {
-    type Err = chain_crypto::PublicKeyFromStrError;
+    type Err = chain_crypto::bech32::Error;
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-        PublicKey::from_str(s).map(Identifier::from)
+        Self::from_bech32_str(s)
     }
 }
 
@@ -503,7 +503,7 @@ mod test {
     #[test]
     fn identifier_display() {
         const EXPECTED_IDENTIFIER_STR: &'static str =
-            "2020202020202020202020202020202020202020202020202020202020202020";
+            "ed25519_pk1yqszqgpqyqszqgpqyqszqgpqyqszqgpqyqszqgpqyqszqgpqyqsqyl7vm8";
         const IDENTIFIER_BYTES: [u8; 32] = [0x20; 32];
 
         let identifier: Identifier<Ed25519> =
