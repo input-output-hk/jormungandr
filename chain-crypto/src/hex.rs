e85479858a8b8f5c79db17d0dd2889346577ada2
Vincent Hanquez <vincent.hanquez@iohk.io>
2019-07-16 21:00:16 +0100

    replace ... by ..= in hex

--- chain-crypto/src/hex.rs
+++ chain-crypto/src/hex.rs
@@ -56,9 +56,9 @@ fn decode_bytes(input: &[u8]) -> Result<Vec<u8>, DecodeError> {
         buf <<= 4;
 
         match byte {
-            b'A'...b'F' => buf |= byte - b'A' + 10,
-            b'a'...b'f' => buf |= byte - b'a' + 10,
-            b'0'...b'9' => buf |= byte - b'0',
+            b'A'..=b'F' => buf |= byte - b'A' + 10,
+            b'a'..=b'f' => buf |= byte - b'a' + 10,
+            b'0'..=b'9' => buf |= byte - b'0',
             b' ' | b'\r' | b'\n' | b'\t' => {
                 buf >>= 4;
                 continue;
