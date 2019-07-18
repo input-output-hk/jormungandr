08371919189b2f4ff3068e684e5ee754051b4ae0
Vincent Hanquez <vincent.hanquez@iohk.io>
2019-07-18 22:01:34 +0100

    Add a new check module for ledger checking helpers

---
+++ chain-impl-mockchain/src/ledger/check.rs
@@ -0,0 +1,18 @@
+
+use super::Error;
+use crate::certificate;
+use crate::transaction::*;
+use chain_addr::Address;
+
+/// check that the transaction input/outputs/witnesses is valid for stake_owner_delegation
+pub(super) fn valid_stake_owner_delegation_transaction(
+    auth_cert: &AuthenticatedTransaction<Address, certificate::OwnerStakeDelegation>,
+) -> Result<(), Error> {
+    if auth_cert.transaction.inputs.len() != 1
+        || auth_cert.witnesses.len() != 1
+        || auth_cert.transaction.outputs.len() != 0
+    {
+        return Err(Error::OwnerStakeDelegationInvalidTransaction);
+    }
+    Ok(())
+}
--- chain-impl-mockchain/src/ledger/mod.rs
+++ chain-impl-mockchain/src/ledger/mod.rs
@@ -1,3 +1,4 @@
+mod check;
 pub mod ledger;
 
 pub use ledger::*;
