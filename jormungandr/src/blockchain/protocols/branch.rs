8a516a424f69acdc587189b7c29803650d2c398f
Mikhail Zabaluev <mikhail.zabaluev@iohk.io>
2019-07-24 16:15:44 +0200

    Made locking and inner data of Branch private
    In blockchain::protocols::branch:
    
    Don't expose BranchData outside of the implementation module.
    All public methods will be on Branch and will do locking asyncrhonously.

--- jormungandr/src/blockchain/protocols/branch.rs
+++ jormungandr/src/blockchain/protocols/branch.rs
@@ -1,8 +1,5 @@
 use crate::blockchain::protocols::Ref;
-use tokio::{
-    prelude::*,
-    sync::lock::{Lock, LockGuard},
-};
+use tokio::{prelude::*, sync::lock::Lock};
 
 #[derive(Clone)]
 pub struct Branches {
@@ -15,7 +12,7 @@ pub struct Branch {
 }
 
 /// the data that is contained in a branch
-pub struct BranchData {
+struct BranchData {
     /// reference to the block where the branch points to
     reference: Ref,
 
@@ -41,23 +38,15 @@ impl Branch {
         }
     }
 
-    /// try to acquire a lock on the given Branch in an async way.
-    ///
-    #[inline]
-    pub fn poll_lock<E>(&mut self) -> Poll<LockGuard<BranchData>, E> {
-        Ok(self.inner.poll_lock())
-    }
-
     pub fn get_ref(&self) -> impl Future<Item = Ref, Error = std::convert::Infallible> {
         let mut branch = self.clone();
-        future::poll_fn(move || branch.poll_lock()).map(|guard| guard.reference().clone())
+        future::poll_fn(move || Ok(branch.inner.poll_lock())).map(|guard| guard.reference().clone())
     }
 }
 
 impl BranchData {
     /// create the branch data with the current `last_updated` to
     /// the current time this function was called
-    #[inline(always)]
     fn new(reference: Ref) -> Self {
         BranchData {
             reference,
@@ -65,16 +54,14 @@ impl BranchData {
         }
     }
 
-    #[inline(always)]
-    pub fn update(&mut self, reference: Ref) -> Ref {
+    fn update(&mut self, reference: Ref) -> Ref {
         let old_reference = std::mem::replace(&mut self.reference, reference);
         self.last_updated = std::time::SystemTime::now();
 
         old_reference
     }
 
-    #[inline(always)]
-    pub fn reference(&self) -> &Ref {
+    fn reference(&self) -> &Ref {
         &self.reference
     }
 }
