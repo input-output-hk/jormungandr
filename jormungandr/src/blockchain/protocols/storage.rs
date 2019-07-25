703e1c2a983b2687e1e03d33cb9709bdf98a273a
Nicolas Di Prima <nicolas.diprima@iohk.io>
2019-07-25 10:52:23 +0200

    add stream over block of the storage

--- jormungandr/src/blockchain/protocols/storage.rs
+++ jormungandr/src/blockchain/protocols/storage.rs
@@ -2,13 +2,24 @@ use crate::{
     blockcfg::{Block, HeaderHash},
     start_up::NodeStorage,
 };
-use chain_storage::error::Error as StorageError;
+use chain_storage::{
+    error::Error as StorageError,
+    store::{for_path_to_nth_ancestor, BlockInfo},
+};
+use std::ops::Deref as _;
 use tokio::prelude::*;
 use tokio::sync::lock::Lock;
 
 #[derive(Clone)]
 pub struct Storage {
     inner: Lock<NodeStorage>,
+}
+
+pub struct BlockStream {
+    lock: Lock<NodeStorage>,
+    to_depth: u64,
+    cur_depth: u64,
+    pending_infos: Vec<BlockInfo<HeaderHash>>,
 }
 
 impl Storage {
@@ -87,5 +98,69 @@ impl Storage {
                 Ok(()) => future::ok(()),
             }
         })
+    }
+
+    pub fn stream_from_to(
+        &self,
+        from: HeaderHash,
+        to: HeaderHash,
+    ) -> impl Future<Item = Option<BlockStream>, Error = StorageError> {
+        let mut inner = self.inner.clone();
+        let inner_2 = self.inner.clone();
+
+        future::poll_fn(move || Ok(inner.poll_lock())).and_then(move |store| {
+            match store.is_ancestor(&from, &to) {
+                Err(error) => future::err(error),
+                Ok(None) => future::ok(None),
+                Ok(Some(distance)) => match store.get_block_info(&to) {
+                    Err(error) => future::err(error),
+                    Ok(to_info) => future::ok(Some(BlockStream {
+                        lock: inner_2,
+                        to_depth: to_info.depth,
+                        cur_depth: to_info.depth - distance,
+                        pending_infos: vec![to_info],
+                    })),
+                },
+            }
+        })
+    }
+}
+
+impl Stream for BlockStream {
+    type Item = BlockInfo<HeaderHash>;
+    type Error = StorageError;
+
+    fn poll(&mut self) -> Poll<Option<Self::Item>, Self::Error> {
+        if self.cur_depth >= self.to_depth {
+            return Ok(Async::Ready(None));
+        }
+
+        let guard = try_ready!(Ok(self.lock.poll_lock()));
+
+        self.cur_depth += 1;
+
+        let block_info = self.pending_infos.pop().unwrap();
+
+        if block_info.depth == self.cur_depth {
+            // We've seen this block on a previous ancestor traversal.
+            Ok(Async::Ready(Some(block_info)))
+        } else {
+            // We don't have this block yet, so search back from
+            // the furthest block that we do have.
+            assert!(self.cur_depth < block_info.depth);
+            let depth = block_info.depth;
+            let parent = block_info.parent_id();
+            self.pending_infos.push(block_info);
+            let block_info = for_path_to_nth_ancestor(
+                guard.deref().deref(),
+                &parent,
+                depth - self.cur_depth - 1,
+                |new_info| {
+                    self.pending_infos.push(new_info.clone());
+                },
+            )?;
+
+            Ok(Async::Ready(Some(block_info)))
+        }
     }
 }
