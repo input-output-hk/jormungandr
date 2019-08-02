877a65e8d678d6322329bfc328be2e2d6815eb1f
Vincent Hanquez <vincent.hanquez@iohk.io>
2019-08-02 18:08:46 +0100

    add convertion of DurationSeconds and export

--- chain-time/src/lib.rs
+++ chain-time/src/lib.rs
@@ -1,7 +1,9 @@
 pub mod era;
 pub mod timeframe;
 pub mod timeline;
+pub mod units;
 
 pub use era::{Epoch, TimeEra};
 pub use timeframe::{Slot, SlotDuration, TimeFrame};
-pub use timeline::Timeline;
+pub use timeline::{TimeOffsetSeconds, Timeline};
+pub use units::DurationSeconds;
