5e92629418cdfae389a4a216f3e90d51e8e80989
Mikhail Zabaluev <mikhail.zabaluev@iohk.io>
2019-08-02 12:30:48 +0300

    Error type for new bootstrap
    Provide an error type for network::protocols::bootstrap,
    enumerating all failure cases and providing conventional error reporting
    facilities facilities for them.

--- jormungandr/src/network/protocols/bootstrap.rs
+++ jormungandr/src/network/protocols/bootstrap.rs
@@ -1,38 +1,83 @@
 use super::super::{grpc, BlockConfig};
-use crate::blockcfg::Block;
-use crate::blockchain::protocols::{Blockchain, PreCheckedHeader, Ref};
+use crate::blockcfg::{Block, HeaderHash};
+use crate::blockchain::protocols::{Blockchain, Error as BlockchainError, PreCheckedHeader, Ref};
 use crate::settings::start::network::Peer;
 use chain_core::property::HasHeader;
 use network_core::client::{block::BlockService, Client as _};
+use network_core::error::Error as NetworkError;
 use network_grpc::client::Connection;
 use slog::Logger;
-use std::fmt::Debug;
 use tokio::prelude::*;
 use tokio::runtime::current_thread;
+
+use std::error;
+use std::fmt::{self, Debug, Display};
+
+type ConnectError = network_grpc::client::ConnectError<std::io::Error>;
+
+#[derive(Debug)]
+pub enum Error {
+    Connect(ConnectError),
+    ClientNotReady(NetworkError),
+    PullRequestFailed(NetworkError),
+    PullStreamFailed(NetworkError),
+    HeaderCheckFailed(BlockchainError),
+    BlockAlreadyPresent(HeaderHash),
+    BlockMissingParent(HeaderHash),
+    ApplyBlockFailed(BlockchainError),
+}
+
+impl Display for Error {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        use self::Error::*;
+        match self {
+            Connect(_) => write!(f, "failed to connect to bootstrap peer"),
+            ClientNotReady(_) => write!(f, "connection broken"),
+            PullRequestFailed(_) => write!(f, "bootstrap pull request failed"),
+            PullStreamFailed(_) => write!(f, "bootstrap pull stream failed"),
+            HeaderCheckFailed(_) => write!(f, "block header check failed"),
+            BlockAlreadyPresent(hash) => write!(f, "received block {} is already present", hash),
+            BlockMissingParent(hash) => write!(
+                f,
+                "received block {} is not connected to the block chain",
+                hash
+            ),
+            ApplyBlockFailed(_) => write!(f, "failed to apply block to the blockchain"),
+        }
+    }
+}
+
+impl error::Error for Error {
+    fn source(&self) -> Option<&(dyn error::Error + 'static)> {
+        use self::Error::*;
+        match self {
+            Connect(e) => Some(e),
+            ClientNotReady(e) => Some(e),
+            PullRequestFailed(e) => Some(e),
+            PullStreamFailed(e) => Some(e),
+            HeaderCheckFailed(e) => Some(e),
+            BlockAlreadyPresent(_) => None,
+            BlockMissingParent(_) => None,
+            ApplyBlockFailed(e) => Some(e),
+        }
+    }
+}
 
 pub fn bootstrap_from_peer(
     peer: Peer,
     blockchain: Blockchain,
     tip: Ref,
     logger: &Logger,
-) -> Result<Ref, ()> {
+) -> Result<Ref, Error> {
     info!(logger, "connecting to bootstrap peer {}", peer.connection);
     let bootstrap = grpc::connect(peer.address(), None)
-        .map_err(|e| {
-            error!(logger, "failed to connect to bootstrap peer: {:?}", e);
-        })
-        .and_then(|client: Connection<BlockConfig>| {
-            client.ready().map_err(|e| {
-                error!(logger, "bootstrap peer disconnected: {:?}", e);
-            })
-        })
+        .map_err(Error::Connect)
+        .and_then(|client: Connection<BlockConfig>| client.ready().map_err(Error::ClientNotReady))
         .and_then(|mut client| {
             let tip_hash = *tip.hash();
             client
                 .pull_blocks_to_tip(&[tip_hash])
-                .map_err(|e| {
-                    error!(logger, "PullBlocksToTip request failed: {:?}", e);
-                })
+                .map_err(Error::PullRequestFailed)
                 .and_then(|stream| bootstrap_from_stream(blockchain, tip, stream, logger.clone()))
         });
 
@@ -47,16 +92,14 @@ fn bootstrap_from_stream<S>(
     tip: Ref,
     stream: S,
     logger: Logger,
-) -> impl Future<Item = Ref, Error = ()>
+) -> impl Future<Item = Ref, Error = Error>
 where
-    S: Stream<Item = Block>,
+    S: Stream<Item = Block, Error = NetworkError>,
     S::Error: Debug,
 {
     let fold_logger = logger.clone();
     stream
-        .map_err(move |e| {
-            error!(logger, "bootstrap block streaming failed: {:?}", e);
-        })
+        .map_err(Error::PullStreamFailed)
         .fold(tip, move |tip, block| {
             handle_block(blockchain.clone(), block, fold_logger.clone())
         })
@@ -66,44 +109,33 @@ fn handle_block(
     mut blockchain: Blockchain,
     block: Block,
     logger: Logger,
-) -> impl Future<Item = Ref, Error = ()> {
+) -> impl Future<Item = Ref, Error = Error> {
     let header = block.header();
     debug!(
         logger,
         "received block from the bootstrap node: {:#?}", header
     );
-    let err1_logger = logger.clone();
-    let err2_logger = logger.clone();
-    let err3_logger = logger.clone();
     let mut end_blockchain = blockchain.clone();
     blockchain
         .pre_check_header(header)
-        .map_err(move |e| {
-            warn!(err1_logger, "header pre-check failed: {:?}", e);
-        })
-        .and_then(move |pre_checked| match pre_checked {
+        .map_err(Error::HeaderCheckFailed)
+        .and_then(|pre_checked| match pre_checked {
             PreCheckedHeader::AlreadyPresent { header, .. } => {
-                warn!(logger, "block {} is already present", header.hash());
-                Err(())
+                Err(Error::BlockAlreadyPresent(header.hash()))
             }
             PreCheckedHeader::MissingParent { header, .. } => {
-                warn!(logger, "received a disconnected block {}", header.hash());
-                Err(())
+                Err(Error::BlockMissingParent(header.hash()))
             }
             PreCheckedHeader::HeaderWithCache { header, parent_ref } => Ok((header, parent_ref)),
         })
         .and_then(move |(header, parent_ref)| {
             blockchain
                 .post_check_header(header, parent_ref)
-                .map_err(move |e| {
-                    warn!(err2_logger, "header post-check failed: {:?}", e);
-                })
+                .map_err(Error::HeaderCheckFailed)
         })
         .and_then(move |post_checked| {
             end_blockchain
                 .apply_block(post_checked, block)
-                .map_err(move |e| {
-                    error!(err3_logger, "failed to apply block to storage: {:?}", e);
-                })
+                .map_err(Error::ApplyBlockFailed)
         })
 }
