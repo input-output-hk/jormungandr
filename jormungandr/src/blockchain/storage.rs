c0acb902a3260c3311d5eff48457a2d0f4835d65
Nicolas Di Prima <nicolas.diprima@iohk.io>
2019-08-09 19:17:35 +0100

    allow temporary access to the underlying non thread safe storage API

--- jormungandr/src/blockchain/storage.rs
+++ jormungandr/src/blockchain/storage.rs
@@ -8,7 +8,7 @@ use chain_storage::{
 };
 use std::ops::Deref as _;
 use tokio::prelude::*;
-use tokio::sync::lock::Lock;
+use tokio::sync::lock::{Lock, LockGuard};
 
 #[derive(Clone)]
 pub struct Storage {
@@ -27,6 +27,13 @@ impl Storage {
         Storage {
             inner: Lock::new(storage),
         }
+    }
+
+    #[deprecated(since = "new blockchain API", note = "use the stream iterator instead")]
+    pub fn get_inner(&self) -> impl Future<Item = LockGuard<NodeStorage>, Error = StorageError> {
+        let mut inner = self.inner.clone();
+
+        future::poll_fn(move || Ok(inner.poll_lock()))
     }
 
     pub fn get_tag(
