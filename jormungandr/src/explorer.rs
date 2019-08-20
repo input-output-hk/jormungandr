4d4b794b7856ca734680881a23f057cb37e5ba2e
Enzo Cioppettini <ecioppettini@atixlabs.com>
2019-08-20 15:05:59 -0300

    store Ref instead of blocks

--- jormungandr/src/explorer.rs
+++ jormungandr/src/explorer.rs
@@ -1,4 +1,4 @@
-use super::blockchain::Blockchain;
+use super::blockchain::{Blockchain, Ref};
 use crate::blockcfg::Block;
 use crate::blockcfg::ChainLength;
 use crate::intercom::ExplorerMsg;
@@ -6,27 +6,21 @@ use crate::utils::task::{Input, ThreadServiceInfo};
 use chain_impl_mockchain::multiverse::{GCRoot, Multiverse};
 use futures::Future;
 use futures::IntoFuture;
+use std::collections::{HashMap, HashSet};
 
 use chain_core::property::Block as _;
 
 pub struct Process {
-    multiverse: Multiverse<State>,
-}
-
-struct State {
-    block: Block,
-}
-
-impl State {
-    pub fn new(block: Block) -> Self {
-        Self { block }
-    }
+    multiverse: Multiverse<Ref>,
+    // This is kind of the same thing the multiverse holds (with Ref instead of BlockId)
+    chain_length_to_hash: HashMap<ChainLength, Vec<Ref>>,
 }
 
 impl Process {
     pub fn new() -> Self {
         Self {
-            multiverse: Multiverse::<State>::new(),
+            multiverse: Multiverse::<Ref>::new(),
+            chain_length_to_hash: HashMap::new(),
         }
     }
 
@@ -45,42 +39,25 @@ impl Process {
         };
 
         match bquery {
-            ExplorerMsg::NewBlock(header) => {
-                //I don't really understand what's the purpose of the gcroot yet
-                let _gcroot: Result<GCRoot, ()> = blockchain
-                    .storage()
-                    .get(header.hash())
-                    .or_else(|storage_error| {
-                        error!(logger, "{}", storage_error);
-                        Err(())
-                    })
-                    .and_then(|block| {
-                        debug!(
-                            logger,
-                            "storing blockhash: {} with chain_length: {}",
-                            header.hash(),
-                            header.chain_length()
-                        );
-
-                        block
-                            .map(|some_block| self.store_block(some_block))
-                            .unwrap_or_else(|| Err(error!(logger, "Block is not in storage")))
-                    })
-                    .wait();
+            ExplorerMsg::NewBlock(new_block_ref) => {
+                let _gcroot = self.store_ref(new_block_ref);
             }
         };
     }
 
-    fn store_block(&mut self, block: Block) -> Result<GCRoot, ()> {
-        let header = &block.header;
-        let _gcroot =
-            self.multiverse
-                .insert(header.chain_length(), header.hash(), State::new(block));
+    fn store_ref(&mut self, new_block_ref: Ref) -> GCRoot {
+        let chain_length = new_block_ref.chain_length();
+        let header_hash = new_block_ref.hash();
 
-        Ok(_gcroot)
-    }
+        self.chain_length_to_hash
+            .entry(chain_length)
+            .or_insert(Vec::new())
+            .push(new_block_ref.clone());
 
-    fn _get_block_by_chain_length(_length: ChainLength) {
-        unimplemented!()
+        let gc_root = self
+            .multiverse
+            .insert(chain_length, header_hash, new_block_ref.clone());
+
+        gc_root
     }
 }
