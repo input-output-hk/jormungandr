7e1da466785cf3d9f53c3bc274d3dbafc6f58ae5
Enzo Cioppettini <ecioppettini@atixlabs.com>
2019-08-29 15:45:47 -0300

    add transaction type to graph

--- jormungandr/src/explorer/graphql/mod.rs
+++ jormungandr/src/explorer/graphql/mod.rs
@@ -1,6 +1,7 @@
 use crate::blockcfg::{self, FragmentId};
 use crate::blockchain::Blockchain;
 use chain_core::property::Block as _;
+use chain_core::property::Fragment;
 pub use juniper::http::GraphQLRequest;
 use juniper::EmptyMutation;
 use juniper::FieldResult;
@@ -17,6 +18,25 @@ struct Block {
     hash: String,
     /// Date the Block was included in the blockchain
     date: BlockDate,
+    /// The transactions contained in the block
+    transactions: Vec<Transaction>,
+}
+
+impl From<blockcfg::Block> for Block {
+    fn from(block: blockcfg::Block) -> Block {
+        Block {
+            hash: block.id().to_string(),
+            date: block.date().into(),
+            transactions: block
+                .contents
+                .iter()
+                .filter_map(|fragment| {
+                    //TODO: Implement
+                    None
+                })
+                .collect(),
+        }
+    }
 }
 
 #[derive(juniper::GraphQLObject)]
@@ -35,6 +55,37 @@ impl From<blockcfg::BlockDate> for BlockDate {
     }
 }
 
+#[derive(juniper::GraphQLObject)]
+// A transaction in the blockchain
+struct Transaction {
+    // The hash that identifies the transaction
+    id: String,
+    // The block this transaction is in
+    block: Block,
+    inputs: Vec<TransactionInput>,
+    outputs: Vec<TransactionOutput>,
+}
+
+#[derive(juniper::GraphQLObject)]
+struct TransactionInput {
+    amount: Lovelaces,
+    kind: TransactionInputKind,
+}
+
+#[derive(juniper::GraphQLObject)]
+struct TransactionOutput {
+    amount: Lovelaces,
+}
+
+#[derive(juniper::GraphQLEnum)]
+enum TransactionInputKind {
+    Utxo,
+    Account,
+}
+
+#[derive(juniper::GraphQLScalarValue)]
+struct Lovelaces(String);
+
 #[derive(juniper::GraphQLScalarValue)]
 struct Epoch(String);
 
@@ -47,20 +98,40 @@ pub struct Query;
     Context = Context,
 )]
 impl Query {
-    /// Query a Block from a Transaction hash
-    fn block(transaction: String, context: &Context) -> FieldResult<Option<Block>> {
+    fn block(id: String, context: &Context) -> FieldResult<Vec<Block>> {
+        unimplemented!();
+    }
+
+    fn transaction(id: String, context: &Context) -> FieldResult<Option<Transaction>> {
         // This call blocks the current thread (the call to wait), but it won't block the node's
         // thread, as queries are only executed in an exclusive runtime
-        let id = FragmentId::from_str(&transaction)?;
-        let block = context
+        let id = FragmentId::from_str(&id)?;
+        let block_option = context
             .db
             .find_block_by_transaction(id, context.blockchain.clone())
-            .wait()?
-            .map(|b| Block {
-                hash: b.id().to_string(),
-                date: b.date().into(),
-            });
-        Ok(block)
+            .wait()?;
+
+        let block = match block_option {
+            Some(b) => b,
+            None => return Ok(None),
+        };
+
+        let tx = block
+            .contents
+            .iter()
+            .find(|fragment| fragment.id() == id)
+            .map(|tx| tx.clone())
+            // FIXME: Maybe throw some error, although this shouldn't happen
+            .ok_or_else(|| unreachable!());
+
+        tx.map(|tx| Transaction {
+            id: format!("{}", id),
+            block: block.into(),
+            // TODO: Transform this things
+            inputs: Vec::new(),
+            outputs: Vec::new(),
+        })
+        .map(Some)
     }
 }
 
