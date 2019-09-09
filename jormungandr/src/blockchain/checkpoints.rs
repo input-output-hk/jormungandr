8066128183b558efcaa8a6650eea31aafa5404d5
Nicolas Di Prima <nicolas.diprima@iohk.io>
2019-09-09 15:36:14 +0100

    add `Checkpoints` a structure with appropriate algorithm to generate meaningful checkpoints
    useful for the network and other peers to have a useful list of points to catchup from

---
+++ jormungandr/src/blockchain/checkpoints.rs
@@ -0,0 +1,63 @@
+use crate::{blockcfg::HeaderHash, blockchain::Ref};
+use std::sync::Arc;
+
+/// list of pre-computed checkpoints from a given [`Ref`].
+///
+/// [`Ref`]: ./struct.Ref.html
+pub struct Checkpoints(Vec<HeaderHash>);
+
+impl Checkpoints {
+    /// create a new list of checkpoints from the given starting point (tip).
+    ///
+    /// The function make use of the `Ref` object pointing to previous epoch in order
+    /// to populate _interestingly_ spaced checkpoints.
+    ///
+    /// For now the algorithm provide the current block, the parent, the last block of the
+    /// previous epoch, the last block of the epoch before that... until the block0.
+    pub fn new_from(from: Arc<Ref>) -> Self {
+        let mut checkpoints = vec![from.hash(), from.block_parent_hash().clone()];
+
+        let mut ignore_prev = 0;
+        let mut current_ref = from;
+        while let Some(prev_epoch) = current_ref.last_ref_previous_epoch() {
+            current_ref = Arc::clone(prev_epoch);
+
+            for _ in 0..ignore_prev {
+                if let Some(prev_epoch) = current_ref.last_ref_previous_epoch() {
+                    current_ref = Arc::clone(&prev_epoch);
+                } else {
+                    break;
+                }
+            }
+
+            ignore_prev += 1;
+
+            checkpoints.push(current_ref.hash());
+        }
+
+        Checkpoints(checkpoints)
+    }
+
+    pub fn iter(&self) -> impl Iterator<Item = &HeaderHash> {
+        self.0.iter()
+    }
+
+    pub fn as_slice(&self) -> &[HeaderHash] {
+        self.0.as_slice()
+    }
+}
+
+impl AsRef<[HeaderHash]> for Checkpoints {
+    fn as_ref(&self) -> &[HeaderHash] {
+        self.as_slice()
+    }
+}
+
+impl IntoIterator for Checkpoints {
+    type Item = HeaderHash;
+    type IntoIter = ::std::vec::IntoIter<Self::Item>;
+
+    fn into_iter(self) -> Self::IntoIter {
+        self.0.into_iter()
+    }
+}
