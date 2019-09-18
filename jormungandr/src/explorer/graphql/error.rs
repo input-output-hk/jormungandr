df1ef0b7008b5b9e7030f38c172cd94f798062ef
Enzo Cioppettini <ecioppettini@atixlabs.com>
2019-09-18 12:38:44 -0300

    add Unimplemented error type

--- jormungandr/src/explorer/graphql/error.rs
+++ jormungandr/src/explorer/graphql/error.rs
@@ -4,5 +4,13 @@ error_chain! {
             description("an error that shouldn't happen"),
             display("{}", msg)
         }
+        NotFound(msg: String) {
+            description("resource not found"),
+            display("{}", msg)
+        }
+        Unimplemented {
+            description("feature not implemented yet"),
+            display("unimplemented")
+        }
     }
 }
