a857ff6f00b63210c2d6fc6bf59d625a1868d19a
dkijania <dariusz.kijania@iohk.io>
2019-10-07 12:16:24 +0200

    added jcli library output for utils module

---
+++ jcli/src/lib.rs
@@ -0,0 +1,30 @@
+extern crate bech32;
+extern crate chain_addr;
+extern crate chain_core;
+extern crate chain_crypto;
+extern crate chain_impl_mockchain;
+extern crate chain_time;
+extern crate gtmpl;
+extern crate hex;
+extern crate jormungandr_lib;
+#[cfg(test)]
+#[macro_use]
+extern crate maplit;
+extern crate mime;
+extern crate openapiv3;
+extern crate rand;
+extern crate rand_chacha;
+extern crate reqwest;
+extern crate serde;
+extern crate serde_json;
+extern crate serde_yaml;
+extern crate structopt;
+#[macro_use(custom_error)]
+extern crate custom_error;
+extern crate strfmt;
+extern crate valico;
+
+pub mod jcli_app;
+
+use std::error::Error;
+use structopt::StructOpt;
