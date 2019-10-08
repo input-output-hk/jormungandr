643ad28c8626fea9f3259eb8546009a0c1e983d6
dkijania <dariusz.kijania@iohk.io>
2019-10-08 08:02:36 +0200

    made testing cert builder module public

--- chain-impl-mockchain/src/testing/builders/mod.rs
+++ chain-impl-mockchain/src/testing/builders/mod.rs
@@ -1,12 +1,14 @@
 pub mod block_builder;
+pub mod cert_builder;
 pub mod stake_pool_builder;
 pub mod tx_builder;
 pub mod tx_cert_builder;
 pub mod witness_builder;
 
 pub use block_builder::*;
+pub use cert_builder::*;
 pub use stake_pool_builder::*;
+pub use tx_builder::*;
 pub use tx_builder::*;
 pub use tx_cert_builder::*;
 pub use witness_builder::*;
-pub use tx_builder::*;
