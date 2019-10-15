da61227ec9986d9102af1ea020c21577f4544ec5
Mikhail Zabaluev <mikhail.zabaluev@iohk.io>
2019-10-15 22:29:37 +0300

    Cancellable future to connect client
    A future that drives client connection and stream subscriptions,
    that needs to be cancellable at any point in the process.
    
    Reworked client::connect to return the abovementioned future
    and the cancellation/result handle yielding PeerComms when
    the client connects.
    
    PeerMap keeps track of the ConnectHandle for a peer while the client
    is connecting, providing accessors to peer's communication handles
    possibly updated after the connection is established, as well as
    methods called when a server subscription request comes in, which
    cancel the pending client connection and return the comm handles
    cleaned up from pending items in expectation of server-side
    subscriptions.
    
    PeerComms::update merges communication handles from a
    new peer into an existing entry, taking into account any
    pending items.

--- jormungandr/src/main.rs
+++ jormungandr/src/main.rs
@@ -49,6 +49,7 @@ extern crate slog_json;
 extern crate slog_syslog;
 extern crate slog_term;
 extern crate structopt;
+extern crate thiserror;
 extern crate tk_listen;
 extern crate tokio;
 
--- jormungandr/src/network/p2p/comm/peer_map.rs
+++ jormungandr/src/network/p2p/comm/peer_map.rs
@@ -1,11 +1,54 @@
+use super::super::topology::NodeId;
 use super::{PeerComms, PeerStats};
-use crate::network::p2p::topology::NodeId;
+use crate::network::client::ConnectHandle;
 
 use linked_hash_map::LinkedHashMap;
 
 pub struct PeerMap {
-    map: LinkedHashMap<NodeId, PeerComms>,
+    map: LinkedHashMap<NodeId, PeerData>,
     capacity: usize,
+}
+
+#[derive(Default)]
+struct PeerData {
+    comms: PeerComms,
+    stats: PeerStats,
+    connecting: Option<ConnectHandle>,
+}
+
+impl PeerData {
+    fn with_comms(comms: PeerComms) -> Self {
+        PeerData {
+            comms,
+            stats: PeerStats::default(),
+            connecting: None,
+        }
+    }
+
+    fn updated_comms(&mut self) -> &mut PeerComms {
+        if let Some(ref mut handle) = self.connecting {
+            match handle.try_complete() {
+                Ok(None) => {}
+                Ok(Some(comms)) => {
+                    self.connecting = None;
+                    self.comms.update(comms);
+                }
+                Err(_) => {
+                    self.connecting = None;
+                }
+            }
+        }
+        &mut self.comms
+    }
+
+    fn server_comms(&mut self) -> &mut PeerComms {
+        // This method is called when a subscription request is received
+        // by the server, normally at the beginning of the peer connecting
+        // as a client. Cancel client connection if it is pending.
+        self.connecting = None;
+        self.comms.clear_pending();
+        &mut self.comms
+    }
 }
 
 impl PeerMap {
@@ -25,37 +68,57 @@ impl PeerMap {
         }
     }
 
-    pub fn refresh_peer_comms(&mut self, id: NodeId) -> Option<&mut PeerComms> {
-        self.map.get_refresh(&id)
+    pub fn refresh_peer(&mut self, id: NodeId) -> Option<&mut PeerStats> {
+        self.map.get_refresh(&id).map(|data| &mut data.stats)
     }
 
-    pub fn ensure_peer_comms(&mut self, id: NodeId) -> &mut PeerComms {
+    pub fn peer_comms(&mut self, id: NodeId) -> Option<&mut PeerComms> {
+        self.map.get_mut(&id).map(PeerData::updated_comms)
+    }
+
+    fn ensure_peer(&mut self, id: NodeId) -> &mut PeerData {
         if !self.map.contains_key(&id) {
-            self.insert_peer(id, PeerComms::new());
+            self.evict_if_full();
         }
-        self.map.get_mut(&id).unwrap()
+        self.map.entry(id).or_insert_with(Default::default)
+    }
+
+    pub fn server_comms(&mut self, id: NodeId) -> &mut PeerComms {
+        self.ensure_peer(id).server_comms()
     }
 
     pub fn insert_peer(&mut self, id: NodeId, comms: PeerComms) {
         self.evict_if_full();
-        self.map.insert(id, comms);
+        let data = PeerData::with_comms(comms);
+        self.map.insert(id, data);
+    }
+
+    pub fn add_connecting(&mut self, id: NodeId, handle: ConnectHandle) -> &mut PeerComms {
+        let data = self.ensure_peer(id);
+        data.connecting = Some(handle);
+        data.updated_comms()
     }
 
     pub fn remove_peer(&mut self, id: NodeId) -> Option<PeerComms> {
-        self.map.remove(&id)
+        self.map.remove(&id).map(|mut data| {
+            // A bit tricky here: use PeerData::updated_comms for the
+            // side effect, then return the up-to-date member.
+            data.updated_comms();
+            data.comms
+        })
     }
 
     pub fn next_peer_for_block_fetch(&mut self) -> Option<(NodeId, &mut PeerComms)> {
         self.map
             .iter_mut()
             .next_back()
-            .map(|(&id, comms)| (id, comms))
+            .map(|(&id, data)| (id, data.updated_comms()))
     }
 
     pub fn stats(&self) -> Vec<(NodeId, PeerStats)> {
         self.map
             .iter()
-            .map(|(id, comms)| (*id, comms.stats.clone()))
+            .map(|(&id, data)| (id, data.stats.clone()))
             .collect()
     }
 
@@ -67,12 +130,16 @@ impl PeerMap {
 }
 
 pub struct Entry<'a> {
-    inner: linked_hash_map::OccupiedEntry<'a, NodeId, PeerComms>,
+    inner: linked_hash_map::OccupiedEntry<'a, NodeId, PeerData>,
 }
 
 impl<'a> Entry<'a> {
-    pub fn comms(&mut self) -> &mut PeerComms {
-        self.inner.get_mut()
+    pub fn updated_comms(&mut self) -> &mut PeerComms {
+        self.inner.get_mut().updated_comms()
+    }
+
+    pub fn stats(&mut self) -> &mut PeerStats {
+        &mut self.inner.get_mut().stats
     }
 
     pub fn remove(self) {
