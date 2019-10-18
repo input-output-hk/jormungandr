810d8e41cf217a51ada34ed00ea1e0b08709e4c2
Dariusz Kijania <dariusz.kijania@iohk.io>
2019-10-18 14:34:44 +0200

    increasing grace_percentage parameter. Will make test_leader_election_is_consistent_with_stake_distribution test more stable (#117)

--- chain-impl-mockchain/src/leadership/genesis/mod.rs
+++ chain-impl-mockchain/src/leadership/genesis/mod.rs
@@ -355,7 +355,7 @@ mod tests {
             total_election_count as f32 / leader_election_parameters.pools_count as f32;
         let ideal_election_percentage =
             ideal_election_count_per_pool as f32 / total_election_count as f32;
-        let grace_percentage: f32 = 0.05;
+        let grace_percentage: f32 = 0.08;
         println!(
             "ideal percentage: {:.2}, grace_percentage: {:.2}",
             ideal_election_percentage, grace_percentage
