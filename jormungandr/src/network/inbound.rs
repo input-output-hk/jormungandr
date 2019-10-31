4cf45d7702b6017d4785fb3cd530cf0f234162dc
Mikhail Zabaluev <mikhail.zabaluev@iohk.io>
2019-10-31 23:32:43 +0200

    Log stream termination for push requests

--- jormungandr/src/network/inbound.rs
+++ jormungandr/src/network/inbound.rs
@@ -100,28 +100,33 @@ impl<T, Msg> Sink for InboundProcessing<T, Msg> {
     }
 }
 
+fn log_stream_termination(logger: &Logger, res: &Result<(), ProcessingError>) {
+    match res {
+        Ok(()) => {
+            debug!(logger, "request stream closed by the peer");
+        }
+        Err(e) => {
+            debug!(
+                logger,
+                "request stream failed";
+                "error" => ?e,
+            );
+        }
+    }
+}
+
 impl<T, Msg> MapResponse for InboundProcessing<T, Msg> {
     type Response = ();
     type ResponseFuture = FutureResult<(), core_error::Error>;
 
     fn on_stream_termination(&mut self, res: Result<(), ProcessingError>) -> Self::ResponseFuture {
-        match res {
-            Ok(()) => {
-                debug!(self.logger, "request stream closed by the peer");
-                future::ok(())
-            }
-            Err(e) => {
-                debug!(
-                    self.logger,
-                    "request stream failed";
-                    "error" => ?e,
-                );
-                future::err(core_error::Error::new(
-                    core_error::Code::Canceled,
-                    "not completely processed due to request stream failure",
-                ))
-            }
-        }
+        log_stream_termination(&self.logger, &res);
+        future::result(res.map_err(|_| {
+            core_error::Error::new(
+                core_error::Code::Canceled,
+                "not completely processed due to request stream failure",
+            )
+        }))
     }
 }
 
@@ -134,6 +139,7 @@ where
     type ResponseFuture = ReplyFuture<R, core_error::Error>;
 
     fn on_stream_termination(&mut self, res: Result<(), ProcessingError>) -> Self::ResponseFuture {
+        log_stream_termination(self.logger(), &res);
         self.take_reply_future()
     }
 }
