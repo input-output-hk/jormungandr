0ef09d4a4db8e316d192f6339c88cb4247217d6f
dkijania <dariusz.kijania@iohk.io>
2019-11-15 18:16:31 +0100

    added error_chain to main test module

--- jormungandr-scenario-tests/src/test/mod.rs
+++ jormungandr-scenario-tests/src/test/mod.rs
@@ -1,3 +1,11 @@
 pub mod comm;
 pub mod network;
 pub mod utils;
+
+error_chain! {
+    links {
+        Node(crate::node::Error, crate::node::ErrorKind);
+        Wallet(crate::wallet::Error, crate::wallet::ErrorKind);
+        Scenario(crate::scenario::Error, crate::scenario::ErrorKind);
+    }
+}
