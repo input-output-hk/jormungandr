5453b3db3b1b6d90d4f2b4d78a1a770f482c7ce6
Vincent Hanquez <vincent.hanquez@iohk.io>
2019-11-25 09:39:52 +0800

    Add some method to leaderlog

--- chain-impl-mockchain/src/block/leaderlog.rs
+++ chain-impl-mockchain/src/block/leaderlog.rs
@@ -1,15 +1,19 @@
 use crate::certificate::PoolId;
-use imhamt::Hamt;
+use imhamt::{Hamt, HamtIter, InsertError};
 use std::collections::hash_map::DefaultHasher;
 
 /// Count how many blocks have been created by a specific Pool
-#[derive(Clone)]
+#[derive(Clone, PartialEq, Eq)]
 pub struct LeadersParticipationRecord {
     total: u32,
     log: Hamt<DefaultHasher, PoolId, u32>,
 }
 
 impl LeadersParticipationRecord {
+    pub fn total(&self) -> u32 {
+        self.total
+    }
+
     /// new empty leader log
     pub fn new() -> Self {
         Self {
@@ -26,5 +30,20 @@ impl LeadersParticipationRecord {
                 .log
                 .insert_or_update_simple(pool.clone(), 1, |v| Some(v + 1)),
         }
+    }
+
+    /// Set a pool id to a specific value.
+    ///
+    /// if the value already exists, then it returns an insert error.
+    /// This should only be used related to the iterator construction,
+    pub fn set_for(&mut self, pool: PoolId, v: u32) -> Result<(), InsertError> {
+        self.total += v;
+        self.log = self.log.insert(pool, v)?;
+        Ok(())
+    }
+
+    /// Iterate over all known pool record
+    pub fn iter<'a>(&'a self) -> HamtIter<'a, PoolId, u32> {
+        self.log.iter()
     }
 }
