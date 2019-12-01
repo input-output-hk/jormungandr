86e392e72af61f6fa37cecea9f639acab9da2834
Nicolas Di Prima <nicolas@primetype.co.uk>
2019-12-01 10:53:09 +0000

    actually set the new updated value and don't dispose of it

--- chain-impl-mockchain/src/block/leaderlog.rs
+++ chain-impl-mockchain/src/block/leaderlog.rs
@@ -23,13 +23,11 @@ impl LeadersParticipationRecord {
     }
 
     /// Add one count to a pool. if the pool doesn't exist, then set it to 1
-    pub fn increase_for(&self, pool: &PoolId) -> Self {
-        Self {
-            total: self.total + 1,
-            log: self
-                .log
-                .insert_or_update_simple(pool.clone(), 1, |v| Some(v + 1)),
-        }
+    pub fn increase_for(&mut self, pool: &PoolId) {
+        self.total = self.total + 1;
+        self.log = self
+            .log
+            .insert_or_update_simple(pool.clone(), 1, |v| Some(v + 1));
     }
 
     /// Set a pool id to a specific value.
