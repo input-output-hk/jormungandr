170186729cd42a4fa320c7c1a08ccaa41aa3297f
Dariusz Kijania <dariusz.kijania@iohk.io>
2019-12-02 13:02:44 +0100

    initial funds overflow test (#181)

--- chain-impl-mockchain/src/ledger/tests/initial_funds_tests.rs
+++ chain-impl-mockchain/src/ledger/tests/initial_funds_tests.rs
@@ -1,41 +1,58 @@
 #![cfg(test)]
 
 use crate::{
+    ledger::check::CHECK_TX_MAXIMUM_INPUTS,
     testing::{
-        ledger::{ConfigBuilder, LedgerBuilder},
         arbitrary::address::ArbitraryAddressDataValueVec,
-        data::AddressDataValue
+        data::AddressDataValue,
+        ledger::{ConfigBuilder, LedgerBuilder},
     },
     value::Value,
 };
 use chain_addr::Discrimination;
 use quickcheck::TestResult;
 use quickcheck_macros::quickcheck;
+use std::iter;
 
 #[quickcheck]
 pub fn ledger_verifies_value_of_initial_funds(
     arbitrary_faucets: ArbitraryAddressDataValueVec,
 ) -> TestResult {
-    let config = ConfigBuilder::new(0)
-        .with_discrimination(Discrimination::Test);
+    let config = ConfigBuilder::new(0).with_discrimination(Discrimination::Test);
 
-    TestResult::from_bool(LedgerBuilder::from_config(config)
-        .initial_funds(&arbitrary_faucets.values())
-        .build()
-        .is_ok()
+    TestResult::from_bool(
+        LedgerBuilder::from_config(config)
+            .initial_funds(&arbitrary_faucets.values())
+            .build()
+            .is_ok(),
     )
 }
 
 #[test]
 pub fn ledger_fails_to_start_when_there_is_zero_output() {
-    let config = ConfigBuilder::new(0)
-        .with_discrimination(Discrimination::Test);
+    let config = ConfigBuilder::new(0).with_discrimination(Discrimination::Test);
 
-    let address = AddressDataValue::account(Discrimination::Test,Value::zero());
+    let address = AddressDataValue::account(Discrimination::Test, Value::zero());
 
-    assert!(LedgerBuilder::from_config(config)
-        .faucet(&address)
+    assert!(
+        LedgerBuilder::from_config(config)
+            .faucet(&address)
+            .build()
+            .is_err(),
+        "Ledger should fail to start with zero value output"
+    );
+}
+
+#[test]
+#[should_panic]
+pub fn ledger_fails_to_start_when_there_are_more_than_255_initials() {
+    let config = ConfigBuilder::new(0).with_discrimination(Discrimination::Test);
+    let addresses: Vec<AddressDataValue> =
+        iter::from_fn(|| Some(AddressDataValue::account(Discrimination::Test, Value(10))))
+            .take(CHECK_TX_MAXIMUM_INPUTS as usize + 1)
+            .collect();
+    LedgerBuilder::from_config(config)
+        .faucets(&addresses)
         .build()
-        .is_err(),
-        "Ledger should fail to start with zero value output");
+        .unwrap();
 }
