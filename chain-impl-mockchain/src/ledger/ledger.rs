29ff4284780d0759f4e05dccb7bcf20b3c81e728
Nicolas Di Prima <nicolas@primetype.co.uk>
2019-12-04 09:25:26 +0000

    revert invalid change

--- chain-impl-mockchain/src/ledger/ledger.rs
+++ chain-impl-mockchain/src/ledger/ledger.rs
@@ -469,7 +469,7 @@ impl Ledger {
         let mut leftover_reward = distr.after_tax;
         for (account, stake) in distribution.stake_owners.iter() {
             let ps = PercentStake::new(*stake, distribution.total.total_stake);
-            let r = ps.scale_value(leftover_reward);
+            let r = ps.scale_value(distr.after_tax);
             leftover_reward = (leftover_reward - r).unwrap();
             self.accounts = self
                 .accounts
