b0418fc0b3123875b28411bf28319080b635afdb
CodeSandwich <igor.zuk@protonmail.com>
2020-01-15 08:13:27 +0100

    Port REST server error to thiserror

--- jormungandr/src/rest/server/error.rs
+++ jormungandr/src/rest/server/error.rs
@@ -1,38 +1,18 @@
-use native_tls::Error as TlsError;
-use std::error::Error as StdError;
-use std::fmt::{Display, Formatter, Result as FmtResult};
-use std::io::Error as IoError;
+use std::io;
+use thiserror::Error;
 
-#[derive(Debug)]
+#[derive(Debug, Error)]
 pub enum Error {
-    Pkcs12LoadFailed(IoError),
-    Pkcs12Invalid(TlsError),
-    BindFailed(IoError),
+    #[error("failed to load PKCS12 identity file")]
+    Pkcs12LoadFailed(#[source] io::Error),
+    #[error("invalid PKCS12 identity file")]
+    Pkcs12Invalid(#[source] native_tls::Error),
+    #[error("failed to bind the port")]
+    BindFailed(#[source] io::Error),
+    #[error("couldn't stop server, it's already stopped")]
     ServerAlreadyStopped,
+    #[error("timeout during server stopping")]
     ServerStopTimeout,
+    #[error("failed to stop server")]
     ServerStopFailed,
-}
-
-impl Display for Error {
-    fn fmt(&self, f: &mut Formatter) -> FmtResult {
-        match *self {
-            Error::Pkcs12LoadFailed(_) => write!(f, "Failed to load PKCS12 identity file"),
-            Error::Pkcs12Invalid(_) => write!(f, "Invalid PKCS12 identity file"),
-            Error::BindFailed(_) => write!(f, "Failed to bind the port"),
-            Error::ServerAlreadyStopped => write!(f, "Couldn't stop server, it's already stopped"),
-            Error::ServerStopTimeout => write!(f, "Timeout during server stopping"),
-            Error::ServerStopFailed => write!(f, "Failed to stop server"),
-        }
-    }
-}
-
-impl StdError for Error {
-    fn source(&self) -> Option<&(dyn StdError + 'static)> {
-        match *self {
-            Error::Pkcs12LoadFailed(ref cause) => Some(cause),
-            Error::Pkcs12Invalid(ref cause) => Some(cause),
-            Error::BindFailed(ref cause) => Some(cause),
-            _ => None,
-        }
-    }
 }
