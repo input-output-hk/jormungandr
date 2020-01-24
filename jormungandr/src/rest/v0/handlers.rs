a1cd853096eca508af720f6ec5df4bb7ffbf90a6
Michael Fazio <michaelfazio@me.com>
2020-01-24 17:25:27 +0800

    Changed peers counts from strings to int

--- jormungandr/src/rest/v0/handlers.rs
+++ jormungandr/src/rest/v0/handlers.rs
@@ -171,9 +171,9 @@ async fn create_stats(context: &FullContext) -> Result<serde_json::Value, Error>
         "lastBlockContentSize": tip_header.block_content_size(),
         "lastBlockSum": block_input_sum.0,
         "lastBlockFees": block_fee_sum.0,
-        "peerAvailableCnt": nodes_count.all_available_nodes_count().to_string(),
-        "peerUnreachableCnt": nodes_count.all_unreachable_nodes_count().to_string(),
-        "peerQuarantinedCnt": nodes_count.all_quarantined_nodes_count().to_string(),
+        "peerAvailableCnt": nodes_count.all_available_nodes_count(),
+        "peerUnreachableCnt": nodes_count.all_unreachable_nodes_count(),
+        "peerQuarantinedCnt": nodes_count.all_quarantined_nodes_count(),
     }))
 }
 
