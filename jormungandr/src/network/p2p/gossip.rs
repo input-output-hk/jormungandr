51b5dae123d87a1cc3af73882380255dd3a91e80
danielsanchezq <daniel.sanchez@iohk.io>
2020-02-05 11:52:46 +0100

    Cargo fmt

--- jormungandr/src/network/p2p/gossip.rs
+++ jormungandr/src/network/p2p/gossip.rs
@@ -176,74 +176,74 @@ impl property::Deserialize for Gossip {
 #[cfg(test)]
 mod tests {
     use super::*;
-    use poldercast::{NodeProfile, NodeProfileBuilder, Address};
-    use std::str::FromStr;
+    use poldercast::{Address, NodeProfile, NodeProfileBuilder};
     use std::net::{Ipv4Addr, Ipv6Addr};
+    use std::str::FromStr;
 
     #[test]
     fn gossip_global_ipv4_private() {
-        let mut builder : NodeProfileBuilder = NodeProfileBuilder::new();
+        let mut builder: NodeProfileBuilder = NodeProfileBuilder::new();
         let ip = Ipv4Addr::new(10, 0, 0, 1);
         builder.address(Address::new(ip).ok().unwrap());
-        let node : Gossip = Gossip::from(builder.build());
+        let node: Gossip = Gossip::from(builder.build());
         assert!(!node.is_global());
     }
 
     #[test]
     fn gossip_global_ipv6_private() {
-        let mut builder : NodeProfileBuilder = NodeProfileBuilder::new();
+        let mut builder: NodeProfileBuilder = NodeProfileBuilder::new();
         let ip = Ipv4Addr::new(0, 0, 0, 0);
         let ipv6 = ip.to_ipv6_compatible();
         builder.address(Address::new(ipv6).ok().unwrap());
-        let node : Gossip = Gossip::from(builder.build());
+        let node: Gossip = Gossip::from(builder.build());
         assert!(!node.is_global());
     }
 
     #[test]
     fn gossip_global_ipv4_loopback() {
-        let mut builder : NodeProfileBuilder = NodeProfileBuilder::new();
+        let mut builder: NodeProfileBuilder = NodeProfileBuilder::new();
         let ip = Ipv4Addr::new(127, 255, 255, 255);
         // Address should not be private but be loopback
         assert!(!ip.is_private());
         builder.address(Address::new(ip).ok().unwrap());
-        let node : Gossip = Gossip::from(builder.build());
+        let node: Gossip = Gossip::from(builder.build());
         assert!(!node.is_global());
     }
 
     #[test]
     fn gossip_global_ipv6_loopback() {
-        let mut builder : NodeProfileBuilder = NodeProfileBuilder::new();
+        let mut builder: NodeProfileBuilder = NodeProfileBuilder::new();
         let ip = Ipv4Addr::new(127, 255, 255, 255);
         // Address should not be private but be loopback
         assert!(!ip.is_private());
         let ipv6 = ip.to_ipv6_compatible();
         builder.address(Address::new(ipv6).ok().unwrap());
-        let node : Gossip = Gossip::from(builder.build());
+        let node: Gossip = Gossip::from(builder.build());
         assert!(!node.is_global());
     }
 
     #[test]
     fn gossip_global_ipv4_link_local() {
-        let mut builder : NodeProfileBuilder = NodeProfileBuilder::new();
+        let mut builder: NodeProfileBuilder = NodeProfileBuilder::new();
         let ip = Ipv4Addr::new(169, 254, 10, 65);
         // Address should not be private nor loopback
         assert!(!ip.is_private());
         assert!(!ip.is_loopback());
         builder.address(Address::new(ip).ok().unwrap());
-        let node : Gossip = Gossip::from(builder.build());
+        let node: Gossip = Gossip::from(builder.build());
         assert!(!node.is_global());
     }
 
     #[test]
     fn gossip_global_ipv6_link_local() {
-        let mut builder : NodeProfileBuilder = NodeProfileBuilder::new();
+        let mut builder: NodeProfileBuilder = NodeProfileBuilder::new();
         let ip = Ipv4Addr::new(169, 254, 10, 65);
         // Address should not be private not loopback
         assert!(!ip.is_private());
         assert!(!ip.is_loopback());
         let ipv6 = ip.to_ipv6_compatible();
         builder.address(Address::new(ipv6).ok().unwrap());
-        let node : Gossip = Gossip::from(builder.build());
+        let node: Gossip = Gossip::from(builder.build());
         assert!(!node.is_global());
     }
 }
