b71ad5590d856e8ce7ae5da89ffe4929cd764912
dkijania <dariusz.kijania@iohk.io>
2020-02-13 09:42:21 +0100

    moving measurement mod to libs. small clear imports

--- jormungandr-lib/src/testing/measurement/status.rs
+++ jormungandr-lib/src/testing/measurement/status.rs
@@ -1,13 +1,13 @@
 use std::fmt;
 
 #[derive(Clone, Debug, Eq, PartialEq)]
-pub enum MeasurementStatus {
+pub enum Status {
     Green,
     Yellow,
     Red,
 }
 
-impl fmt::Display for MeasurementStatus {
+impl fmt::Display for Status {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         write!(f, "{:?}", self)
     }
