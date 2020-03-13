0751bd1269f40e0c8c8ba07d54dbdf7bfbfcb5ab
Mikhail Zabaluev <mikhail.zabaluev@iohk.io>
2020-03-13 12:58:06 +0200

    Draft conversion of mod bootstrap

--- jormungandr/src/network/grpc/mod.rs
+++ jormungandr/src/network/grpc/mod.rs
@@ -1,26 +1,5 @@
 mod client;
 mod server;
 
-use crate::{
-    blockcfg::{Block, BlockDate, Fragment, FragmentId, Header, HeaderHash},
-    network::{
-        p2p::{Gossip as NodeData, Id},
-        BlockConfig,
-    },
-};
-
-pub use self::client::{
-    connect, fetch_block, ConnectError, ConnectFuture, Connection, FetchBlockError,
-};
+pub use self::client::{connect, fetch_block, Client, ConnectError, FetchBlockError};
 pub use self::server::run_listen_socket;
-
-impl network_grpc::client::ProtocolConfig for BlockConfig {
-    type Block = Block;
-    type Header = Header;
-    type BlockId = HeaderHash;
-    type BlockDate = BlockDate;
-    type Fragment = Fragment;
-    type FragmentId = FragmentId;
-    type Node = NodeData;
-    type NodeId = Id;
-}
