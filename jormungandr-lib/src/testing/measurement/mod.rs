a01b53f01eae77928b6b02f859e410fe3a3351c5
dkijania <dariusz.kijania@iohk.io>
2020-03-16 13:19:18 +0100

    added consumption benchmark for monitoring cpu & measurement

--- jormungandr-lib/src/testing/measurement/mod.rs
+++ jormungandr-lib/src/testing/measurement/mod.rs
@@ -6,10 +6,10 @@ mod thresholds;
 
 pub use attribute::{Efficiency, Endurance, Speed};
 pub use benchmark::{
-    benchmark_efficiency, benchmark_endurance, benchmark_speed, EfficiencyBenchmarkDef,
-    EfficiencyBenchmarkFinish, EfficiencyBenchmarkRun, EnduranceBenchmarkDef,
-    EnduranceBenchmarkFinish, EnduranceBenchmarkRun, SpeedBenchmarkDef, SpeedBenchmarkFinish,
-    SpeedBenchmarkRun,
+    benchmark_consumption, benchmark_efficiency, benchmark_endurance, benchmark_speed,
+    EfficiencyBenchmarkDef, EfficiencyBenchmarkFinish, EfficiencyBenchmarkRun,
+    EnduranceBenchmarkDef, EnduranceBenchmarkFinish, EnduranceBenchmarkRun, SpeedBenchmarkDef,
+    SpeedBenchmarkFinish, SpeedBenchmarkRun,
 };
 pub use marker::{Counter, Timestamp};
 pub use status::Status;
