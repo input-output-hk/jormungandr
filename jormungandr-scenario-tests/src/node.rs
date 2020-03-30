dafb1b5b90cf6a172368dabedc5c38cf247b5322
dkijania <dariusz.kijania@iohk.io>
2020-03-30 19:39:31 +0200

    fmt

--- jormungandr-scenario-tests/src/node.rs
+++ jormungandr-scenario-tests/src/node.rs
@@ -15,7 +15,8 @@ use jormungandr_integration_tests::{
     response_to_vec,
 };
 use jormungandr_lib::interfaces::{
-    EnclaveLeaderId, FragmentLog, FragmentStatus, NodeState, NodeStatsDto, PeerStats, PeerRecord, Info,
+    EnclaveLeaderId, FragmentLog, FragmentStatus, Info, NodeState, NodeStatsDto, PeerRecord,
+    PeerStats,
 };
 use rand_core::RngCore;
 use std::{
@@ -260,11 +261,8 @@ impl NodeController {
 
     pub fn network_stats(&self) -> Result<Vec<PeerStats>> {
         let response_text = self.get("network/stats")?.text()?;
-        self.progress_bar.log_info(format!(
-            "network/stats: {}",
-            response_text
-        ));
-
+        self.progress_bar
+            .log_info(format!("network/stats: {}", response_text));
 
         let network_stats: Vec<PeerStats> = if response_text.is_empty() {
             Vec::new()
@@ -277,10 +275,8 @@ impl NodeController {
     pub fn p2p_quarantined(&self) -> Result<Vec<PeerRecord>> {
         let response_text = self.get("network/p2p/quarantined")?.text()?;
 
-        self.progress_bar.log_info(format!(
-            "network/p2p_quarantined: {}",
-            response_text
-        ));
+        self.progress_bar
+            .log_info(format!("network/p2p_quarantined: {}", response_text));
 
         let network_stats: Vec<PeerRecord> = if response_text.is_empty() {
             Vec::new()
@@ -293,10 +289,8 @@ impl NodeController {
     pub fn p2p_non_public(&self) -> Result<Vec<PeerRecord>> {
         let response_text = self.get("network/p2p/non_public")?.text()?;
 
-        self.progress_bar.log_info(format!(
-            "network/non_publicS: {}",
-            response_text
-        ));
+        self.progress_bar
+            .log_info(format!("network/non_publicS: {}", response_text));
 
         let network_stats: Vec<PeerRecord> = if response_text.is_empty() {
             Vec::new()
@@ -309,10 +303,8 @@ impl NodeController {
     pub fn p2p_available(&self) -> Result<Vec<PeerRecord>> {
         let response_text = self.get("network/p2p/available")?.text()?;
 
-                self.progress_bar.log_info(format!(
-            "network/available: {}",
-            response_text
-        ));
+        self.progress_bar
+            .log_info(format!("network/available: {}", response_text));
 
         let network_stats: Vec<PeerRecord> = if response_text.is_empty() {
             Vec::new()
@@ -325,11 +317,8 @@ impl NodeController {
     pub fn p2p_view(&self) -> Result<Vec<Info>> {
         let response_text = self.get("network/p2p/view")?.text()?;
 
-
-        self.progress_bar.log_info(format!(
-            "network/view: {}",
-            response_text
-        ));
+        self.progress_bar
+            .log_info(format!("network/view: {}", response_text));
 
         let network_stats: Vec<Info> = if response_text.is_empty() {
             Vec::new()
--- jormungandr-scenario-tests/src/test/features/p2p.rs
+++ jormungandr-scenario-tests/src/test/features/p2p.rs
@@ -32,7 +32,7 @@ pub fn p2p_stats_test(mut context: Context<ChaChaRng>) -> Result<ScenarioResult>
                 account "delegated3" with  2_000_000_000 delegates to LEADER3,
                 account "delegated4" with  2_000_000_000 delegates to LEADER4,
             ],
-        }  
+        }
     };
 
     let mut controller = scenario_settings.build(context)?;
@@ -42,113 +42,130 @@ pub fn p2p_stats_test(mut context: Context<ChaChaRng>) -> Result<ScenarioResult>
     leader1.wait_for_bootstrap()?;
 
     let leader1_node_id = leader1.stats()?.stats.expect("empty stats").node_id.clone();
-    assert_node_stats(&leader1,0,0,0,0,0)?;
+    assert_node_stats(&leader1, 0, 0, 0, 0, 0)?;
 
-
-    utils::assert_equals(&vec![],&leader1.network_stats()?,"network_stats")?;
-    utils::assert_equals(&vec![],&leader1.p2p_quarantined()?,"p2p_quarantined")?;
-    utils::assert_equals(&vec![],&leader1.p2p_non_public()?,"p2p_non_public")?;
-    utils::assert_equals(&vec![],&leader1.p2p_available()?,"p2p_available")?;
-    utils::assert_equals(&vec![],&leader1.p2p_view()?,"p2p_view")?;
-
+    utils::assert_equals(&vec![], &leader1.network_stats()?, "network_stats")?;
+    utils::assert_equals(&vec![], &leader1.p2p_quarantined()?, "p2p_quarantined")?;
+    utils::assert_equals(&vec![], &leader1.p2p_non_public()?, "p2p_non_public")?;
+    utils::assert_equals(&vec![], &leader1.p2p_available()?, "p2p_available")?;
+    utils::assert_equals(&vec![], &leader1.p2p_view()?, "p2p_view")?;
 
     let leader2 =
         controller.spawn_node(LEADER2, LeadershipMode::Leader, PersistenceMode::Persistent)?;
     leader2.wait_for_bootstrap()?;
-    
+
     utils::wait(90);
-    assert_node_stats(&leader1,1,1,0,1,0)?;
-    assert_node_stats(&leader2,1,1,0,1,0)?;
-   /* 
+    assert_node_stats(&leader1, 1, 1, 0, 1, 0)?;
+    assert_node_stats(&leader2, 1, 1, 0, 1, 0)?;
+    /*
     let leader3 =
         controller.spawn_node(LEADER3, LeadershipMode::Leader, PersistenceMode::Persistent)?;
     leader3.wait_for_bootstrap()?;
     */
- /*   let leader4 =
-        controller.spawn_node(LEADER4, LeadershipMode::Leader, PersistenceMode::Persistent)?;
-    leader4.wait_for_bootstrap()?;
-    
-    utils::wait(90);
+    /*   let leader4 =
+            controller.spawn_node(LEADER4, LeadershipMode::Leader, PersistenceMode::Persistent)?;
+        leader4.wait_for_bootstrap()?;
 
-    
-    println!("{:?}",leader1.stats()?.stats.expect("empty stats"));
-    println!("{:?}",leader1.network_stats());
-    println!("{:?}",leader1.p2p_quarantined());
-    println!("{:?}",leader1.p2p_non_public());
-    println!("{:?}",leader1.p2p_available());
-    println!("{:?}",leader1.p2p_view());
+        utils::wait(90);
 
 
-    println!("{:?}",leader2.stats()?.stats.expect("empty stats"));
-    println!("{:?}",leader2.network_stats());
-    println!("{:?}",leader2.p2p_quarantined());
-    println!("{:?}",leader2.p2p_non_public());
-    println!("{:?}",leader2.p2p_available());
-    println!("{:?}",leader2.p2p_view());
-
-    println!("{:?}",leader3.stats()?.stats.expect("empty stats"));
-    println!("{:?}",leader3.network_stats());
-    println!("{:?}",leader3.p2p_quarantined());
-    println!("{:?}",leader3.p2p_non_public());
-    println!("{:?}",leader3.p2p_available());
-    println!("{:?}",leader3.p2p_view());
-*/
-  //  assert_node_stats(&leader1,3,4,0,3,0)?;
-   // assert_node_stats(&leader2,3,4,0,3,0)?;
-   // assert_node_stats(&leader3,3,4,0,3,0)?;
- //   assert_node_stats(&leader4,3,4,0,3,0)?;
-
-/*
-    leader2.shutdown()?;
-
-    utils::wait(60);
-
-    println!("{:?}",leader1.network_stats());
-    println!("{:?}",leader1.p2p_quarantined());
-    println!("{:?}",leader1.p2p_non_public());
-    println!("{:?}",leader1.p2p_available());
-    println!("{:?}",leader1.p2p_view());
-
-    assert_node_stats(&leader1,2,3,1,4,1)?;
+        println!("{:?}",leader1.stats()?.stats.expect("empty stats"));
+        println!("{:?}",leader1.network_stats());
+        println!("{:?}",leader1.p2p_quarantined());
+        println!("{:?}",leader1.p2p_non_public());
+        println!("{:?}",leader1.p2p_available());
+        println!("{:?}",leader1.p2p_view());
 
 
-    let leader2 =
-        controller.spawn_node(LEADER2, LeadershipMode::Leader, PersistenceMode::Persistent)?;
-    leader2.wait_for_bootstrap()?;
-    
+        println!("{:?}",leader2.stats()?.stats.expect("empty stats"));
+        println!("{:?}",leader2.network_stats());
+        println!("{:?}",leader2.p2p_quarantined());
+        println!("{:?}",leader2.p2p_non_public());
+        println!("{:?}",leader2.p2p_available());
+        println!("{:?}",leader2.p2p_view());
 
-    utils::wait(40);
+        println!("{:?}",leader3.stats()?.stats.expect("empty stats"));
+        println!("{:?}",leader3.network_stats());
+        println!("{:?}",leader3.p2p_quarantined());
+        println!("{:?}",leader3.p2p_non_public());
+        println!("{:?}",leader3.p2p_available());
+        println!("{:?}",leader3.p2p_view());
+    */
+    //  assert_node_stats(&leader1,3,4,0,3,0)?;
+    // assert_node_stats(&leader2,3,4,0,3,0)?;
+    // assert_node_stats(&leader3,3,4,0,3,0)?;
+    //   assert_node_stats(&leader4,3,4,0,3,0)?;
 
-    assert_node_stats(&leader1,3,4,0,3,0);
-    assert_node_stats(&leader2,3,4,0,3,0);
-    assert_node_stats(&leader3,3,4,0,3,0);
-    assert_node_stats(&leader4,3,4,0,3,0);
+    /*
+        leader2.shutdown()?;
 
-*/
+        utils::wait(60);
+
+        println!("{:?}",leader1.network_stats());
+        println!("{:?}",leader1.p2p_quarantined());
+        println!("{:?}",leader1.p2p_non_public());
+        println!("{:?}",leader1.p2p_available());
+        println!("{:?}",leader1.p2p_view());
+
+        assert_node_stats(&leader1,2,3,1,4,1)?;
+
+
+        let leader2 =
+            controller.spawn_node(LEADER2, LeadershipMode::Leader, PersistenceMode::Persistent)?;
+        leader2.wait_for_bootstrap()?;
+
+
+        utils::wait(40);
+
+        assert_node_stats(&leader1,3,4,0,3,0);
+        assert_node_stats(&leader2,3,4,0,3,0);
+        assert_node_stats(&leader3,3,4,0,3,0);
+        assert_node_stats(&leader4,3,4,0,3,0);
+
+    */
     leader1.shutdown()?;
     leader2.shutdown()?;
-  //  leader3.shutdown()?;
-  //  leader4.shutdown()?;
+    //  leader3.shutdown()?;
+    //  leader4.shutdown()?;
     controller.finalize();
     Ok(ScenarioResult::passed())
 }
 
-fn assert_node_stats(node: &NodeController,
-    peer_available_cnt: usize, 
-    peer_connected_cnt: usize, 
-    peer_quarantined_cnt: usize, 
-    peer_total_cnt: usize, 
-    peer_unreachable_cnt: usize
+fn assert_node_stats(
+    node: &NodeController,
+    peer_available_cnt: usize,
+    peer_connected_cnt: usize,
+    peer_quarantined_cnt: usize,
+    peer_total_cnt: usize,
+    peer_unreachable_cnt: usize,
 ) -> Result<()> {
-    
-    node.log_stats();    
+    node.log_stats();
     let stats = node.stats()?.stats.expect("empty stats");
-    utils::assert_equals(&peer_available_cnt,&stats.peer_available_cnt.clone(),&format!("peer_available_cnt, Node {}",node.alias()))?;
-    utils::assert_equals(&peer_connected_cnt,&stats.peer_connected_cnt,&format!("peer_connected_cnt, Node {}",node.alias()))?;
-    utils::assert_equals(&peer_quarantined_cnt,&stats.peer_quarantined_cnt,&format!("peer_quarantined_cnt, Node {}",node.alias()))?;
-    utils::assert_equals(&peer_total_cnt,&stats.peer_total_cnt,&format!("peer_total_cnt, Node {}",node.alias()))?;
-    utils::assert_equals(&peer_unreachable_cnt,&stats.peer_unreachable_cnt,&format!("peer_unreachable_cnt, Node {}",node.alias()))?;
+    utils::assert_equals(
+        &peer_available_cnt,
+        &stats.peer_available_cnt.clone(),
+        &format!("peer_available_cnt, Node {}", node.alias()),
+    )?;
+    utils::assert_equals(
+        &peer_connected_cnt,
+        &stats.peer_connected_cnt,
+        &format!("peer_connected_cnt, Node {}", node.alias()),
+    )?;
+    utils::assert_equals(
+        &peer_quarantined_cnt,
+        &stats.peer_quarantined_cnt,
+        &format!("peer_quarantined_cnt, Node {}", node.alias()),
+    )?;
+    utils::assert_equals(
+        &peer_total_cnt,
+        &stats.peer_total_cnt,
+        &format!("peer_total_cnt, Node {}", node.alias()),
+    )?;
+    utils::assert_equals(
+        &peer_unreachable_cnt,
+        &stats.peer_unreachable_cnt,
+        &format!("peer_unreachable_cnt, Node {}", node.alias()),
+    )?;
 
-    
     Ok(())
 }
