aaac73ab56ef9d30866625417dc48e6652d94c14
danielsanchezq <daniel.sanchez@iohk.io>
2020-04-01 16:36:27 +0200

    Removed wrong committed test Cargo fmt

--- jormungandr/src/network/p2p/layers/sea_bottle.rs
+++ jormungandr/src/network/p2p/layers/sea_bottle.rs
@@ -1,15 +1,17 @@
-use std::sync::Arc;
 use crate::blockchain;
+use chain_impl_mockchain::certificate::PoolId;
 use chain_impl_mockchain::stake::PoolStakeInformation;
 use std::cmp::Ordering;
-use chain_impl_mockchain::certificate::PoolId;
-
+use std::sync::Arc;
 
 /// In order to send the sea in the bottle message we need to select the best fit stake pools
 /// in this case we can retrieve the PoolId sorted from a `blockchain::Ref`
-pub fn stake_pools_sorted_by<F>(block_ref: Arc<blockchain::Ref>, comp: F) -> impl IntoIterator<Item = PoolId>
-    where
-        F: Fn(&PoolStakeInformation, &PoolStakeInformation) -> Ordering,
+pub fn stake_pools_sorted_by<F>(
+    block_ref: Arc<blockchain::Ref>,
+    comp: F,
+) -> impl IntoIterator<Item = PoolId>
+where
+    F: Fn(&PoolStakeInformation, &PoolStakeInformation) -> Ordering,
 {
     match block_ref.epoch_leadership_schedule().stake_distribution() {
         None => Vec::new(),
@@ -24,23 +26,14 @@ pub fn stake_pools_sorted_by<F>(block_ref: Arc<blockchain::Ref>, comp: F) -> imp
     }
 }
 
-
 /// Simple cmp function for `PoolStakeInformation` by `stake.total` attribute.
 /// Used together with `stake_pools_sorted_by`
 /// ```ignore
 /// stake_pools_sorted_by(block_ref, cmp_pool_stake_information_by_stake_total)
 /// ```
-pub fn cmp_pool_stake_information_by_stake_total(pool_a: &PoolStakeInformation, pool_b: &PoolStakeInformation) -> Ordering {
+pub fn cmp_pool_stake_information_by_stake_total(
+    pool_a: &PoolStakeInformation,
+    pool_b: &PoolStakeInformation,
+) -> Ordering {
     pool_a.stake.total.cmp(&pool_b.stake.total)
-}
-
-#[cfg(test)]
-pub mod test {
-    use super::*;
-
-    #[test]
-    fn foo() {
-        let mut v : Vec<&PoolStakeInformation> = Vec::new();
-        v.sort_by(cmp_pool_stake_information_by_stake_total);
-    }
 }
