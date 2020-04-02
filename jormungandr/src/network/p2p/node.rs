3caceffeba235d030a74d8b91242869ea22fcfb3
Mikhail Zabaluev <mikhail.zabaluev@iohk.io>
2020-04-02 16:59:51 +0300

    Simplify p2p::Node
    Not using Gossip trait from the old crate.

--- jormungandr/src/network/p2p/node.rs
+++ jormungandr/src/network/p2p/node.rs
@@ -1,7 +1,6 @@
 use crate::network::p2p::Id;
 use bincode;
 use chain_core::property;
-use network_core::gossip;
 use std::net::SocketAddr;
 
 pub struct Node {
@@ -12,23 +11,15 @@ impl Node {
     pub fn new(info: poldercast::NodeInfo) -> Self {
         Self { info }
     }
-}
-
-impl gossip::Node for Node {
-    type Id = Id;
 
     #[inline]
-    fn id(&self) -> Self::Id {
+    pub fn id(&self) -> Id {
         (*self.info.id()).into()
     }
 
     #[inline]
-    fn address(&self) -> Option<SocketAddr> {
-        if let Some(address) = self.info.address() {
-            address.to_socketaddr()
-        } else {
-            None
-        }
+    pub fn address(&self) -> Option<SocketAddr> {
+        self.info.address().map(|addr| addr.to_socketaddr())
     }
 }
 
