ac1478f411b3b33e28b4d00e27990602254cff29
danielsanchezq <daniel.sanchez@iohk.io>
2020-04-22 17:50:46 +0200

    Updated poldercast version in jormungandr-lib
    Removed deprecated poldercast::Id usages

--- jormungandr-lib/src/interfaces/config/node.rs
+++ jormungandr-lib/src/interfaces/config/node.rs
@@ -17,8 +17,6 @@ pub struct P2p {
     /// The public address to which other peers may connect to
     pub public_address: poldercast::Address,
 
-    pub public_id: poldercast::Id,
-
     /// the rendezvous points for the peer to connect to in order to initiate
     /// the p2p discovery from.
     pub trusted_peers: Vec<TrustedPeer>,
@@ -99,7 +97,6 @@ pub struct PreferredListConfig {
 #[serde(deny_unknown_fields)]
 pub struct TrustedPeer {
     pub address: poldercast::Address,
-    pub id: poldercast::Id,
 }
 
 #[derive(Debug, Clone, Deserialize, Serialize)]
@@ -119,7 +116,6 @@ impl P2p {
     pub fn make_trusted_peer_setting(&self) -> TrustedPeer {
         TrustedPeer {
             address: self.get_listen_address(),
-            id: self.public_id.clone(),
         }
     }
 
