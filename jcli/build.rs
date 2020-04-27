a9e68160eb79fa3756d4a8e80089b51640a29c5e
Yevhenii Babichenko <eugene.babichenko@iohk.io>
2020-04-27 15:16:03 +0300

    override package version for nightly builds

--- jcli/build.rs
+++ jcli/build.rs
@@ -1,8 +1,16 @@
 fn main() {
+    let pkg_version = if let Ok(date) = std::env::var("DATE") {
+        format!("{}.{}", env!("CARGO_PKG_VERSION"), date)
+    } else {
+        env!("CARGO_PKG_VERSION").to_string()
+    };
+
+    println!("cargo:rustc-env=CARGO_PKG_VERSION={}", pkg_version);
+
     let version = versionisator::Version::new(
         env!("CARGO_MANIFEST_DIR"),
         env!("CARGO_PKG_NAME").to_string(),
-        env!("CARGO_PKG_VERSION").to_string(),
+        pkg_version,
     );
 
     println!("cargo:rustc-env=FULL_VERSION={}", version.full());
