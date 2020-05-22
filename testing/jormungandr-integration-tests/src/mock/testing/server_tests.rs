9acc529bbc7e89eb7ccae8fec79a3aa4d9a400be
dkijania <dariusz.kijania@iohk.io>
2020-05-22 11:18:11 +0200

    reimplemented mock tests
    Mock server tests are ignored right now. for further investigations

--- testing/jormungandr-integration-tests/src/mock/testing/server_tests.rs
+++ testing/jormungandr-integration-tests/src/mock/testing/server_tests.rs
@@ -3,61 +3,24 @@ use crate::{
         configuration, file_utils, jormungandr::logger::Level, jormungandr::starter::Starter,
     },
     mock::{
-        server::{self, MethodType, MockLogger, ProtocolVersion},
-        testing::{setup::bootstrap_node_with_peer, setup::build_configuration},
+        client::JormungandrClient,
+        server::{
+            self, JormungandrServerImpl, MethodType, MockLogger, NodeServer, ProtocolVersion,
+        },
+        testing::setup::{
+            bootstrap_node_with_peer, build_configuration, start_mock, MockController, MockExitCode,
+        },
     },
 };
 use chain_core::property::FromStr;
 use chain_impl_mockchain::key::Hash;
+use futures_util::future::FutureExt;
 use std::{
     thread::{self, JoinHandle},
     time::{Duration, Instant},
 };
-
-#[derive(Clone, Debug, PartialEq)]
-pub enum MockExitCode {
-    Timeout,
-    Success,
-}
-
-pub fn start_mock<F: 'static + std::marker::Send>(
-    mock_port: u16,
-    genesis_hash: Hash,
-    tip_hash: Hash,
-    protocol_version: ProtocolVersion,
-    stop_func: F,
-) -> JoinHandle<MockExitCode>
-where
-    F: Fn(&MockLogger) -> bool,
-{
-    let log_file = file_utils::get_path_in_temp("mock.log");
-    let logger = MockLogger::new(log_file.clone());
-
-    println!("mock will log into location: {:?}", log_file);
-
-    thread::spawn(move || {
-        let _server = server::start(
-            mock_port,
-            genesis_hash,
-            tip_hash,
-            protocol_version,
-            log_file.clone(),
-        );
-
-        let start = Instant::now();
-        let timeout = Duration::from_secs(120);
-
-        loop {
-            thread::sleep(Duration::from_secs(1));
-            if start.elapsed() > timeout {
-                return MockExitCode::Timeout;
-            }
-            if stop_func(&logger) {
-                return MockExitCode::Success;
-            }
-        }
-    })
-}
+use tokio::sync::oneshot;
+use tonic::transport::Server;
 
 pub fn fake_hash() -> Hash {
     Hash::from_str("efe2d4e5c4ad84b8e67e7b5676fff41cad5902a60b8cb6f072f42d7c7d26c944").unwrap()
@@ -68,59 +31,60 @@ pub fn peer_addr(port: u16) -> Option<String> {
 }
 
 // L1005 Handshake version discrepancy
-// Mock broken: https://github.com/stepancheg/grpc-rust/issues/184
+#[tokio::test]
 #[ignore]
-#[test]
-pub fn wrong_protocol() {
+pub async fn wrong_protocol() {
     let mock_port = configuration::get_available_port();
-    let (server, config) = bootstrap_node_with_peer(mock_port);
+    let config = build_configuration(mock_port);
 
-    let mock_thread = start_mock(
+    let mock_controller = start_mock(
         mock_port,
         Hash::from_str(&config.genesis_block_hash()).unwrap(),
         fake_hash(),
         ProtocolVersion::Bft,
-        |logger: &MockLogger| logger.executed_at_least_once(MethodType::Handshake),
     );
+    tokio::time::delay_for(Duration::from_millis(1000)).await;
 
-    assert_eq!(
-        mock_thread.join().expect("mock thread error"),
-        MockExitCode::Success,
-        "Mock server timeout while waiting to stop event be triggered"
-    );
+    let server = Starter::new().config(config.clone()).start_async().unwrap();
 
+    let mock_result = mock_controller.finish_and_verify_that(|mock_verifier| {
+        mock_verifier.method_executed_at_least_once(MethodType::Handshake)
+    });
     server.shutdown();
-
-    assert!(
-        server.logger.get_log_entries().any(|x| {
-            x.msg == "protocol handshake with peer failed"
-                && x.reason_contains("unsupported protocol version 0")
-                && x.peer_addr == peer_addr(mock_port)
-                && x.level == Level::INFO
-        }),
-        format!("Log content: {}", server.logger.get_log_content())
+    assert_eq!(
+        mock_result,
+        MockExitCode::Success,
+        "Handshake with mock never happened"
     );
 }
 
 // L1004 Handshake hash discrepancy
-// Mock broken: https://github.com/stepancheg/grpc-rust/issues/184
+#[tokio::test]
 #[ignore]
-#[test]
-pub fn wrong_genesis_hash() {
+pub async fn wrong_genesis_hash() {
     let mock_port = configuration::get_available_port();
-    let mock_thread = start_mock(
+    let config = build_configuration(mock_port);
+
+    let mock_controller = start_mock(
         mock_port,
         fake_hash(),
         fake_hash(),
         ProtocolVersion::GenesisPraos,
-        |logger: &MockLogger| logger.executed_at_least_once(MethodType::Handshake),
+    );
+    tokio::time::delay_for(Duration::from_millis(1000)).await;
+
+    let server = Starter::new().config(config.clone()).start_async().unwrap();
+
+    let mock_result = mock_controller.finish_and_verify_that(|mock_verifier| {
+        mock_verifier.method_executed_at_least_once(MethodType::Handshake)
+    });
+    server.shutdown();
+    assert_eq!(
+        mock_result,
+        MockExitCode::Success,
+        "Handshake with mock never happened"
     );
 
-    let (server, _) = bootstrap_node_with_peer(mock_port);
-    assert_eq!(
-        mock_thread.join().expect("mock thread error"),
-        MockExitCode::Success
-    );
     server.shutdown();
     assert!(
         server.logger.get_log_entries().any(|x| {
@@ -134,28 +98,33 @@ pub fn wrong_genesis_hash() {
 }
 
 // L1002 Handshake compatible
+#[tokio::test]
 #[ignore]
-#[test]
-pub fn handshake_ok() {
+pub async fn handshake_ok() {
     let mock_port = configuration::get_available_port();
     let config = build_configuration(mock_port);
 
-    let mock_thread = start_mock(
+    let mock_controller = start_mock(
         mock_port,
         Hash::from_str(&config.genesis_block_hash()).unwrap(),
         fake_hash(),
         ProtocolVersion::GenesisPraos,
-        |logger: &MockLogger| logger.executed_at_least_once(MethodType::Handshake),
     );
+    tokio::time::delay_for(Duration::from_millis(1000)).await;
 
-    let server = Starter::new().config(config.clone()).start().unwrap();
-    assert_eq!(
-        mock_thread.join().expect("mock thread error"),
-        MockExitCode::Success
-    );
+    let server = Starter::new().config(config.clone()).start_async().unwrap();
 
+    let mock_result = mock_controller.finish_and_verify_that(|mock_verifier| {
+        mock_verifier.method_executed_at_least_once(MethodType::Handshake)
+    });
     server.shutdown();
-    server.logger.print_raw_log();
+
+    assert_eq!(
+        mock_result,
+        MockExitCode::Success,
+        "Handshake with mock never happened"
+    );
+
     assert!(!server
         .logger
         .get_log_entries()
