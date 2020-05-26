b765b6f67e765986363a558f3774ade42947d231
dkijania <dariusz.kijania@iohk.io>
2020-05-26 13:35:45 +0200

    added resend and verify mechanism to sender

--- testing/jormungandr-testing-utils/src/testing/fragments/sender.rs
+++ testing/jormungandr-testing-utils/src/testing/fragments/sender.rs
@@ -1,61 +1,59 @@
-use chain_impl_mockchain::{
-    fee::LinearFee,
-    fragment::{Fragment, FragmentId},
+use crate::{
+    testing::{
+        assure_node_in_sync,
+        fragments::node::{FragmentNode, MemPoolCheck},
+        FragmentSenderSetup, FragmentVerifier, SyncNode, SyncNodeError, SyncWaitParams,
+    },
+    wallet::Wallet,
 };
+use chain_impl_mockchain::{fee::LinearFee, fragment::Fragment};
 use jormungandr_lib::{
     crypto::hash::Hash,
     interfaces::{FragmentStatus, Value},
 };
 
-use crate::{
-    testing::{
-        fragments::node::{FragmentNode, MemPoolCheck},
-        FragmentVerifier, FragmentVerifierError,
-    },
-    wallet::Wallet,
-};
-
-use std::{thread, time::Duration};
+use std::time::Duration;
 
 use thiserror::Error;
 
 #[derive(Error, Debug)]
 pub enum FragmentSenderError {
-    #[error("fragment sent to node: {alias} is not in block '{status:?}'. logs: {logs}")]
+    #[error("fragment sent to node: {alias} is not in block due to  '{reason}'. logs: {logs}")]
     FragmentNotInBlock {
         alias: String,
-        status: FragmentStatus,
+        reason: String,
         logs: String,
     },
-    #[error("transaction already balanced")]
-    FragmentIsPendingForTooLong,
     #[error(
-        "fragment sent to node: {alias} is not in in fragment pool :({fragment_id}). logs: {logs}"
+        "Too many attempts failed ({attempts}) while trying to send fragment to node: {alias}"
     )]
-    FragmentNoInMemPoolLogs {
-        alias: String,
-        fragment_id: FragmentId,
-        logs: String,
-    },
+    TooManyAttemptsFailed { attempts: u8, alias: String },
     #[error("fragment verifier error")]
     FragmentVerifierError(#[from] super::FragmentVerifierError),
     #[error("cannot send fragment")]
     SendFragmentError(#[from] super::node::FragmentNodeError),
+    #[error("cannot sync node before sending fragment")]
+    SyncNodeError(#[from] crate::testing::SyncNodeError),
     #[error("wallet error")]
     WalletError(#[from] crate::wallet::WalletError),
 }
 
-pub struct FragmentSender {
+pub struct FragmentSender<'a> {
     block0_hash: Hash,
     fees: LinearFee,
+    setup: FragmentSenderSetup<'a>,
 }
 
-impl FragmentSender {
-    pub fn new(block0_hash: Hash, fees: LinearFee) -> Self {
-        Self { block0_hash, fees }
+impl<'a> FragmentSender<'a> {
+    pub fn new(block0_hash: Hash, fees: LinearFee, setup: FragmentSenderSetup<'a>) -> Self {
+        Self {
+            block0_hash,
+            fees,
+            setup,
+        }
     }
 
-    pub fn send_transaction<A: FragmentNode + ?Sized>(
+    pub fn send_transaction<A: FragmentNode + SyncNode + Sized>(
         &self,
         from: &mut Wallet,
         to: &Wallet,
@@ -64,10 +62,10 @@ impl FragmentSender {
     ) -> Result<MemPoolCheck, FragmentSenderError> {
         let address = to.address();
         let fragment = from.transaction_to(&self.block0_hash, &self.fees, address, value)?;
-        Ok(via.send_fragment(fragment)?)
+        self.send_fragment(fragment, via)
     }
 
-    pub fn send_transactions_ignore_errors<A: FragmentNode + ?Sized>(
+    pub fn send_transactions<A: FragmentNode + SyncNode + Sized>(
         &self,
         n: u32,
         mut wallet1: &mut Wallet,
@@ -76,16 +74,12 @@ impl FragmentSender {
         value: Value,
     ) -> Result<(), FragmentSenderError> {
         for _ in 0..n {
-            let check = self.send_transaction(&mut wallet1, &wallet2, node, value);
-            if let Err(err) = check {
-                println!("ignoring error : {:?}", err);
-            }
-            thread::sleep(Duration::from_secs(1));
+            self.send_transaction(&mut wallet1, &wallet2, node, value)?;
         }
         Ok(())
     }
 
-    pub fn send_transactions_round_trip<A: FragmentNode + ?Sized>(
+    pub fn send_transactions_round_trip<A: FragmentNode + SyncNode + Sized>(
         &self,
         n: u32,
         mut wallet1: &mut Wallet,
@@ -93,26 +87,81 @@ impl FragmentSender {
         node: &A,
         value: Value,
     ) -> Result<(), FragmentSenderError> {
-        let verifier = FragmentVerifier;
-
         for _ in 0..n {
-            let check = self.send_transaction(&mut wallet1, &wallet2, node, value.clone())?;
-            verifier.wait_and_verify_is_in_block(Duration::from_secs(2), check, node)?;
+            self.send_transaction(&mut wallet1, &wallet2, node, value.clone())?;
             wallet1.confirm_transaction();
-            let check = self.send_transaction(&mut wallet2, &wallet1, node, value.clone())?;
-            verifier.wait_and_verify_is_in_block(Duration::from_secs(2), check, node)?;
+            self.send_transaction(&mut wallet2, &wallet1, node, value.clone())?;
             wallet2.confirm_transaction();
         }
         Ok(())
     }
 
-    pub fn send_fragment_and_verify_is_in_block<A: FragmentNode + ?Sized>(
+    fn verify<A: FragmentNode + SyncNode + Sized>(
+        &self,
+        check: &MemPoolCheck,
+        node: &A,
+    ) -> Result<(), FragmentSenderError> {
+        let verifier = FragmentVerifier;
+        match verifier.wait_fragment(Duration::from_secs(2), check.clone(), node)? {
+            FragmentStatus::Rejected { reason } => Err(FragmentSenderError::FragmentNotInBlock {
+                alias: FragmentNode::alias(node).to_string(),
+                reason: reason,
+                logs: FragmentNode::log_content(node),
+            }),
+            FragmentStatus::InABlock { .. } => Ok(()),
+            _ => unimplemented!(),
+        }
+    }
+
+    pub fn send_fragment<A: FragmentNode + SyncNode + Sized>(
         &self,
         fragment: Fragment,
         node: &A,
-    ) -> Result<(), FragmentVerifierError> {
-        let verifier = FragmentVerifier;
-        let check = node.send_fragment(fragment)?;
-        verifier.wait_and_verify_is_in_block(Duration::from_secs(2), check, node)
+    ) -> Result<MemPoolCheck, FragmentSenderError> {
+        self.wait_for_node_sync_if_enabled(node)
+            .map_err(|e| FragmentSenderError::SyncNodeError(e))?;
+        for _ in 0..self.setup.attempts_count() {
+            let check = node
+                .send_fragment(fragment.clone())
+                .map_err(|e| FragmentSenderError::SendFragmentError(e))?;
+            if self.setup.no_verify() {
+                return Ok(check);
+            }
+
+            if let Err(err) = self.verify(&check, node) {
+                if self.setup.ignore_any_errors() {
+                    println!("Ignoring error: {:?}", err);
+                    return Ok(check);
+                }
+                println!(
+                    "Error while sending fragment {:?}. Retrying if possible...",
+                    err
+                );
+                continue;
+            }
+            return Ok(check);
+        }
+
+        Err(FragmentSenderError::TooManyAttemptsFailed {
+            attempts: self.setup.attempts_count(),
+            alias: FragmentNode::alias(node).to_string(),
+        })
+    }
+
+    fn wait_for_node_sync_if_enabled<A: FragmentNode + SyncNode + Sized>(
+        &self,
+        node: &A,
+    ) -> Result<(), SyncNodeError> {
+        if self.setup.no_sync_nodes() {
+            return Ok(());
+        }
+
+        let nodes_length = (self.setup.sync_nodes().len() + 1) as u64;
+        assure_node_in_sync(
+            node,
+            self.setup.sync_nodes(),
+            SyncWaitParams::network_size(nodes_length, 2).into(),
+            "waiting for node to be in sync before sending transaction",
+        )
     }
 }
---
+++ testing/jormungandr-testing-utils/src/testing/fragments/setup.rs
@@ -0,0 +1,75 @@
+use crate::testing::SyncNode;
+
+pub struct FragmentSenderSetup<'a> {
+    pub resend_on_error: Option<u8>,
+    pub sync_nodes: Vec<&'a dyn SyncNode>,
+    pub ignore_any_errors: bool,
+    pub no_verify: bool,
+}
+
+impl<'a> FragmentSenderSetup<'a> {
+    pub const NO_VERIFY: FragmentSenderSetup<'a> = FragmentSenderSetup {
+        resend_on_error: None,
+        sync_nodes: vec![],
+        ignore_any_errors: false,
+        no_verify: true,
+    };
+
+    pub const RESEND_3_TIMES: FragmentSenderSetup<'a> = FragmentSenderSetup {
+        resend_on_error: Some(3),
+        sync_nodes: vec![],
+        ignore_any_errors: false,
+        no_verify: false,
+    };
+
+    pub fn resend_3_times_and_sync_with(sync_nodes: Vec<&'a dyn SyncNode>) -> Self {
+        Self {
+            resend_on_error: Some(3),
+            sync_nodes: sync_nodes,
+            ignore_any_errors: false,
+            no_verify: false,
+        }
+    }
+
+    pub fn new() -> Self {
+        Self {
+            resend_on_error: None,
+            sync_nodes: Vec::new(),
+            ignore_any_errors: false,
+            no_verify: false,
+        }
+    }
+
+    pub fn resend_on_error(&self) -> Option<u8> {
+        self.resend_on_error.clone()
+    }
+
+    pub fn sync_nodes(&self) -> Vec<&'a dyn SyncNode> {
+        self.sync_nodes.clone()
+    }
+
+    pub fn no_sync_nodes(&self) -> bool {
+        self.sync_nodes().len() == 0
+    }
+
+    pub fn ignore_any_errors(&self) -> bool {
+        self.ignore_any_errors
+    }
+
+    pub fn attempts_count(&self) -> u8 {
+        match self.resend_on_error {
+            Some(resend_counter) => resend_counter + 1,
+            None => 1,
+        }
+    }
+
+    pub fn no_verify(&self) -> bool {
+        self.no_verify
+    }
+}
+
+impl<'a> Default for FragmentSenderSetup<'a> {
+    fn default() -> FragmentSenderSetup<'a> {
+        FragmentSenderSetup::new()
+    }
+}
