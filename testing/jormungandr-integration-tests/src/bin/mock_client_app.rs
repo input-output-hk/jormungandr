9c222e809ba21fa57167c233ad68f44fa24d1836
dkijania <dariusz.kijania@iohk.io>
2020-06-19 01:55:17 +0200

    expose ip in mock_client_app bin as cmd arg

--- testing/jormungandr-integration-tests/src/bin/mock_client_app.rs
+++ testing/jormungandr-integration-tests/src/bin/mock_client_app.rs
@@ -4,8 +4,8 @@ use std::env;
 #[tokio::main]
 async fn main() {
     let args: Vec<String> = env::args().collect();
-    let port: u16 = args[1].parse().unwrap();
-    let client = JormungandrClient::new("127.0.0.1", port);
+    let port: u16 = args[2].parse().unwrap();
+    let client = JormungandrClient::new(&args[1], port);
     let response = client.handshake().await;
     println!("{:?}", response);
 }
