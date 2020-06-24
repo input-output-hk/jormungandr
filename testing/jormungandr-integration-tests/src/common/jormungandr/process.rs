e3b60f382990fb410dd01bbff8e596c2b2cad093
Enzo Cioppettini <ecioppettini@atixlabs.com>
2020-06-24 17:05:54 -0300

    add notifier test submodule

--- testing/jormungandr-integration-tests/src/common/jormungandr/process.rs
+++ testing/jormungandr-integration-tests/src/common/jormungandr/process.rs
@@ -1,4 +1,4 @@
-use super::JormungandrError;
+use super::{notifier, JormungandrError};
 use crate::common::jcli_wrapper;
 use assert_fs::{fixture::ChildPath, TempDir};
 use chain_impl_mockchain::fee::LinearFee;
@@ -64,6 +64,10 @@ impl JormungandrProcess {
         JormungandrRest::new_with_cert(self.rest_uri(), cert)
     }
 
+    pub fn notifier(&self) -> notifier::JormungandrNotifier {
+        notifier::JormungandrNotifier::new(self.notifier_url())
+    }
+
     pub fn shutdown(&self) {
         jcli_wrapper::assert_rest_shutdown(&self.rest_uri());
     }
@@ -120,6 +124,10 @@ impl JormungandrProcess {
 
     pub fn rest_uri(&self) -> String {
         uri_from_socket_addr(self.rest_socket_addr)
+    }
+
+    pub fn notifier_url(&self) -> url::Url {
+        notifier::uri_from_socket_addr(self.rest_socket_addr)
     }
 
     pub fn fees(&self) -> LinearFee {
