72822134d3e9d15e235f9447d0de6671186b8d8d
Enzo Cioppettini <ecioppettini@atixlabs.com>
2020-06-29 14:37:52 -0300

    add notifier max connections tests
    add also setting to configuration builder, in order to test it, and
    improve the panic handling of the notifier client

--- testing/jormungandr-integration-tests/src/jormungandr/notifier/mod.rs
+++ testing/jormungandr-integration-tests/src/jormungandr/notifier/mod.rs
@@ -1,10 +1,16 @@
 use crate::common::{
-    jormungandr::{notifier::JsonMessage, ConfigurationBuilder},
+    jormungandr::{
+        notifier::{JsonMessage, NotifierMessage},
+        ConfigurationBuilder,
+    },
     startup,
 };
 
 use jormungandr_lib::interfaces::ActiveSlotCoefficient;
-use std::sync::{Arc, Condvar, Mutex};
+use std::sync::{
+    atomic::{AtomicBool, AtomicUsize, Ordering},
+    Arc,
+};
 
 #[test]
 pub fn notifier_shows_the_same_tip_as_rest() {
@@ -19,27 +25,61 @@ pub fn notifier_shows_the_same_tip_as_rest() {
 
     let mut notifier = jormungandr.notifier();
 
-    #[allow(clippy::mutex_atomic)]
-    let pair = Arc::new((Mutex::new(0usize), Condvar::new()));
-    let pair2 = pair.clone();
+    let mut counter = Arc::new(AtomicUsize::new(0));
 
     notifier
         .new_client(move |msg| {
-            if let JsonMessage::NewTip(hash) = msg {
+            if let NotifierMessage::JsonMessage(JsonMessage::NewTip(hash)) = msg {
                 let rest_tip = rest.tip().expect("couldn't get tip from rest");
                 assert_eq!(hash, rest_tip);
 
-                let (lock, cvar) = &*pair2;
-                let mut done = lock.lock().unwrap();
-                *done += 1;
-                cvar.notify_one();
+                let count = Arc::get_mut(&mut counter).unwrap();
+
+                let count = count.fetch_add(1, Ordering::AcqRel);
+
+                if count == 5 {
+                    return false;
+                }
             };
+            true
         })
         .expect("couldn't connect client");
 
-    let (lock, cvar) = &*pair;
-    let mut done = lock.lock().unwrap();
-    while !*done < 5 {
-        done = cvar.wait(done).unwrap();
-    }
+    notifier.wait_all().unwrap();
+}
+
+#[test]
+pub fn notifier_fails_with_more_than_max_connections() {
+    let faucet = startup::create_new_account_address();
+
+    let mut config = ConfigurationBuilder::new();
+    config
+        .with_consensus_genesis_praos_active_slot_coeff(ActiveSlotCoefficient::MAXIMUM)
+        .with_notifier_max_connections(1);
+
+    let (jormungandr, _) = startup::start_stake_pool(&[faucet], &[], &mut config).unwrap();
+
+    let mut notifier = jormungandr.notifier();
+
+    let waiting = Arc::new(AtomicBool::new(true));
+    let waiting1 = Arc::clone(&waiting);
+
+    notifier
+        .new_client(move |_msg| waiting1.load(Ordering::Acquire))
+        .expect("couldn't connect client");
+
+    notifier
+        .new_client(move |msg| {
+            match msg {
+                NotifierMessage::MaxConnectionsReached => {
+                    waiting.store(false, Ordering::Release);
+                }
+                _ => unreachable!("shouldn't be able to connect"),
+            }
+
+            true
+        })
+        .expect("couldn't connect client");
+
+    notifier.wait_all().unwrap();
 }
