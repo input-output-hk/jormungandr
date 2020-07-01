bcb116a1a926e9c7fc28dbcd8c5192cd1411825f
dkijania <dariusz.kijania@iohk.io>
2020-07-01 09:37:56 +0200

    updated tls test

--- testing/jormungandr-integration-tests/src/jormungandr/tls.rs
+++ testing/jormungandr-integration-tests/src/jormungandr/tls.rs
@@ -1,16 +1,21 @@
-use crate::common::{
-    file_assert, file_utils,
-    jormungandr::{ConfigurationBuilder, Starter},
-};
-use jormungandr_lib::{interfaces::Tls, testing::Openssl};
+use crate::common::jormungandr::{ConfigurationBuilder, Starter, StartupVerificationMode};
+use assert_fs::TempDir;
+use assert_fs::{assert::PathAssert, fixture::PathChild};
+use jormungandr_lib::interfaces::Tls;
+use jormungandr_testing_utils::testing::Openssl;
+
 #[test]
+#[ignore]
 #[cfg(any(unix, windows))]
 pub fn test_rest_tls_config() {
+    let temp_dir = TempDir::new().unwrap().into_persistent();
+
     let openssl = Openssl::new().expect("no openssla installed.");
-    let prv_key_file = file_utils::get_path_in_temp("prv.key");
-    let pk8_key_file = file_utils::get_path_in_temp("prv.pk8");
-    let csr_cert_file = file_utils::get_path_in_temp("cert.csr");
-    let cert_file = file_utils::get_path_in_temp("cert.crt");
+    let prv_key_file = temp_dir.child("prv.key");
+    let pk8_key_file = temp_dir.child("prv.pk8");
+    let csr_cert_file = temp_dir.child("cert.csr");
+    let cert_file = temp_dir.child("cert.crt");
+    let der_file = temp_dir.child("cert.der");
 
     println!(
         "{}",
@@ -36,20 +41,30 @@ pub fn test_rest_tls_config() {
             .x509(&prv_key_file, &csr_cert_file, &cert_file)
             .expect("cannot generate a self-signed certificate for private key")
     );
+    println!(
+        "{}",
+        openssl
+            .convert_to_der(&cert_file, &der_file)
+            .expect("cannot convert cert file to der file")
+    );
 
-    file_assert::assert_file_exists_and_not_empty(&prv_key_file);
-    file_assert::assert_file_exists_and_not_empty(&cert_file);
+    prv_key_file.assert(crate::predicate::file_exists_and_not_empty());
+    cert_file.assert(crate::predicate::file_exists_and_not_empty());
 
     let config = ConfigurationBuilder::new()
         .with_rest_tls_config(Tls {
-            cert_file: cert_file.as_os_str().to_str().unwrap().to_owned(),
-            priv_key_file: pk8_key_file.as_os_str().to_str().unwrap().to_owned(),
+            cert_file: cert_file.path().as_os_str().to_str().unwrap().to_owned(),
+            priv_key_file: pk8_key_file.path().as_os_str().to_str().unwrap().to_owned(),
         })
-        .build();
+        .build(&temp_dir);
 
-    let jormungandr = Starter::new().config(config).start().unwrap();
-
+    let jormungandr = Starter::new()
+        .config(config)
+        .verify_by(StartupVerificationMode::Log)
+        .start()
+        .unwrap();
+    println!("Bootstrapped");
     jormungandr.assert_no_errors_in_log();
 
-    println!("{:?}", jormungandr.secure_rest(cert_file).stats().unwrap());
+    println!("{:?}", jormungandr.secure_rest(&der_file).stats().unwrap());
 }
