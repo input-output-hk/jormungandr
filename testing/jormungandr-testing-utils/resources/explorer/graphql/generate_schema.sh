6f5f267b862a562b9079cb91a964ee69a8555f9d
dkijania <dariusz.kijania@iohk.io>
2020-09-02 22:12:02 +0200

    added script to generate schema

---
+++ testing/jormungandr-testing-utils/resources/explorer/graphql/generate_schema.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+
+if [ $# -ne 2 ]; then
+    echo "usage: $0 <ENDPOINT> <OUTPUT_SCHEMA>"
+    echo "    <ENDPOINT>  graphql endpoint (http://127.0.0.1:8000/api/v0/explorer/graphql)"
+    echo "    <OUTPUT_SCHEMA> schema output location (schema.graphql)"
+    exit 1
+fi
+
+echo "================Explorer schema generator ================="
+echo "This Script will dump schema into: '$2'"
+
+echo $(npm install -g get-graphql-schema)
+echo $(get-graphql-schema $1 > $2)
+echo "================ DONE ====================================="
