55ebf581e6e17953e143f036d8f9177311fa7d59
dkijania <dariusz.kijania@iohk.io>
2020-09-03 06:29:36 +0200

    added test to generate schema

--- testing/jormungandr-integration-tests/src/jormungandr/explorer/mod.rs
+++ testing/jormungandr-integration-tests/src/jormungandr/explorer/mod.rs
@@ -1,13 +1,57 @@
 use crate::common::{
-    jcli_wrapper, jormungandr::ConfigurationBuilder, process_utils::Wait, startup,
+    jcli_wrapper,
+    jormungandr::{ConfigurationBuilder, Starter},
+    process_utils::Wait,
+    startup,
     transaction_utils::TransactionHash,
 };
-use jormungandr_lib::{crypto::hash::Hash, interfaces::ActiveSlotCoefficient};
+use assert_fs::assert::PathAssert;
+use assert_fs::fixture::PathChild;
+use assert_fs::TempDir;
+use chain_impl_mockchain::key::Hash;
+use jormungandr_lib::interfaces::ActiveSlotCoefficient;
 use std::str::FromStr;
-use std::time::Duration;
+use std::{path::PathBuf, process::Command, time::Duration};
+/// test checks if there is upto date schema
+/// prereq:
+/// -npm
+/// read more: https://github.com/prisma-labs/get-graphql-schema
+#[test]
+#[cfg(feature = "explorer-schema-gen")]
+#[cfg(unix)]
+pub fn explorer_schema_diff_test() {
+    let temp_dir = TempDir::new().unwrap();
+    let config = ConfigurationBuilder::new().with_explorer().build(&temp_dir);
+
+    let jormungandr = Starter::new()
+        .temp_dir(temp_dir)
+        .config(config)
+        .start()
+        .unwrap();
+
+    let schema_temp_dir = TempDir::new().unwrap();
+    let actual_schema_path = schema_temp_dir.child("new_schema.graphql");
+
+    Command::new("../jormungandr-testing-utils/resources/explorer/graphql/generate_schema.sh")
+        .args(&[
+            jormungandr.explorer().uri(),
+            actual_schema_path
+                .path()
+                .as_os_str()
+                .to_str()
+                .unwrap()
+                .to_string(),
+        ])
+        .spawn()
+        .unwrap()
+        .wait()
+        .unwrap();
+
+    jormungandr_testing_utils::testing::node::explorer::compare_schema(actual_schema_path.path());
+}
 
 #[test]
-pub fn explorer_test() {
+pub fn explorer_sanity_test() {
     let mut faucet = startup::create_new_account_address();
     let receiver = startup::create_new_account_address();
 
