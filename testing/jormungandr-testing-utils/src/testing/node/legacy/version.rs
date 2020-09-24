ad7f45e726564bbf26ca6ea9daee861af129d312
dkijania <dariusz.kijania@iohk.io>
2020-09-24 16:34:01 +0200

    used semver instead of inhouse Version

--- testing/jormungandr-testing-utils/src/testing/node/legacy/version.rs
+++ testing/jormungandr-testing-utils/src/testing/node/legacy/version.rs
@@ -1,6 +1,6 @@
-use std::{cmp::Ordering, fmt, num::ParseIntError, str::FromStr};
 pub use semver::Version;
+use std::{cmp::Ordering, fmt, num::ParseIntError, str::FromStr};
 
-pub const fn version_0_8_19() -> Version {
+pub fn version_0_8_19() -> Version {
     Version::new(0, 8, 19)
 }
