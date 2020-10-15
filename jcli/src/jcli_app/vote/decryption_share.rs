7e1b63e10cf45d2c80fd3858ce0e6c134fcb4f03
Yevhenii Babichenko <eugene.babichenko@iohk.io>
2020-10-15 12:29:24 +0300

    jcli decryption_shares: employ io::read_line()

--- jcli/src/jcli_app/vote/decryption_share.rs
+++ jcli/src/jcli_app/vote/decryption_share.rs
@@ -1,12 +1,8 @@
 use super::Error;
-use crate::jcli_app::utils::OutputFormat;
+use crate::jcli_app::utils::{io, OutputFormat};
 use chain_vote::{OpeningVoteKey, Tally};
 use serde::Serialize;
-use std::{
-    fs::File,
-    io::{stdin, Read},
-    path::PathBuf,
-};
+use std::path::PathBuf;
 use structopt::StructOpt;
 
 /// Create the decryption share for decrypting the tally of private voting.
@@ -32,22 +28,13 @@ struct Output {
 
 impl TallyDecryptionShare {
     pub fn exec(&self) -> Result<(), Error> {
-        let encrypted_tally_hex = if let Some(encrypted_tally_path) = &self.encrypted_tally {
-            let mut data = Vec::new();
-            File::open(encrypted_tally_path)?.read_to_end(&mut data)?;
-            data
-        } else {
-            let mut data = Vec::new();
-            stdin().read_to_end(&mut data)?;
-            data
-        };
+        let encrypted_tally_hex = io::read_line(&self.encrypted_tally)?;
         let encrypted_tally_bytes = hex::decode(encrypted_tally_hex)?;
         let encrypted_tally =
             Tally::from_bytes(&encrypted_tally_bytes).ok_or(Error::EncryptedTallyRead)?;
 
         let decryption_key = {
-            let mut data = Vec::new();
-            File::open(&self.decryption_key)?.read_to_end(&mut data)?;
+            let data = io::read_line(&Some(&self.decryption_key))?;
             let mut bytes = [0u8; 32];
             hex::decode_to_slice(data, &mut bytes as &mut [u8])?;
             OpeningVoteKey::from_bytes(&bytes).ok_or(Error::DecryptionKeyRead)?
