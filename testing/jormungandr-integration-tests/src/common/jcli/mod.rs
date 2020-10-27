9f52942a2107d0d0721246fdd9ae9b7464cc563e
dkijania <dariusz.kijania@iohk.io>
2020-10-27 22:33:35 +0100

    implement jcli as command executor

--- testing/jormungandr-integration-tests/src/common/jcli/mod.rs
+++ testing/jormungandr-integration-tests/src/common/jcli/mod.rs
@@ -1,11 +1,23 @@
 mod api;
 mod command;
+mod services;
 
-use command::JCliCommand;
+pub use command::JCliCommand;
+pub use services::FragmentCheck;
 
+pub use services::Error;
+
+use super::jormungandr::JormungandrProcess;
 use crate::common::configuration;
-use api::{JCliGenesis, JCliKey};
-use std::{path::PathBuf, process::Command};
+use api::{Address, Certificate, Genesis, Key, Rest, Transaction};
+use assert_fs::TempDir;
+use chain_impl_mockchain::fragment::FragmentId;
+use jormungandr_lib::crypto::hash::Hash;
+use services::{CertificateBuilder, FragmentSender, FragmentsCheck, TransactionBuilder};
+use std::{
+    path::{Path, PathBuf},
+    process::Command,
+};
 
 #[derive(Clone, Debug)]
 pub struct JCli {
@@ -23,15 +35,59 @@ impl JCli {
         Self { exe }
     }
 
-    pub fn genesis(&self) -> JCliGenesis {
-        let command = Command::new(self.exe.clone());
-        let jcli_command = JCliCommand::new(command);
-        JCliGenesis::new(jcli_command.genesis())
+    pub fn path(&self) -> &Path {
+        self.exe.as_path()
     }
 
-    pub fn key(&self) -> JCliKey {
+    pub fn genesis(&self) -> Genesis {
         let command = Command::new(self.exe.clone());
         let jcli_command = JCliCommand::new(command);
-        JCliKey::new(jcli_command.key())
+        Genesis::new(jcli_command.genesis())
+    }
+
+    pub fn key(&self) -> Key {
+        let command = Command::new(self.exe.clone());
+        let jcli_command = JCliCommand::new(command);
+        Key::new(jcli_command.key())
+    }
+
+    pub fn address(&self) -> Address {
+        let command = Command::new(self.exe.clone());
+        let jcli_command = JCliCommand::new(command);
+        Address::new(jcli_command.address())
+    }
+
+    pub fn rest(&self) -> Rest {
+        let command = Command::new(self.exe.clone());
+        let jcli_command = JCliCommand::new(command);
+        Rest::new(jcli_command.rest())
+    }
+
+    pub fn transaction(&self) -> Transaction {
+        let command = Command::new(self.exe.clone());
+        let jcli_command = JCliCommand::new(command);
+        Transaction::new(jcli_command.transaction())
+    }
+
+    pub fn certificate(&self) -> Certificate {
+        let command = Command::new(self.exe.clone());
+        let jcli_command = JCliCommand::new(command);
+        Certificate::new(jcli_command.certificate())
+    }
+
+    pub fn fragment_sender<'a>(&self, jormungandr: &'a JormungandrProcess) -> FragmentSender<'a> {
+        FragmentSender::new(self.clone(), jormungandr)
+    }
+
+    pub fn transaction_builder(&self, genesis_hash: Hash) -> TransactionBuilder {
+        TransactionBuilder::new(self.clone(), genesis_hash)
+    }
+
+    pub fn certificate_builder(&self, genesis_hash: Hash) -> CertificateBuilder {
+        CertificateBuilder::new(self.clone())
+    }
+
+    pub fn fragments_checker<'a>(&self, jormungandr: &'a JormungandrProcess) -> FragmentsCheck<'a> {
+        FragmentsCheck::new(self.clone(), jormungandr)
     }
 }
