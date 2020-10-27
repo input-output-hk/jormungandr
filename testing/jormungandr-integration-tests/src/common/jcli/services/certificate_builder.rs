21a4b086e89e411c08a18732bdfa0c734cd62644
dkijania <dariusz.kijania@iohk.io>
2020-10-27 23:22:46 +0100

    added services for complex jcli commands

---
+++ testing/jormungandr-integration-tests/src/common/jcli/services/certificate_builder.rs
@@ -0,0 +1,102 @@
+#![allow(dead_code)]
+
+use crate::common::{data::witness::Witness, jcli::JCli};
+use assert_cmd::assert::OutputAssertExt;
+use assert_fs::fixture::ChildPath;
+use assert_fs::prelude::*;
+use assert_fs::TempDir;
+use chain_core::property::Deserialize;
+use chain_impl_mockchain::{fee::LinearFee, fragment::Fragment};
+use jormungandr_lib::{
+    crypto::hash::Hash,
+    interfaces::{LegacyUTxO, TaxType, UTxOInfo, Value},
+};
+use jormungandr_testing_utils::testing::process::ProcessOutput;
+use jormungandr_testing_utils::wallet::Wallet;
+use jortestkit::{file, process::output_extensions::ProcessOutput as _};
+use std::path::{Path, PathBuf};
+
+pub struct CertificateBuilder {
+    jcli: JCli,
+}
+
+impl CertificateBuilder {
+    pub fn new(jcli: JCli) -> Self {
+        Self { jcli }
+    }
+    pub fn new_signed_stake_pool_cert(
+        &self,
+        pool_kes_pk: &str,
+        pool_vrf_pk: &str,
+        stake_key_file: &Path,
+        start_validity: u32,
+        management_threshold: u32,
+        owner_pk: &str,
+        tax_type: Option<TaxType>,
+    ) {
+        let temp_dir = TempDir::new().unwrap();
+
+        let stake_pool_cert = self.jcli.certificate().new_stake_pool_registration(
+            &pool_kes_pk,
+            &pool_vrf_pk,
+            start_validity,
+            management_threshold,
+            owner_pk,
+            tax_type,
+        );
+        let stake_pool_cert_file = temp_dir.child("stake_pool.cert");
+        stake_pool_cert_file.write_str(&stake_pool_cert).unwrap();
+
+        let stake_pool_signcert_file = temp_dir.child("stake_pool.signcert");
+        self.jcli.certificate().sign(
+            &stake_key_file,
+            stake_pool_cert_file.path(),
+            stake_pool_signcert_file.path(),
+        );
+    }
+
+    pub fn new_signed_stake_pool_delegation(
+        mut self,
+        stake_pool_id: &str,
+        stake_key_pub: &str,
+        stake_key_file: &Path,
+    ) -> String {
+        let temp_dir = TempDir::new().unwrap();
+
+        let stake_delegation_cert = self
+            .jcli
+            .certificate()
+            .new_stake_delegation(stake_pool_id, stake_key_pub);
+
+        let stake_delegation_cert_file = temp_dir.child("stake_delegation.cert");
+        stake_delegation_cert_file
+            .write_str(&stake_delegation_cert)
+            .unwrap();
+        let stake_delegation_signcert_file = temp_dir.child("stake_delegation.signcert");
+
+        self.jcli.certificate().sign(
+            &stake_key_file,
+            stake_delegation_cert_file.path(),
+            stake_delegation_signcert_file.path(),
+        );
+        file::read_file(stake_delegation_signcert_file.path())
+    }
+
+    pub fn new_signed_vote_plan<P: AsRef<Path>, Q: AsRef<Path>>(
+        mut self,
+        proposal_file: P,
+        stake_key_file: Q,
+    ) -> PathBuf {
+        let temp_dir = TempDir::new().unwrap();
+        let cert = self.jcli.certificate().new_vote_plan(proposal_file);
+
+        let cert_file = temp_dir.child("vote_plan.cert");
+        cert_file.write_str(&cert).unwrap();
+
+        let signcert_file = temp_dir.child("vote_plan.signcert");
+        self.jcli
+            .certificate()
+            .sign(&stake_key_file, cert_file.path(), signcert_file.path());
+        PathBuf::from(signcert_file.path())
+    }
+}
---
+++ testing/jormungandr-integration-tests/src/common/jcli/services/mod.rs
@@ -0,0 +1,29 @@
+mod certificate_builder;
+mod fragment_check;
+mod fragment_sender;
+mod fragments_check;
+mod transaction_builder;
+
+pub use certificate_builder::CertificateBuilder;
+pub use fragment_check::FragmentCheck;
+pub use fragment_sender::FragmentSender;
+pub use fragments_check::FragmentsCheck;
+pub use transaction_builder::TransactionBuilder;
+
+use jormungandr_lib::crypto::hash::Hash;
+use thiserror::Error;
+
+#[derive(Debug, Error)]
+pub enum Error {
+    #[error("transaction {transaction_id} is not in block. message log: {message_log}. Jormungandr log: {log_content}")]
+    TransactionNotInBlock {
+        message_log: String,
+        transaction_id: Hash,
+        log_content: String,
+    },
+    #[error("at least one transaction is not in block. message log: {message_log}. Jormungandr log: {log_content}")]
+    TransactionsNotInBlock {
+        message_log: String,
+        log_content: String,
+    },
+}
