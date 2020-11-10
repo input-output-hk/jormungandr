87f28af58122ba4df14aeecb4ed835d7377bee3a
dkijania <dariusz.kijania@iohk.io>
2020-11-10 11:44:15 +0100

    added apis for vote commands

---
+++ testing/jormungandr-integration-tests/src/common/jcli/api/votes/tally.rs
@@ -0,0 +1,45 @@
+use crate::common::jcli::command::votes::TallyCommand;
+use assert_cmd::assert::OutputAssertExt;
+use jortestkit::prelude::ProcessOutput;
+use std::path::Path;
+
+pub struct Tally {
+    tally_command: TallyCommand,
+}
+
+impl Tally {
+    pub fn new(tally_command: TallyCommand) -> Self {
+        Self { tally_command }
+    }
+
+    pub fn generate_decryption_share<P: AsRef<Path>, Q: AsRef<Path>>(
+        self,
+        decryption_key: P,
+        encrypted_tally: Q,
+    ) -> String {
+        self.tally_command
+            .generate_decryption_share(decryption_key, encrypted_tally)
+            .build()
+            .assert()
+            .success()
+            .get_output()
+            .as_lossy_string()
+    }
+
+    pub fn decrypt_with_shares<P: AsRef<Path>, R: AsRef<Path>>(
+        self,
+        encrypted_tally: P,
+        max_votes: u32,
+        shares: R,
+        tablesize: u32,
+        threshold: u32,
+    ) -> String {
+        self.tally_command
+            .decrypt_with_shares(encrypted_tally, max_votes, shares, tablesize, threshold)
+            .build()
+            .assert()
+            .success()
+            .get_output()
+            .as_lossy_string()
+    }
+}
