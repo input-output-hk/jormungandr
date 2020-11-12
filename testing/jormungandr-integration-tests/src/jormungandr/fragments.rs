b8dca68a8cea76c3421397c385cbbbae886adb02
dkijania <dariusz.kijania@iohk.io>
2020-11-12 15:03:13 +0100

    incorporated changes in test

--- testing/jormungandr-integration-tests/src/jormungandr/fragments.rs
+++ testing/jormungandr-integration-tests/src/jormungandr/fragments.rs
@@ -1,27 +1,25 @@
 use crate::common::{jormungandr::ConfigurationBuilder, startup};
-use chain_impl_mockchain::fee::LinearFee;
+use chain_impl_mockchain::{chaintypes::ConsensusType, fee::LinearFee};
 use jormungandr_lib::interfaces::{ActiveSlotCoefficient, Mempool};
-use jormungandr_testing_utils::{
-    stake_pool::StakePool,
-    testing::{FragmentGenerator, FragmentSender, FragmentSenderSetup},
+use jormungandr_testing_utils::testing::{
+    FragmentGenerator, FragmentSender, FragmentSenderSetup, FragmentVerifier, MemPoolCheck,
 };
+use std::time::Duration;
 
 #[test]
 pub fn send_all_fragments() {
     let receiver = startup::create_new_account_address();
     let mut sender = startup::create_new_account_address();
 
-    let fee = LinearFee::new(1, 1, 1);
-    let value_to_send = 1;
-
     let (jormungandr, _) = startup::start_stake_pool(
         &[sender.clone()],
         &[receiver.clone()],
         ConfigurationBuilder::new()
-            .with_slots_per_epoch(20)
+            .with_block0_consensus(ConsensusType::GenesisPraos)
+            .with_slots_per_epoch(10)
             .with_consensus_genesis_praos_active_slot_coeff(ActiveSlotCoefficient::MAXIMUM)
             .with_slot_duration(3)
-            .with_linear_fees(fee.clone())
+            .with_linear_fees(LinearFee::new(1, 1, 1))
             .with_explorer()
             .with_mempool(Mempool {
                 pool_max_entries: 1_000_000usize.into(),
@@ -35,16 +33,26 @@ pub fn send_all_fragments() {
         jormungandr.fees(),
         FragmentSenderSetup::resend_3_times(),
     );
-    let stake_pool = StakePool::new(&sender);
-    fragment_sender.send_pool_registration(&sender, stake_pool.clone(), &jormungandr);
 
-    let fragment_generator = FragmentGenerator::new(
+    let time_era = jormungandr.time_era();
+
+    let mut fragment_generator = FragmentGenerator::new(
         &mut sender,
         &receiver,
-        vec![stake_pool],
-        vec![],
         &jormungandr,
         jormungandr.explorer(),
-        20,
+        time_era.slots_per_epoch(),
     );
+
+    fragment_generator.prepare(
+        &fragment_sender,
+        jormungandr.explorer().current_time(),
+        jormungandr.time_era(),
+    );
+
+    let mem_checks: Vec<MemPoolCheck> = fragment_generator.send_all(&fragment_sender).unwrap();
+    let verifier = FragmentVerifier;
+    verifier
+        .wait_and_verify_all_are_in_block(Duration::from_secs(2), mem_checks, &jormungandr)
+        .unwrap();
 }
