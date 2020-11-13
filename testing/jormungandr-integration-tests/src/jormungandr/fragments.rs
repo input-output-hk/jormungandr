a02dd70de78c08f25531a80e9ab9e0e06c848424
dkijania <dariusz.kijania@iohk.io>
2020-11-13 16:38:55 +0100

    use remote instead of node reference

--- testing/jormungandr-integration-tests/src/jormungandr/fragments.rs
+++ testing/jormungandr-integration-tests/src/jormungandr/fragments.rs
@@ -9,7 +9,7 @@ use std::time::Duration;
 #[test]
 pub fn send_all_fragments() {
     let receiver = startup::create_new_account_address();
-    let mut sender = startup::create_new_account_address();
+    let sender = startup::create_new_account_address();
 
     let (jormungandr, _) = startup::start_stake_pool(
         &[sender.clone()],
@@ -37,20 +37,17 @@ pub fn send_all_fragments() {
     let time_era = jormungandr.time_era();
 
     let mut fragment_generator = FragmentGenerator::new(
-        &mut sender,
-        &receiver,
-        &jormungandr,
+        sender,
+        receiver,
+        jormungandr.to_remote(),
         jormungandr.explorer(),
         time_era.slots_per_epoch(),
+        fragment_sender,
     );
 
-    fragment_generator.prepare(
-        &fragment_sender,
-        jormungandr.explorer().current_time(),
-        jormungandr.time_era(),
-    );
+    fragment_generator.prepare();
 
-    let mem_checks: Vec<MemPoolCheck> = fragment_generator.send_all(&fragment_sender).unwrap();
+    let mem_checks: Vec<MemPoolCheck> = fragment_generator.send_all().unwrap();
     let verifier = FragmentVerifier;
     verifier
         .wait_and_verify_all_are_in_block(Duration::from_secs(2), mem_checks, &jormungandr)
