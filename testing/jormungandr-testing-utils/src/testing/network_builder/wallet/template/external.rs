e909dcf9279d16795f7f835b2ac507619076739a
dkijania <dariusz.kijania@iohk.io>
2020-12-18 10:05:04 +0100

    added external wallet to network builder
    which corresponds to wallet which is out of controller influence

---
+++ testing/jormungandr-testing-utils/src/testing/network_builder/wallet/template/external.rs
@@ -0,0 +1,34 @@
+use chain_impl_mockchain::value::Value;
+use crate::testing::network_builder::WalletAlias;
+
+/// Struct can be used to differentiate wallet template
+/// which only adress is known and controller cannot control it
+#[derive(Clone, Debug)]
+pub struct ExternalWalletTemplate {
+    alias: WalletAlias,
+    address: String,
+    value: Value,
+}
+
+impl ExternalWalletTemplate {
+    #[inline]
+    pub fn new<S: Into<WalletAlias>>(
+        alias: S,
+        value: Value,
+        address: String,
+    ) -> Self {
+        Self {
+            alias: alias.into(),
+            value,
+            address,
+        }
+    }
+
+    pub fn value(&self) -> &Value {
+        &self.value
+    }
+
+    pub fn address(&self) -> String {
+        self.address.clone()
+    }
+}
---
+++ testing/jormungandr-testing-utils/src/testing/network_builder/wallet/template/mod.rs
@@ -0,0 +1,55 @@
+mod external;
+mod legacy;
+
+pub use legacy::LegacyWalletTemplate;
+pub use external::ExternalWalletTemplate;
+use super::{WalletAlias,WalletType};
+use chain_impl_mockchain::value::Value;
+use crate::testing::network_builder::NodeAlias;
+
+#[derive(Clone, Debug)]
+pub struct WalletTemplate {
+    alias: WalletAlias,
+    value: Value,
+    wallet_type: WalletType,
+    delegate: Option<NodeAlias>,
+}
+
+impl WalletTemplate {
+    pub fn new_account<S: Into<WalletAlias>>(alias: S, value: Value) -> Self {
+        Self::new(alias, value, WalletType::Account)
+    }
+    pub fn new_utxo<S: Into<WalletAlias>>(alias: S, value: Value) -> Self {
+        Self::new(alias, value, WalletType::UTxO)
+    }
+
+    #[inline]
+    fn new<S: Into<WalletAlias>>(alias: S, value: Value, wallet_type: WalletType) -> Self {
+        Self {
+            alias: alias.into(),
+            value,
+            wallet_type,
+            delegate: None,
+        }
+    }
+
+    pub fn alias(&self) -> &WalletAlias {
+        &self.alias
+    }
+
+    pub fn wallet_type(&self) -> &WalletType {
+        &self.wallet_type
+    }
+
+    pub fn value(&self) -> &Value {
+        &self.value
+    }
+
+    pub fn delegate(&self) -> &Option<NodeAlias> {
+        &self.delegate
+    }
+
+    pub fn delegate_mut(&mut self) -> &mut Option<NodeAlias> {
+        &mut self.delegate
+    }
+}
