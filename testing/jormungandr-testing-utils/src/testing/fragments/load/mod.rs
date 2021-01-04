2e266ab70e2f691cf3ed5126e5662e435a02ace1
dkijania <dariusz.kijania@iohk.io>
2021-01-04 17:34:12 +0100

    added vote cast generator

--- testing/jormungandr-testing-utils/src/testing/fragments/load/mod.rs
+++ testing/jormungandr-testing-utils/src/testing/fragments/load/mod.rs
@@ -1,7 +1,9 @@
 mod batch_generator;
 mod status_provider;
 mod transaction_generator;
+mod vote_casts_generator;
 
 pub use batch_generator::BatchFragmentGenerator;
 pub use status_provider::FragmentStatusProvider;
 pub use transaction_generator::TransactionGenerator;
+pub use vote_casts_generator::VoteCastsGenerator;
