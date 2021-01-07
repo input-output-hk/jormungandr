fe36b161ac45358628923188dcdd1523095c14f8
dkijania <dariusz.kijania@iohk.io>
2021-01-07 13:01:11 +0100

    removing duplicated bins

--- testing/jormungandr-integration-tests/src/bin/fragment_load.rs
+++
@@ -1,6 +0,0 @@
-use jormungandr_integration_tests::common::load::{FragmentLoadCommand, FragmentLoadCommandError};
-use structopt::StructOpt;
-
-pub fn main() -> Result<(), FragmentLoadCommandError> {
-    FragmentLoadCommand::from_args().exec()
-}
--- testing/jormungandr-integration-tests/src/common/load/mod.rs
+++
@@ -1,5 +0,0 @@
-mod bootstrap;
-mod fragment;
-
-pub use bootstrap::{ClientLoadConfig, ClientLoadError, PassiveBootstrapLoad, ScenarioType};
-pub use fragment::{FragmentLoadCommand, FragmentLoadCommandError};
