d4c75ece71eb3e44a0aa0b4d52904cbc0675a65f
dkijania <dariusz.kijania@iohk.io>
2021-01-27 11:26:38 +0100

    pulled up configuration for wait_until_all_processed

--- testing/jormungandr-integration-tests/src/common/jcli/services/fragments_check.rs
+++ testing/jormungandr-integration-tests/src/common/jcli/services/fragments_check.rs
@@ -5,6 +5,7 @@ use crate::common::{
 };
 use jormungandr_lib::interfaces::FragmentLog;
 use jortestkit::prelude::ProcessOutput;
+use jortestkit::prelude::Wait;
 use jortestkit::process::run_process_until_response_matches;
 use std::process::Command;
 
@@ -19,11 +20,11 @@ impl<'a> FragmentsCheck<'a> {
     }
 
     pub fn wait_until_in_block(self) -> Result<(), Error> {
-        self.wait_until_all_processed()?;
+        self.wait_until_all_processed(&Default::default())?;
         self.check_log_shows_in_block()
     }
 
-    pub fn wait_until_all_processed(&self) -> Result<(), Error> {
+    pub fn wait_until_all_processed(&self, wait: &Wait) -> Result<(), Error> {
         run_process_until_response_matches(
             JCliCommand::new(Command::new(self.jcli.path()))
                 .rest()
@@ -38,8 +39,8 @@ impl<'a> FragmentsCheck<'a> {
                 let at_least_one_pending = fragments.iter().any(|x| x.is_pending());
                 !at_least_one_pending
             },
-            1,
-            150,
+            wait.sleep_duration().as_secs(),
+            wait.attempts(),
             "Waiting for last transaction to be inBlock or rejected",
             "transaction is pending for too long",
         )
