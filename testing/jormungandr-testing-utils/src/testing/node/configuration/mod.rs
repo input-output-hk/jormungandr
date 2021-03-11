278efadd32e3d5c3c51338ef961d1ac17b1fe25d
Giacomo Pasini <giacomo.pasini@iohk.io>
2021-03-11 16:21:55 +0100

    Filter in-use ports when assigning a new one

--- testing/jormungandr-testing-utils/src/testing/node/configuration/mod.rs
+++ testing/jormungandr-testing-utils/src/testing/node/configuration/mod.rs
@@ -1,5 +1,6 @@
 use lazy_static::lazy_static;
-use rand::Rng;
+use netstat2::{get_sockets_info, AddressFamilyFlags, ProtocolFlags};
+use std::collections::HashSet;
 use std::sync::atomic::{AtomicU16, Ordering};
 
 mod block0_config_builder;
@@ -17,12 +18,23 @@ pub use node_config_builder::NodeConfigBuilder;
 pub use secret_model_factory::{write_secret, SecretModelFactory};
 
 lazy_static! {
-    static ref NEXT_AVAILABLE_PORT_NUMBER: AtomicU16 = {
-        let initial_port = rand::thread_rng().gen_range(6000, 10999);
-        AtomicU16::new(initial_port)
+    static ref NEXT_AVAILABLE_PORT_NUMBER: AtomicU16 = AtomicU16::new(10000);
+    static ref OCCUPIED_PORTS: HashSet<u16> = {
+        let af_flags = AddressFamilyFlags::IPV4;
+        let proto_flags = ProtocolFlags::TCP | ProtocolFlags::UDP;
+        get_sockets_info(af_flags, proto_flags)
+            .unwrap()
+            .into_iter()
+            .map(|s| s.local_port())
+            .collect::<HashSet<_>>()
     };
 }
 
 pub fn get_available_port() -> u16 {
-    NEXT_AVAILABLE_PORT_NUMBER.fetch_add(1, Ordering::SeqCst)
+    loop {
+        let candidate_port = NEXT_AVAILABLE_PORT_NUMBER.fetch_add(1, Ordering::SeqCst);
+        if !(*OCCUPIED_PORTS).contains(&candidate_port) {
+            return candidate_port;
+        }
+    }
 }
