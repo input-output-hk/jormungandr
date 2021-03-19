06023f416c37894c3cadecbe8d4c873442048844
Giacomo Pasini <giacomo.pasini@iohk.io>
2021-03-19 12:25:43 +0100

    Remove traces of old poldercast features from peer stats
    A lot of the content of those records was linked to old
    features of poldercast that do no exists anymore.
    Let's remove them and keep just the ones that still makes
    sense.

--- jormungandr-lib/src/interfaces/peer_stats.rs
+++ jormungandr-lib/src/interfaces/peer_stats.rs
@@ -1,6 +1,5 @@
-use crate::time::{SecondsSinceUnixEpoch, SystemTime};
+use crate::time::SystemTime;
 use serde::{Deserialize, Serialize};
-use std::collections::HashMap;
 use std::net::SocketAddr;
 
 #[derive(Debug, Clone, Eq, PartialEq, Serialize, Deserialize)]
@@ -16,57 +15,16 @@ pub struct PeerStats {
 #[derive(Debug, Clone, Eq, PartialEq, Serialize, Deserialize)]
 #[serde(deny_unknown_fields)]
 pub struct PeerRecord {
+    pub id: String,
     pub address: String,
-    pub profile: Profile,
-    pub record: Record,
-    pub logs: Logs,
-}
-
-#[derive(Debug, Clone, Eq, PartialEq, Serialize, Deserialize)]
-#[serde(deny_unknown_fields)]
-pub struct Profile {
-    pub info: Info,
+    pub last_update: SystemTime,
+    pub quarantined: Option<SystemTime>,
     pub subscriptions: Vec<Subscription>,
-}
-
-#[derive(Debug, Clone, Eq, PartialEq, Serialize, Deserialize)]
-pub struct Info {
-    pub address: String,
 }
 
 #[derive(Debug, Clone, Eq, PartialEq, Serialize, Deserialize)]
 #[serde(deny_unknown_fields)]
 pub struct Subscription {
-    pub interest: String,
-    pub topic: u32,
-}
-
-#[derive(Debug, Clone, Eq, PartialEq, Serialize, Deserialize)]
-#[serde(deny_unknown_fields)]
-pub struct Record {
-    pub strikes: Vec<Strike>,
-}
-
-#[derive(Debug, Clone, Eq, PartialEq, Serialize, Deserialize)]
-#[serde(deny_unknown_fields)]
-pub struct Strike {
-    pub reason: String,
-    pub when: When,
-}
-
-#[derive(Debug, Clone, Eq, PartialEq, Serialize, Deserialize)]
-#[serde(deny_unknown_fields)]
-pub struct When {
-    pub secs_since_epoch: SecondsSinceUnixEpoch,
-    pub nanos_since_epoch: u128,
-}
-
-#[derive(Debug, Clone, Eq, PartialEq, Serialize, Deserialize)]
-#[serde(deny_unknown_fields)]
-pub struct Logs {
-    pub creation_time: When,
-    pub last_gossip: When,
-    pub last_update: When,
-    pub quarantined: Option<When>,
-    pub last_use_of: HashMap<String, When>,
+    pub interest: u32,
+    pub topic: String,
 }
