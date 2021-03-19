4df3ac4dc0f66d6935f665fecfbbff6010c29fac
Filip Dulic <filip.dulic@bloxico.com>
2021-03-19 13:06:35 -0600

    give user feedback about ignored log settings and expected behavior
    also addresses
    https://github.com/input-output-hk/jormungandr/pull/3145#pullrequestreview-616255382

--- jormungandr/src/settings/start/mod.rs
+++ jormungandr/src/settings/start/mod.rs
@@ -74,6 +74,12 @@ impl RawSettings {
         //  Read log settings from the config file path.
         if let Some(log) = self.config.as_ref().and_then(|cfg| cfg.log.as_ref()) {
             if let Some(cfg) = log.0.first() {
+                if log.0.len() > 1 {
+                    info_msgs.push(format!(
+                        "only one entry for log settings allowed, ignoring these: {:?}",
+                        &log.0[1..],
+                    ));
+                }
                 if let Some(level) = cfg.level {
                     log_config.level = level;
                 }
