042151169e47cd9aa5c84122d803833bd0711a04
Giacomo Pasini <giacomo.pasini@iohk.io>
2021-03-19 13:39:19 +0100

    Update after new poldercast version

--- jormungandr/src/network/p2p/topology.rs
+++ jormungandr/src/network/p2p/topology.rs
@@ -2,175 +2,220 @@
 //!
 
 use crate::{
-    network::p2p::{layers::PreferredListLayer, Address, Gossips},
+    network::p2p::{Address, Gossip, Gossips, Quarantine},
     settings::start::network::Configuration,
 };
-use chain_crypto::{Ed25519, SecretKey};
-use poldercast::Topology;
-use rand_chacha::ChaChaRng;
+use jormungandr_lib::time::SystemTime;
+use poldercast::{Profile as PoldercastProfile, Subscription, Subscriptions, Topology};
+use serde::Serializer;
 use tokio::sync::RwLock;
-use tracing::{span, Level, Span};
+use tracing::instrument;
 
-use std::net::SocketAddr;
+use super::{topic, NodeId};
+
+use std::hash::{Hash, Hasher};
 use std::sync::Arc;
 
+fn serialize_display<T: std::fmt::Display, S>(item: &T, serializer: S) -> Result<S::Ok, S::Error>
+where
+    S: Serializer,
+{
+    serializer.serialize_str(&item.to_string())
+}
+
+#[derive(Eq, Clone, Serialize, Debug)]
+pub struct Profile {
+    #[serde(serialize_with = "serialize_display")]
+    pub id: NodeId,
+    pub address: Address,
+    pub last_update: SystemTime,
+    pub quarantined: Option<SystemTime>,
+    pub subscriptions: Vec<(String, String)>,
+}
+
+impl PartialEq for Profile {
+    fn eq(&self, other: &Self) -> bool {
+        self.id == other.id && self.address == other.address
+    }
+}
+
+impl Hash for Profile {
+    fn hash<H: Hasher>(&self, state: &mut H) {
+        self.id.hash(state);
+        self.address.hash(state);
+    }
+}
+
+impl From<Arc<PoldercastProfile>> for Profile {
+    fn from(other: Arc<PoldercastProfile>) -> Self {
+        Self {
+            id: other.id(),
+            address: other.address(),
+            last_update: other.last_update().to_system_time().into(),
+            quarantined: None,
+            subscriptions: other
+                .subscriptions()
+                .iter()
+                .map(|s| (s.topic().to_string(), format!("{:?}", s.interest_level())))
+                .collect(),
+        }
+    }
+}
+
 pub struct View {
-    pub self_node: Arc<Profile>,
-    pub peers: Vec<Address>,
+    pub self_node: Arc<PoldercastProfile>,
+    pub peers: Vec<Arc<PoldercastProfile>>,
+}
+
+struct Inner {
+    topology: Topology,
+    quarantine: Quarantine,
+    // this is needed to advertise ourself to trusted peers,
+    // poldercast does not allow us to take this out from the
+    // topology
+    initial_self_profile: Arc<PoldercastProfile>,
 }
 
 /// object holding the P2pTopology of the Node
 pub struct P2pTopology {
-    lock: RwLock<Topology>,
-}
-
-/// Builder object used to initialize the `P2pTopology`
-struct Builder {
-    public_addr: SocketAddr,
-    key: SecretKey<Ed25519>,
-    span: Span,
-}
-
-type PcSecretKey = keynesis::key::ed25519::SecretKey;
-
-fn secret_key_into_keynesis(key: SecretKey<25519>) -> PcSecretKey {
-    let key_bytes = key.leak_secret().as_ref();
-    key_bytes.try_into().unwrap()
-}
-
-impl Builder {
-    /// Create a new topology for the given public address of the local node
-    /// and a secret key used for identification.
-    fn new(public_addr: SocketAddr, key: SecretKey<Ed25519>, span: Span) -> Self {
-        Builder {
-            public_addr,
-            key,
-            span,
-        }
-    }
-
-    /// set all the default poldercast modules (Rings, Vicinity and Cyclon)
-    fn set_poldercast_modules(mut self) -> Self {
-        self.topology.add_layer(Rings::default());
-        self.topology.add_layer(Vicinity::default());
-        self.topology.add_layer(Cyclon::default());
-        self
-    }
-
-    fn set_custom_modules(mut self, config: &Configuration, rng: ChaChaRng) -> Self {
-        if let Some(size) = config.max_unreachable_nodes_to_connect_per_event {
-            self.topology
-                .add_layer(custom_layers::RandomDirectConnections::with_max_view_length(size));
-        } else {
-            self.topology
-                .add_layer(custom_layers::RandomDirectConnections::default());
-        }
-
-        self.topology.add_layer(PreferredListLayer::new(
-            config.layers.preferred_list.clone(),
-            rng,
-        ));
-
-        self
-    }
-
-    fn build(self) -> P2pTopology {
-        let key = secret_key_into_keynesis(self.key);
-        let topology = Topology::new(self.public_addr, &key);
-        P2pTopology {
-            lock: RwLock::new(topology),
-        }
-    }
+    lock: RwLock<Inner>,
 }
 
 impl P2pTopology {
-    pub fn new(config: &Configuration, span: Span, rng: ChaChaRng) -> Self {
-        Builder::new(config.profile.clone(), span)
-            .set_poldercast_modules()
-            .set_custom_modules(&config, rng)
-            .set_policy(config.policy.clone())
-            .build()
+    pub fn new(config: &Configuration) -> Self {
+        // This is needed at the beginning to advert ourself to trusted peers
+        let subscriptions = crate::settings::start::config::default_interests()
+            .into_iter()
+            .fold(Subscriptions::new(), |mut acc, (topic, interest)| {
+                acc.push(Subscription::new(topic.0, interest.0).as_slice())
+                    .unwrap();
+                acc
+            });
+
+        // FIXME: How should we handle cases where the is not listen set? Can a node just receive?
+        let addr = config.public_address.unwrap();
+        let key = super::secret_key_into_keynesis(config.node_key.clone());
+        let initial_self_profile = Arc::new(PoldercastProfile::from(poldercast::Gossip::new(
+            addr,
+            &key,
+            subscriptions.as_slice(),
+        )));
+
+        let quarantine = Quarantine::from_config(config.policy.clone());
+        let mut topology = Topology::new(addr, &key);
+        topology.subscribe_topic(topic::MESSAGES);
+        topology.subscribe_topic(topic::BLOCKS);
+        let inner = Inner {
+            topology,
+            initial_self_profile,
+            quarantine,
+        };
+        P2pTopology {
+            lock: RwLock::new(inner),
+        }
     }
 
     /// Returns a list of neighbors selected in this turn
     /// to contact for event dissemination.
-    pub async fn view(&self, selection: poldercast::Selection) -> View {
-        let mut topology = self.lock.write().await;
-        let peers = topology.view(None, selection).into_iter().collect();
+    pub async fn view(&self, selection: poldercast::layer::Selection) -> View {
+        let mut inner = self.lock.write().await;
+        let peers = inner.topology.view(None, selection).into_iter().collect();
         View {
-            self_node: topology.profile().clone(),
+            self_node: inner.initial_self_profile.clone(),
             peers,
         }
     }
 
-    pub async fn initiate_gossips(&self, with: Address) -> Gossips {
-        let mut topology = self.lock.write().await;
-        topology.initiate_gossips(with).into()
+    pub async fn initiate_gossips(&self, recipient: &NodeId) -> Gossips {
+        let mut inner = self.lock.write().await;
+        let mut gossips = inner.topology.gossips_for(recipient);
+        // If the recipient is not already in the topology poldercast
+        // will not return anything. Broadcast out profile anyway
+        if gossips.is_empty() {
+            gossips.push(inner.initial_self_profile.gossip().clone());
+        }
+        Gossips::from(gossips)
     }
 
-    pub async fn accept_gossips(&self, from: Address, gossips: Gossips) {
-        let mut topology = self.lock.write().await;
-        topology.accept_gossips(from, gossips.into())
+    #[instrument(skip(self, gossips), level = "debug")]
+    pub async fn accept_gossips(&self, gossips: Gossips) {
+        let mut inner = self.lock.write().await;
+        let gossips = <Vec<poldercast::Gossip>>::from(gossips);
+        for gossip in gossips {
+            let peer = PoldercastProfile::from_gossip(gossip);
+            tracing::trace!(node = %peer.address(), "received peer from gossip");
+            inner.topology.add_peer(peer);
+        }
+
+        // nodes lifted from quarantine will be considered again in the next update
+        let lifted = inner.quarantine.lift_from_quarantine();
+        for node in lifted {
+            inner.topology.promote_peer(&node.id);
+        }
     }
 
-    pub async fn exchange_gossips(&mut self, with: Address, gossips: Gossips) -> Gossips {
-        let mut topology = self.lock.write().await;
-        topology.exchange_gossips(with, gossips.into()).into()
-    }
-
-    pub async fn node(&self) -> NodeProfile {
-        let topology = self.lock.read().await;
-        topology.profile().clone()
-    }
-
+    #[instrument(skip(self), level = "debug")]
     pub async fn force_reset_layers(&self) {
-        let mut topology = self.lock.write().await;
-        topology.force_reset_layers()
+        tracing::warn!("resetting layers is not supported in this poldercast version");
     }
 
-    pub async fn list_quarantined(&self) -> Vec<poldercast::Node> {
-        let topology = self.lock.read().await;
-        topology
-            .nodes()
-            .all_quarantined_nodes()
+    // FIXME: Poldercast is lacking the abitily to return all the nodes currently
+    // in the topology, without that it would be very inefficient to track
+    // nodes due to transparent eviction from the underlying lru cache.
+    // Until that is implemented this method may return nodes that have been
+    // remove completely from the topology
+    pub async fn list_quarantined(&self) -> Vec<Profile> {
+        self.lock.read().await.quarantine.quarantined_nodes()
+    }
+
+    // FIXME: Poldercast is lacking the abitily to return all the nodes currently
+    // in the topology, without that it would be very inefficient to track
+    // nodes due to transparent eviction from the underlying lru cache.
+    // Until that is implemented this method may not reflect all available peers
+    pub async fn list_available(&self) -> Vec<Profile> {
+        self.view(poldercast::layer::Selection::Any)
+            .await
+            .peers
             .into_iter()
-            .cloned()
+            .map(|profile| profile.into())
             .collect()
     }
 
-    pub async fn list_available(&self) -> Vec<poldercast::Node> {
-        let topology = self.lock.read().await;
-        topology
-            .nodes()
-            .all_available_nodes()
+    // FIXME: Poldercast is lacking the abitily to return all the nodes currently
+    // in the topology, without that it would be very inefficient to track
+    // nodes due to transparent eviction from the underlying lru cache.
+    // Until that is implemented this method may not reflect all available peers
+    pub async fn list_non_public(&self) -> Vec<Profile> {
+        self.view(poldercast::layer::Selection::Any)
+            .await
+            .peers
             .into_iter()
-            .cloned()
+            .filter_map(|profile| {
+                if Gossip::from(profile.gossip().clone()).is_global() {
+                    None
+                } else {
+                    Some(profile.into())
+                }
+            })
             .collect()
     }
 
-    pub async fn list_non_public(&self) -> Vec<poldercast::Node> {
-        let topology = self.lock.read().await;
-        topology
-            .nodes()
-            .all_unreachable_nodes()
-            .into_iter()
-            .cloned()
-            .collect()
+    /// register that we were able to establish an handshake with given peer
+    pub async fn promote_node(&self, node: &NodeId) {
+        let mut inner = self.lock.write().await;
+        inner.topology.promote_peer(node);
     }
 
-    pub async fn nodes_count(&self) -> poldercast::Count {
-        let topology = self.lock.read().await;
-        topology.nodes().node_count()
-    }
-
-    /// register a strike against the given node id
-    ///
-    /// the function returns `None` if the node was not even in the
-    /// the topology (not even quarantined).
-    pub async fn report_node(&self, address: Address, issue: StrikeReason) -> Option<PolicyReport> {
-        let mut topology = self.lock.write().await;
-        topology.update_node(address, |node| {
-            node.record_mut().strike(issue);
-        })
+    /// register a strike against the given peer
+    pub async fn report_node(&self, node_id: &NodeId) {
+        let mut inner = self.lock.write().await;
+        if let Some(node) = inner.topology.get(node_id).cloned() {
+            if inner.quarantine.quarantine_node(node.into()) {
+                inner.topology.remove_peer(node_id);
+                // Don't know what is the purpose of trusted peers in poldercast,
+                // this is a quick hack to treat those as standard ones
+                inner.topology.remove_peer(node_id);
+            }
+        }
     }
 }
