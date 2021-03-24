39b2ae362a6ee05e798a4418d87fd28d967252f1
Giacomo Pasini <giacomo.pasini@iohk.io>
2021-03-24 09:17:23 +0100

    Move topology to its own task
    Topology was handled directly by the network task, and that
    , apart from not being very clean, was causing prolems with
    the new update since topolgy cannot be shared easiliy anymore.
    
    The 'p2p/peers' and 'topology' layers have to talk to each other
    to exchange information (mostly topology -> peers)). At the moment
    each has its own representation for a node and the only thing that
    is taken into consideration from the other layer is the address of
    the node. In the future we would likely want to add verification
    of identities that would require exchanging (and defining a way to
    do so) node public keys. Since the precise way we will use to do
    so has not been decided so, this commit does not try to merge the
    two representations together.

--- jormungandr/src/network/p2p/mod.rs
+++ jormungandr/src/network/p2p/mod.rs
@@ -1,58 +1,14 @@
-use std::convert::TryInto;
-
+/// This module is responsible for handling active peers and communication in a p2p setting.
+/// It takes care of managing connections with said peers and sending messages to them.
+/// The topology task is instead responsible for the discovery of active peers.
+///
+/// FIXME: Topology and peers have indipendent representation of a external node.
+/// At the moment, topology and peers each use a different ID for node identification
+/// but since those are not checked, each layers ignore the other one's IDs.
+/// Ideally, peers should request a proof-of-possession of the key used to
+/// authenticate gossips when connecting and viceversa.
+/// Leaving this to when we will introduce identity verification, since requirements
+/// are likely to change.
 pub mod comm;
-mod gossip;
-pub mod layers;
-mod policy;
-
-mod topology;
-
-pub use self::gossip::{Gossip, Gossips, Peer, Peers};
-pub use self::topology::{P2pTopology, ProfileInfo};
-use chain_crypto::Ed25519;
-use jormungandr_lib::crypto::key::SigningKey;
-pub use poldercast::Profile;
-pub use policy::{PolicyConfig, Quarantine};
 
 pub type Address = std::net::SocketAddr;
-pub type NodeId = keynesis::key::ed25519::PublicKey;
-pub type SecretKey = keynesis::key::ed25519::SecretKey;
-
-pub fn secret_key_into_keynesis(key: SigningKey<Ed25519>) -> SecretKey {
-    let key_bytes = key.into_secret_key().leak_secret();
-    key_bytes.as_ref().try_into().unwrap()
-}
-
-pub fn identifier_into_keynesis(id: jormungandr_lib::interfaces::NodeId) -> NodeId {
-    let id_bytes = id.into_public_key().inner();
-    id_bytes.as_ref().try_into().unwrap()
-}
-
-/**
-# topics definition for p2p interest subscriptions
-*/
-pub mod topic {
-    use poldercast::Topic;
-
-    pub const MESSAGES: Topic = Topic::new([0; 32]);
-    pub const BLOCKS: Topic = {
-        let mut array = [0; 32];
-        array[31] = 1;
-        Topic::new(array)
-    };
-}
-
-/**
-limits for the property::{Serialize/Deserialize} implementations
-*/
-pub mod limits {
-    /// limit the gossip size to 512 bytes (limit per gossip).
-    ///
-    /// a gossip only contains the Id, the address and an array of subscriptions
-    /// which should not go beyond 2 2-tuples of 64bits.
-    pub const MAX_GOSSIP_SIZE: u64 = 512;
-
-    /// limit the ID size to 32 bytes. Right now the Node ID are 24 bytes but
-    /// for backward compatibility keep the value to 32bytes.
-    pub const MAX_ID_SIZE: u64 = 32;
-}
