6d01231c492c6882a8f82b47b04b694b20398a3e
Giacomo Pasini <giacomo.pasini@iohk.io>
2021-04-13 12:16:21 +0200

    Replace private queue with period timer

--- jormungandr/src/topology/process.rs
+++ jormungandr/src/topology/process.rs
@@ -1,16 +1,15 @@
-use super::{Gossips, P2pTopology, Peer};
-use crate::intercom::{self, NetworkMsg, PropagateMsg, ReplyHandle, TopologyMsg};
+use super::{P2pTopology, Peer};
+use crate::intercom::{NetworkMsg, PropagateMsg, TopologyMsg};
 use crate::settings::start::network::Configuration;
-use crate::utils::async_msg::{self, MessageBox, MessageQueue};
-use crate::utils::task::TokioServiceInfo;
-use futures::stream::Stream;
+use crate::utils::async_msg::{MessageBox, MessageQueue};
 use futures::SinkExt;
+use tokio::time::Interval;
 use tokio_stream::StreamExt;
 
-const INTERNAL_QUEUE_LEN: usize = 32;
-
-struct Process<T: Stream<Item = Message> + Unpin> {
-    input: T,
+struct Process {
+    input: MessageQueue<TopologyMsg>,
+    network_msgbox: MessageBox<NetworkMsg>,
+    gossip_interval: Interval,
     topology: P2pTopology,
 }
 
@@ -20,20 +19,7 @@ pub struct TaskData {
     pub config: Configuration,
 }
 
-enum Message {
-    // This is the public interface of P2PTopology
-    Public(TopologyMsg),
-    // This is the task private interface of P2PTopology
-    Private(PrivateMsg),
-}
-
-// Interaction with the topology that is used internally
-// and is not available to other tasks
-enum PrivateMsg {
-    GetGossips(ReplyHandle<Vec<(Peer, Gossips)>>),
-}
-
-pub async fn start(info: TokioServiceInfo, task_data: TaskData) {
+pub async fn start(task_data: TaskData) {
     let TaskData {
         mut network_msgbox,
         topology_queue,
@@ -41,12 +27,6 @@ pub async fn start(info: TokioServiceInfo, task_data: TaskData) {
     } = task_data;
 
     let mut topology = P2pTopology::new(&config);
-
-    let public_input = topology_queue.map(Message::Public);
-    let (internal_msgbox, internal_queue) = async_msg::channel(INTERNAL_QUEUE_LEN);
-    let input = public_input.merge(internal_queue.map(Message::Private));
-
-    let internal_msgbox_1 = internal_msgbox.clone();
 
     // Send gossips for trusted peers at the beginning, without inserting them first in
     // the topology
@@ -64,64 +44,57 @@ pub async fn start(info: TokioServiceInfo, task_data: TaskData) {
             .unwrap_or_else(|e| tracing::error!("Error sending gossips to network task: {}", e));
     }
 
-    info.run_periodic_fallible(
-        "send gossips to network task to propagate",
-        config.gossip_interval,
-        move || handle_gossip(internal_msgbox_1.clone(), network_msgbox.clone()),
-    );
-
-    let mut process = Process { input, topology };
-
+    let mut process = Process {
+        input: topology_queue,
+        gossip_interval: tokio::time::interval(config.gossip_interval),
+        network_msgbox,
+        topology,
+    };
     process.handle_input().await;
 }
 
-impl<T: Stream<Item = Message> + Unpin> Process<T> {
+impl Process {
     async fn handle_input(&mut self) {
-        use Message::{Private, Public};
-        while let Some(input) = self.input.next().await {
-            match input {
-                Public(TopologyMsg::AcceptGossip(gossip)) => self.topology.accept_gossips(gossip),
-                Public(TopologyMsg::DemotePeer(id)) => self.topology.report_node(&id),
-                Public(TopologyMsg::PromotePeer(id)) => self.topology.promote_node(&id),
-                Public(TopologyMsg::View(selection, handle)) => {
-                    handle.reply_ok(self.topology.view(selection))
-                }
-                Public(TopologyMsg::ListAvailable(handle)) => {
-                    handle.reply_ok(self.topology.list_available())
-                }
-                Public(TopologyMsg::ListNonPublic(handle)) => {
-                    handle.reply_ok(self.topology.list_non_public())
-                }
-                Public(TopologyMsg::ListQuarantined(handle)) => {
-                    handle.reply_ok(self.topology.list_quarantined())
-                }
-                Private(PrivateMsg::GetGossips(handle)) => {
-                    let view = self.topology.view(poldercast::layer::Selection::Any);
-                    let mut res = Vec::new();
-                    for peer in view.peers {
-                        // Peers returned bu the topology will always have a NodeId
-                        let id = peer.id.clone().unwrap();
-                        res.push((peer, self.topology.initiate_gossips(Some(&id))));
+        loop {
+            tokio::select! {
+                Some(input) = self.input.next() => {
+                    match input {
+                        TopologyMsg::AcceptGossip(gossip) => self.topology.accept_gossips(gossip),
+                        TopologyMsg::DemotePeer(id) => self.topology.report_node(&id),
+                        TopologyMsg::PromotePeer(id) => self.topology.promote_node(&id),
+                        TopologyMsg::View(selection, handle) => {
+                            handle.reply_ok(self.topology.view(selection))
+                        }
+                        TopologyMsg::ListAvailable(handle) => {
+                            handle.reply_ok(self.topology.list_available())
+                        }
+                        TopologyMsg::ListNonPublic(handle) => {
+                            handle.reply_ok(self.topology.list_non_public())
+                        }
+                        TopologyMsg::ListQuarantined(handle) => {
+                            handle.reply_ok(self.topology.list_quarantined())
+                        }
                     }
-                    handle.reply_ok(res);
-                }
+                },
+                _ = self.gossip_interval.tick() => {
+                        let view = self.topology.view(poldercast::layer::Selection::Any);
+                        for peer in view.peers {
+                            // Peers returned by the topology will always have a NodeId
+                            let id = peer.id.clone().unwrap();
+                            let gossip = self.topology.initiate_gossips(Some(&id));
+
+                            self.network_msgbox
+                                // do not block the current thread to avoid deadlocks
+                                .try_send(NetworkMsg::Propagate(PropagateMsg::Gossip(peer, gossip)))
+                                .unwrap_or_else(|e| {
+                                    tracing::error!(
+                                        reason = ?e,
+                                        "cannot send PropagateMsg request to network"
+                                    )
+                                });
+                        }
+                    }
             }
         }
     }
-}
-
-async fn handle_gossip(
-    mut internal_mbox: MessageBox<PrivateMsg>,
-    mut network_mbox: MessageBox<NetworkMsg>,
-) -> Result<(), Box<dyn std::error::Error>> {
-    let (reply_handle, reply_future) = intercom::unary_reply();
-    internal_mbox
-        .send(PrivateMsg::GetGossips(reply_handle))
-        .await?;
-    for (peer, gossip) in reply_future.await? {
-        network_mbox
-            .send(NetworkMsg::Propagate(PropagateMsg::Gossip(peer, gossip)))
-            .await?;
-    }
-    Ok(())
 }
