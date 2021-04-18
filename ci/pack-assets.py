47283946a569715d856e28cadea0a5928d3061e7
Yevhenii Babichenko <eugene.babichenko@iohk.io>
2021-04-18 23:06:25 +0300

    reformat python scripts with black

--- ci/pack-assets.py
+++ ci/pack-assets.py
@@ -9,7 +9,7 @@ import os
 
 def sha256sum(path):
     h = hashlib.sha256()
-    with open(path, 'rb') as f:
+    with open(path, "rb") as f:
         data = f.read()
         h.update(data)
     return h.hexdigest()
@@ -19,16 +19,16 @@ version = sys.argv[1]
 target = sys.argv[2]
 target_cpu = sys.argv[3]
 
-archive_basename = 'jormungandr-{}-{}-{}'.format(version, target, target_cpu)
+archive_basename = "jormungandr-{}-{}-{}".format(version, target, target_cpu)
 
-root_dir = './target/{}/release'.format(target)
+root_dir = "./target/{}/release".format(target)
 
-if platform.system() == 'Windows':
-    jormungandr_name = 'jormungandr.exe'
-    jcli_name = 'jcli.exe'
+if platform.system() == "Windows":
+    jormungandr_name = "jormungandr.exe"
+    jcli_name = "jcli.exe"
 else:
-    jormungandr_name = 'jormungandr'
-    jcli_name = 'jcli'
+    jormungandr_name = "jormungandr"
+    jcli_name = "jcli"
 
 jormungandr_path = os.path.join(root_dir, jormungandr_name)
 jcli_path = os.path.join(root_dir, jcli_name)
@@ -37,42 +37,49 @@ jormungandr_checksum = sha256sum(jormungandr_path)
 jcli_checksum = sha256sum(jcli_path)
 
 # build archive
-if platform.system() == 'Windows':
+if platform.system() == "Windows":
     import zipfile
-    content_type = 'application/zip'
-    archive_name = '{}.zip'.format(archive_basename)
-    with zipfile.ZipFile(archive_name, mode='x') as archive:
+
+    content_type = "application/zip"
+    archive_name = "{}.zip".format(archive_basename)
+    with zipfile.ZipFile(archive_name, mode="x") as archive:
         archive.write(jormungandr_path, arcname=jormungandr_name)
         archive.write(jcli_path, arcname=jcli_name)
 else:
     import tarfile
-    content_type = 'application/gzip'
-    archive_name = '{}.tar.gz'.format(archive_basename)
-    with tarfile.open(archive_name, 'x:gz') as archive:
+
+    content_type = "application/gzip"
+    archive_name = "{}.tar.gz".format(archive_basename)
+    with tarfile.open(archive_name, "x:gz") as archive:
         archive.add(jormungandr_path, arcname=jormungandr_name)
         archive.add(jcli_path, arcname=jcli_name)
 
 # verify archive
-shutil.unpack_archive(archive_name, './unpack-test')
-jormungandr1_checksum = sha256sum(
-    os.path.join('./unpack-test', jormungandr_name))
-jcli1_checksum = sha256sum(os.path.join('./unpack-test', jcli_name))
-shutil.rmtree('./unpack-test')
+shutil.unpack_archive(archive_name, "./unpack-test")
+jormungandr1_checksum = sha256sum(os.path.join("./unpack-test", jormungandr_name))
+jcli1_checksum = sha256sum(os.path.join("./unpack-test", jcli_name))
+shutil.rmtree("./unpack-test")
 if jormungandr1_checksum != jormungandr_checksum:
-    print('jormungandr checksum mismarch: before {} != after {}'.format(
-        jormungandr_checksum, jormungandr1_checksum))
+    print(
+        "jormungandr checksum mismarch: before {} != after {}".format(
+            jormungandr_checksum, jormungandr1_checksum
+        )
+    )
     exit(1)
 if jcli1_checksum != jcli_checksum:
-    print('jcli checksum mismarch: before {} != after {}'.format(
-        jcli_checksum, jcli1_checksum))
+    print(
+        "jcli checksum mismarch: before {} != after {}".format(
+            jcli_checksum, jcli1_checksum
+        )
+    )
     exit(1)
 
 # save archive checksum
 archive_checksum = sha256sum(archive_name)
-checksum_filename = '{}.sha256'.format(archive_name)
-with open(checksum_filename, 'x') as f:
+checksum_filename = "{}.sha256".format(archive_name)
+with open(checksum_filename, "x") as f:
     f.write(archive_checksum)
 
 # set GitHub Action step outputs
-print('::set-output name=release-archive::{}'.format(archive_name))
-print('::set-output name=release-content-type::{}'.format(content_type))
+print("::set-output name=release-archive::{}".format(archive_name))
+print("::set-output name=release-content-type::{}".format(content_type))
--- ci/release-info.py
+++ ci/release-info.py
@@ -5,36 +5,39 @@ import sys
 from datetime import date
 from subprocess import Popen, PIPE
 
+
 def check_version(crate):
     # Checks package version for matching with the current tag reference
-    if ref is not None and ref != 'refs/tags/v' + str(crate[0]):
+    if ref is not None and ref != "refs/tags/v" + str(crate[0]):
         return 0
     else:
         return 1
+
 
 def print_error(crate, match):
     # Print errors for packages which versions didn't match tag reference
     if not match:
         print(
-            '::error file={path}::version {version} does not match release tag {tag}'
-            .format(tag = ref, version = str(crate[0]), path = str(crate[1]))
+            "::error file={path}::version {version} does not match release tag {tag}".format(
+                tag=ref, version=str(crate[0]), path=str(crate[1])
+            )
         )
+
 
 def bundle_version(crates):
     # Reads package versions from workspace manifest file
     channel = Popen(
-        ['cargo', 'metadata', '--format-version=1', '--no-deps'],
-        stdout=PIPE
+        ["cargo", "metadata", "--format-version=1", "--no-deps"], stdout=PIPE
     )
 
     # parse json data
-    data = json.load(channel.stdout).get('packages')
+    data = json.load(channel.stdout).get("packages")
 
     # read, map and assign workspace crates versions to bundle package versions
     for package, _ in enumerate(data):
-        if data[package]['name'] in crates:
-            crates[data[package]['name']].append(data[package]['version'])
-            crates[data[package]['name']].append(data[package]['manifest_path'])
+        if data[package]["name"] in crates:
+            crates[data[package]["name"]].append(data[package]["version"])
+            crates[data[package]["name"]].append(data[package]["manifest_path"])
 
     # Checks package versions of the crates bundle for consistency with the given tag reference
     consistency = list(map(check_version, list(crates.values())))
@@ -50,48 +53,47 @@ def bundle_version(crates):
 
 event_name = sys.argv[1]
 
-date = date.today().strftime('%Y%m%d')
+date = date.today().strftime("%Y%m%d")
 
 ref = None
-if event_name == 'push':
-    ref = os.getenv('GITHUB_REF')
-    if ref.startswith('refs/tags/'):
-        release_type = 'tagged'
-    elif ref == 'refs/heads/ci/test/nightly':
+if event_name == "push":
+    ref = os.getenv("GITHUB_REF")
+    if ref.startswith("refs/tags/"):
+        release_type = "tagged"
+    elif ref == "refs/heads/ci/test/nightly":
         # emulate the nightly workflow
-        release_type = 'nightly'
+        release_type = "nightly"
         ref = None
     else:
-        raise ValueError('unexpected ref ' + ref)
-elif event_name == 'schedule':
-    release_type = 'nightly'
+        raise ValueError("unexpected ref " + ref)
+elif event_name == "schedule":
+    release_type = "nightly"
 else:
-    raise ValueError('unexpected event name ' + event_name)
+    raise ValueError("unexpected event name " + event_name)
 
 
 # Cargo workspace crates/packages for versioning bundle
 crates = {
-    'jormungandr':[],
-    'jormungandr-lib':[],
-    'jcli':[],
-    'jormungandr-testing-utils':[],
-    'jormungandr-integration-tests':[],
-    'jormungandr-scenario-tests':[]
+    "jormungandr": [],
+    "jormungandr-lib": [],
+    "jcli": [],
+    "jormungandr-testing-utils": [],
+    "jormungandr-integration-tests": [],
+    "jormungandr-scenario-tests": [],
 }
 
 version = bundle_version(crates)
-release_flags = ''
-if release_type == 'tagged':
-    tag = 'v' + version
-elif release_type == 'nightly':
+release_flags = ""
+if release_type == "tagged":
+    tag = "v" + version
+elif release_type == "nightly":
     version = re.sub(
-        r'^(\d+\.\d+\.\d+)(-.*)?$',
-        r'\1-nightly.' + date,
+        r"^(\d+\.\d+\.\d+)(-.*)?$",
+        r"\1-nightly." + date,
         version,
     )
-    tag = 'nightly.' + date
-    release_flags = '--prerelease'
+    tag = "nightly." + date
+    release_flags = "--prerelease"
 
-for name in 'version', 'date', 'tag', 'release_type', 'release_flags':
-    print('::set-output name={0}::{1}'.format(name, globals()[name]))
-
+for name in "version", "date", "tag", "release_type", "release_flags":
+    print("::set-output name={0}::{1}".format(name, globals()[name]))
