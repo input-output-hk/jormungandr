3c58130d9d68f718525b3f85882e909377931ecd
danielsanchezq <daniel.sanchez@iohk.io>
2021-04-20 11:22:50 +0200

    Implement simplified transaction steps: * new * add account * add output * finalize

--- jcli/src/jcli_lib/transaction/add_account.rs
+++ jcli/src/jcli_lib/transaction/add_account.rs
@@ -1,4 +1,5 @@
 use crate::jcli_lib::transaction::{common, Error};
+use crate::transaction::staging::Staging;
 use chain_addr::{Address, Kind};
 use chain_impl_mockchain::transaction::UnspecifiedAccountIdentifier;
 use jormungandr_lib::interfaces;
@@ -22,22 +23,27 @@ pub struct AddAccount {
 impl AddAccount {
     pub fn exec(self) -> Result<(), Error> {
         let mut transaction = self.common.load()?;
-
-        let account_id = match Address::from(self.account).kind() {
-            Kind::Account(key) => {
-                UnspecifiedAccountIdentifier::from_single_account(key.clone().into())
-            }
-            Kind::Multisig(key) => UnspecifiedAccountIdentifier::from_multi_account((*key).into()),
-            Kind::Single(_) => return Err(Error::AccountAddressSingle),
-            Kind::Group(_, _) => return Err(Error::AccountAddressGroup),
-            Kind::Script(_) => return Err(Error::AccountAddressScript),
-        };
-
-        transaction.add_input(interfaces::TransactionInput {
-            input: interfaces::TransactionInputType::Account(account_id.into()),
-            value: self.value,
-        })?;
-
+        add_account(self.account, self.value, &mut transaction)?;
         self.common.store(&transaction)
     }
+}
+
+pub fn add_account(
+    account: interfaces::Address,
+    value: interfaces::Value,
+    transaction: &mut Staging,
+) -> Result<(), Error> {
+    let account_id = match Address::from(self.account).kind() {
+        Kind::Account(key) => UnspecifiedAccountIdentifier::from_single_account(key.clone().into()),
+        Kind::Multisig(key) => UnspecifiedAccountIdentifier::from_multi_account((*key).into()),
+        Kind::Single(_) => return Err(Error::AccountAddressSingle),
+        Kind::Group(_, _) => return Err(Error::AccountAddressGroup),
+        Kind::Script(_) => return Err(Error::AccountAddressScript),
+    };
+
+    transaction.add_input(interfaces::TransactionInput {
+        input: interfaces::TransactionInputType::Account(account_id.into()),
+        value,
+    })?;
+    Ok(())
 }
--- jcli/src/jcli_lib/transaction/simplified.rs
+++ jcli/src/jcli_lib/transaction/simplified.rs
@@ -1,17 +1,57 @@
-use structopt::StructOpt;
 use crate::transaction;
+use crate::transaction::common;
+use crate::transaction::staging::Staging;
+use chain_impl_mockchain::transaction::Output;
+use jormungandr_lib::interfaces;
+use structopt::StructOpt;
+
 #[derive(StructOpt)]
 #[structopt(rename_all = "kebab-case")]
 pub struct Send {
     input_address_sk: String,
-    faucet_address: String,
-    amount: u64,
-    receiver_address: Option<String>,
+
+    /// the account to debit the funds from
+    #[structopt(name = "ACCOUNT")]
+    pub faucet_address: interfaces::Address,
+
+    /// the UTxO address or account address to credit funds to
+    #[structopt(name = "ADDRESS")]
+    pub receiver_address: interfaces::Address,
+
+    /// the value
+    #[structopt(name = "VALUE")]
+    pub value: interfaces::Value,
+
+    #[structopt(flatten)]
+    pub common: common::CommonTransaction,
+
+    #[structopt(flatten)]
+    pub fee: common::CommonFees,
+
+    /// Set the change in the given address
+    pub change: Option<interfaces::Address>,
 }
 
 impl Send {
-    pub fn exec(&self) -> std::io::Result<()> {
-        transaction::new::New
+    pub fn exec(self) -> std::io::Result<()> {
+        let mut transaction = Staging::new();
+
+        // add account
+        transaction::add_account::add_account(
+            self.faucet_address,
+            self.value.clone(),
+            &mut transaction,
+        )?;
+
+        // add output
+        transaction.add_output(Output {
+            address: self.receiver_address.into(),
+            value: self.value.into(),
+        })?;
+
+        //finalize
+        transaction::finalize::finalize(self.fee, self.change, &mut transaction)?;
+
         Ok(())
     }
 }
