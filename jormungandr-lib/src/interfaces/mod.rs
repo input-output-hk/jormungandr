efb7bc214215073d35c292eec32842ebe5d5c1ff
danielsanchezq <daniel.sanchez@iohk.io>
2021-05-06 16:27:17 +0200

    Flatten error into single struct

--- jormungandr-lib/src/interfaces/mod.rs
+++ jormungandr-lib/src/interfaces/mod.rs
@@ -44,7 +44,7 @@ pub use self::fragment::FragmentDef;
 pub use self::fragment_log::{FragmentLog, FragmentOrigin, FragmentStatus};
 pub use self::fragment_log_persistent::{
     get_fragments_log_files_path, load_fragments_from_folder_path, read_entries_from_files_path,
-    Error, FileFragments, PersistentFragmentLog,
+    DeserializeError, FileFragments, PersistentFragmentLog,
 };
 pub use self::leadership_log::{
     EnclaveLeaderId, LeadershipLog, LeadershipLogId, LeadershipLogStatus,
