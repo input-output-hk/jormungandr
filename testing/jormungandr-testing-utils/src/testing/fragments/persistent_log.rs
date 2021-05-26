input-output-hk/catalyst-core

adjust tests after FileFragments introduction

src/jormungandr/testing/jormungandr-testing-utils/src/testing/fragments/persistent_log.rs
@@ -1,56 +1,32 @@
-use bincode::Options;
 use chain_core::property::Serialize;
 use chain_impl_mockchain::fragment::Fragment;
-use jormungandr_lib::interfaces::PersistentFragmentLog;
-use std::fs::File;
-use std::io::BufReader;
+use jormungandr_lib::interfaces::load_fragments_from_folder_path;
 use std::path::PathBuf;
 
 pub struct PersistentLogViewer {
-    persistent_fragment_logs: Vec<PersistentFragmentLog>,
+    dir: PathBuf,
 }
 
 impl PersistentLogViewer {
-    pub fn new(dir: PathBuf) -> Result<Self, std::io::Error> {
-        let mut persistent_fragment_logs = Vec::new();
-
-        for path in std::fs::read_dir(&dir)?.map(|res| res.unwrap().path()) {
-            let mut reader = BufReader::new(File::open(&path).unwrap());
-            loop {
-                let codec = bincode::DefaultOptions::new()
-                    .with_fixint_encoding()
-                    .allow_trailing_bytes();
-                let decoded: PersistentFragmentLog = match codec.deserialize_from(&mut reader) {
-                    Ok(data) => data,
-                    Err(_err) => {
-                        //TODO: add better handling of EOF
-                        break;
-                    }
-                };
-                persistent_fragment_logs.push(decoded);
-            }
-        }
-
-        Ok(Self {
-            persistent_fragment_logs,
-        })
+    pub fn new(dir: PathBuf) -> Self {
+        Self { dir }
     }
 
     pub fn get_all(&self) -> Vec<Fragment> {
-        self.persistent_fragment_logs
-            .iter()
-            .map(|x| x.fragment.clone())
+        load_fragments_from_folder_path(&self.dir)
+            .unwrap()
+            .map(|x| x.unwrap().fragment)
             .collect()
     }
 
     pub fn get_bin(&self) -> Vec<Vec<u8>> {
-        self.get_all()
-            .iter()
-            .map(|x| x.serialize_as_vec().unwrap())
+        load_fragments_from_folder_path(&self.dir)
+            .unwrap()
+            .map(|x| x.unwrap().fragment.serialize_as_vec().unwrap())
             .collect()
     }
 
     pub fn count(&self) -> usize {
-        self.persistent_fragment_logs.len()
+        self.get_all().iter().count()
     }
 }

input-output-hk/jormungandr

testing/jormungandr-testing-utils/src/testing/fragments/persistent_log.rs
@@ -1,56 +1,32 @@
-use bincode::Options;
 use chain_core::property::Serialize;
 use chain_impl_mockchain::fragment::Fragment;
-use jormungandr_lib::interfaces::PersistentFragmentLog;
-use std::fs::File;
-use std::io::BufReader;
+use jormungandr_lib::interfaces::load_persistent_fragments_logs_from_folder_path;
 use std::path::PathBuf;
 
 pub struct PersistentLogViewer {
-    persistent_fragment_logs: Vec<PersistentFragmentLog>,
+    dir: PathBuf,
 }
 
 impl PersistentLogViewer {
-    pub fn new(dir: PathBuf) -> Result<Self, std::io::Error> {
-        let mut persistent_fragment_logs = Vec::new();
-
-        for path in std::fs::read_dir(&dir)?.map(|res| res.unwrap().path()) {
-            let mut reader = BufReader::new(File::open(&path).unwrap());
-            loop {
-                let codec = bincode::DefaultOptions::new()
-                    .with_fixint_encoding()
-                    .allow_trailing_bytes();
-                let decoded: PersistentFragmentLog = match codec.deserialize_from(&mut reader) {
-                    Ok(data) => data,
-                    Err(_err) => {
-                        //TODO: add better handling of EOF
-                        break;
-                    }
-                };
-                persistent_fragment_logs.push(decoded);
-            }
-        }
-
-        Ok(Self {
-            persistent_fragment_logs,
-        })
+    pub fn new(dir: PathBuf) -> Self {
+        Self { dir }
     }
 
     pub fn get_all(&self) -> Vec<Fragment> {
-        self.persistent_fragment_logs
-            .iter()
-            .map(|x| x.fragment.clone())
+        load_persistent_fragments_logs_from_folder_path(&self.dir)
+            .unwrap()
+            .map(|x| x.unwrap().fragment)
             .collect()
     }
 
     pub fn get_bin(&self) -> Vec<Vec<u8>> {
-        self.get_all()
-            .iter()
-            .map(|x| x.serialize_as_vec().unwrap())
+        load_persistent_fragments_logs_from_folder_path(&self.dir)
+            .unwrap()
+            .map(|x| x.unwrap().fragment.serialize_as_vec().unwrap())
             .collect()
     }
 
     pub fn count(&self) -> usize {
-        self.persistent_fragment_logs.len()
+        self.get_all().iter().count()
     }
 }