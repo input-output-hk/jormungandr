7d56d1948693ce4d037a3dcbb447e1d4ce073d59
Michael Fellinger <michael.fellinger@iohk.io>
2021-06-28 11:37:43 +0200

    switch from rust.nix to naersk

--- flake.nix
+++ flake.nix
@@ -2,21 +2,21 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     utils.url = "github:kreisys/flake-utils";
-    rust-nix.url = "github:input-output-hk/rust.nix/work";
-    rust-nix.inputs.nixpkgs.follows = "nixpkgs";
+    naersk.url = "github:nmattia/naersk";
+    naersk.inputs.nixpkgs.follows = "nixpkgs";
   };
-  outputs = { self, nixpkgs, utils, rust-nix }:
+  outputs = { self, nixpkgs, utils, naersk }:
     let
       workspaceCargo = builtins.fromTOML (builtins.readFile ./Cargo.toml);
       inherit (workspaceCargo.workspace) members;
     in utils.lib.simpleFlake {
       inherit nixpkgs;
       systems = [ "x86_64-linux" "aarch64-linux" ];
-      preOverlays = [ rust-nix ];
+      preOverlays = [ naersk ];
       overlay = final: prev:
         let lib = prev.lib;
         in (lib.listToAttrs (lib.forEach members (member:
-          lib.nameValuePair member (final.rust-nix.buildPackage {
+          lib.nameValuePair member (final.naersk.buildPackage {
             inherit ((builtins.fromTOML
               (builtins.readFile (./. + "/${member}/Cargo.toml"))).package)
               name version;
