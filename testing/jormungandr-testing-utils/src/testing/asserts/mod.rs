f023d757eabaa288fec097cb494ccff92f083d05
dkijania <dariusz.kijania@iohk.io>
2021-07-27 13:40:05 +0200

    introduced VotePlanStatusAssert for vote plan verifications

---
+++ testing/jormungandr-testing-utils/src/testing/asserts/mod.rs
@@ -0,0 +1,88 @@
+use chain_addr::AddressReadable;
+use chain_impl_mockchain::key::Hash;
+use jormungandr_lib::interfaces::Initial;
+use jormungandr_lib::interfaces::InitialUTxO;
+use jormungandr_lib::interfaces::{Tally, VotePlanStatus};
+use std::str::FromStr;
+
+pub trait VotePlanStatusAssert {
+    fn assert_all_proposals_are_tallied(&self);
+    fn assert_all_proposals_in_voteplan_are_tallied(&self, vote_plan_status: &VotePlanStatus);
+    fn assert_proposal_tally(&self, vote_plan_id: String, index: u8, expected: Vec<u64>);
+}
+
+impl VotePlanStatusAssert for Vec<VotePlanStatus> {
+    fn assert_all_proposals_are_tallied(&self) {
+        for vote_plan_status in self.iter() {
+            self.assert_all_proposals_in_voteplan_are_tallied(&vote_plan_status);
+        }
+    }
+
+    fn assert_all_proposals_in_voteplan_are_tallied(&self, vote_plan_status: &VotePlanStatus) {
+        for proposal in vote_plan_status.proposals.iter() {
+            assert!(
+                proposal.tally.is_some(),
+                "Proposal is not tallied {:?}",
+                proposal
+            );
+        }
+    }
+
+    fn assert_proposal_tally(&self, vote_plan_id: String, index: u8, expected: Vec<u64>) {
+        let vote_plan_status = self
+            .iter()
+            .find(|c_vote_plan| c_vote_plan.id == Hash::from_str(&vote_plan_id).unwrap().into())
+            .unwrap();
+
+        let tally = vote_plan_status
+            .proposals
+            .iter()
+            .find(|x| x.index == index)
+            .unwrap()
+            .tally
+            .as_ref()
+            .unwrap();
+
+        match tally {
+            Tally::Public { result } => assert_eq!(expected, result.results()),
+            Tally::Private { state: _ } => unimplemented!(),
+        }
+    }
+}
+
+pub trait InitialsAssert {
+    fn assert_contains(&self, entry: InitialUTxO);
+    fn assert_not_contain(&self, entry: InitialUTxO);
+}
+
+impl InitialsAssert for Vec<Initial> {
+    fn assert_contains(&self, expected: InitialUTxO) {
+        let address_readable =
+            AddressReadable::from_address("ca", &expected.address.clone().into()).to_string();
+        for initial in self.iter() {
+            if let Initial::Fund(initial_utxos) = initial {
+                if let Some(entry) = initial_utxos.iter().find(|x| x.address == expected.address) {
+                    assert_eq!(
+                        entry.value, expected.value,
+                        "Address {} found but value is different",
+                        address_readable
+                    );
+                    return;
+                }
+            }
+        }
+        panic!("Address {} not found", address_readable);
+    }
+
+    fn assert_not_contain(&self, entry: InitialUTxO) {
+        let address_readable =
+            AddressReadable::from_address("ca", &entry.address.clone().into()).to_string();
+        for initial in self.iter() {
+            if let Initial::Fund(initial_utxos) = initial {
+                if initial_utxos.iter().any(|x| x.address == entry.address) {
+                    panic!("Address {} found, while it shouldn't", address_readable);
+                }
+            }
+        }
+    }
+}
