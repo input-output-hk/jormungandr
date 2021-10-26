471f47c8734608f085a3bab2a57148cab99ba2c1
Mr-Leshiy <leshiy12345678@gmail.com>
2021-10-26 14:52:08 +0300

    add ConfigParams deserialization from string

--- jcli/src/jcli_lib/vote/update_proposal.rs
+++ jcli/src/jcli_lib/vote/update_proposal.rs
@@ -3,7 +3,10 @@ use crate::{
     rest::{v0::message::post_fragment, RestArgs},
     utils::key_parser::read_secret_key,
 };
-use chain_core::property::Serialize;
+use chain_core::{
+    mempack::ReadError,
+    property::{Deserialize, Serialize},
+};
 use chain_impl_mockchain::{
     fragment::{ConfigParams, Fragment},
     key::{BftLeaderId, EitherEd25519SecretKey},
@@ -18,7 +21,7 @@ use structopt::StructOpt;
 #[structopt(rename_all = "kebab-case")]
 pub struct UpdateProposal {
     /// the config update
-    #[structopt(name = "CONFIG_UPDATE")]
+    #[structopt(name = "CONFIG_UPDATE", parse(try_from_str = parse_hex))]
     config: ConfigParams,
 
     /// the file path to the file to read the signing key from.
@@ -30,11 +33,18 @@ pub struct UpdateProposal {
     rest_args: RestArgs,
 }
 
+fn parse_hex(str: &str) -> Result<ConfigParams, ReadError> {
+    let bytes = hex::decode(str).map_err(|err| ReadError::InvalidData(err.to_string()))?;
+
+    ConfigParams::deserialize(bytes.as_slice())
+        .map_err(|err| ReadError::InvalidData(err.to_string()))
+}
+
 impl UpdateProposal {
     pub fn exec(self) -> Result<(), Error> {
         let secret_key = read_secret_key(self.secret)?;
 
-        let fragment = build_fragment(secret_key);
+        let fragment = build_fragment(secret_key, self.config);
 
         let fragment_id = post_fragment(self.rest_args, fragment)?;
         println!("Posted fragment id: {}", fragment_id);
@@ -43,10 +53,10 @@ impl UpdateProposal {
     }
 }
 
-fn build_fragment(secret_key: EitherEd25519SecretKey) -> Fragment {
+fn build_fragment(secret_key: EitherEd25519SecretKey, config: ConfigParams) -> Fragment {
     let proposer_id = secret_key.to_public();
 
-    let update_proposal = UpdateProposalLib::new(ConfigParams::new());
+    let update_proposal = UpdateProposalLib::new(config);
 
     let bytes = update_proposal.serialize_as_vec().unwrap();
 
