b17713f344161ee9836a3a1133189924e6e22d4e
Mr-Leshiy <leshiy12345678@gmail.com>
2021-11-03 13:39:46 +0200

    fix test

--- testing/jormungandr-integration-tests/src/jcli/update_proposal/e2e.rs
+++ testing/jormungandr-integration-tests/src/jcli/update_proposal/e2e.rs
@@ -18,23 +18,32 @@ fn dummy_change_config_test() {
     let temp_dir = TempDir::new().unwrap();
     let jcli: JCli = Default::default();
 
-    let private_key = jcli.key().generate_default();
-    println!("private key: {}", private_key);
+    let private_key1 = jcli.key().generate_default();
+    let private_key2 = jcli.key().generate_default();
+    assert_ne!(private_key1, private_key2);
 
-    let sk_file_path = temp_dir.join("leader.sk");
+    let sk_file_path1 = temp_dir.join("leader1.sk");
     {
-        let mut sk_file = std::fs::File::create(&sk_file_path).unwrap();
-        sk_file.write_all(private_key.as_bytes()).unwrap();
+        let mut sk_file1 = std::fs::File::create(&sk_file_path1).unwrap();
+        sk_file1.write_all(private_key1.as_bytes()).unwrap();
+    }
+    let sk_file_path2 = temp_dir.join("leader2.sk");
+    {
+        let mut sk_file2 = std::fs::File::create(&sk_file_path2).unwrap();
+        sk_file2.write_all(private_key2.as_bytes()).unwrap();
     }
 
-    let private_key =
-        <SecretKey<Ed25519Extended>>::try_from_bech32_str(private_key.as_str()).unwrap();
-    let public_key = private_key.to_public();
-    let leader_id = ConsensusLeaderId::from(public_key);
-    println!("leader id: {:?}", leader_id);
+    let private_key1 =
+        <SecretKey<Ed25519Extended>>::try_from_bech32_str(private_key1.as_str()).unwrap();
+    let public_key1 = private_key1.to_public();
+    let leader_id1 = ConsensusLeaderId::from(public_key1);
+    let private_key2 =
+        <SecretKey<Ed25519Extended>>::try_from_bech32_str(private_key2.as_str()).unwrap();
+    let public_key2 = private_key2.to_public();
+    let leader_id2 = ConsensusLeaderId::from(public_key2);
 
     let config = ConfigurationBuilder::new()
-        .with_consensus_leaders_ids(vec![leader_id])
+        .with_consensus_leaders_ids(vec![leader_id1, leader_id2])
         .build(&temp_dir);
 
     let new_block_context_max_size = 1000;
@@ -59,19 +68,19 @@ fn dummy_change_config_test() {
 
     let fragment = jcli
         .votes()
-        .update_proposal(change_param_path, sk_file_path.clone());
+        .update_proposal(change_param_path, sk_file_path1.clone());
     let check = jcli.fragment_sender(&jormungandr).send(fragment.as_str());
     check.assert_in_block();
     let proposal_id = check.fragment_id();
 
-    println!("proposal id: {}", proposal_id);
-
     let fragment = jcli
         .votes()
-        .update_vote(proposal_id.to_string(), sk_file_path);
-    jcli.fragment_sender(&jormungandr)
-        .send(fragment.as_str())
-        .assert_in_block();
+        .update_vote(proposal_id.to_string(), sk_file_path1);
+    jcli.fragment_sender(&jormungandr).send(fragment.as_str());
+    let fragment = jcli
+        .votes()
+        .update_vote(proposal_id.to_string(), sk_file_path2);
+    jcli.fragment_sender(&jormungandr).send(fragment.as_str());
 
     wait_for_epoch(current_epoch + 2, jormungandr.rest());
 
