82474c9c1d8d3600315db3f7c6f247f07a514631
Giacomo Pasini <giacomo.pasini@iohk.io>
2021-12-13 16:04:53 +0100

    further generalize function arguments

--- testing/loki/src/process.rs
+++ testing/loki/src/process.rs
@@ -148,7 +148,7 @@ impl Drop for AdversaryNode {
             controller.stop();
         }
 
-        panic::persist_dir_on_panic::<String>(self.temp_dir.take(), Vec::new())
+        panic::persist_dir_on_panic::<String, String>(self.temp_dir.take(), Vec::new())
     }
 }
 
