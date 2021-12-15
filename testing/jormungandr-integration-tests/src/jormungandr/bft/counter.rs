9657c1da56fa4940968b4f58944aa7e9fbc55899
dkijania <dariusz.kijania@iohk.io>
2021-12-15 10:24:26 +0100

    added parallel spending counter test cases

---
+++ testing/jormungandr-integration-tests/src/jormungandr/bft/counter.rs
@@ -0,0 +1,80 @@
+use assert_fs::TempDir;
+use chain_impl_mockchain::{chaintypes::ConsensusType, testing::WitnessMode};
+use jormungandr_lib::interfaces::InitialUTxO;
+use jormungandr_testing_utils::testing::jormungandr::Starter;
+use jormungandr_testing_utils::testing::{
+    jormungandr::ConfigurationBuilder, startup, FragmentSender, FragmentSenderSetup,
+};
+
+#[test]
+fn parallel_transaction_using_different_lanes() {
+    let temp_dir = TempDir::new().unwrap();
+    let receiver = startup::create_new_account_address();
+    let mut sender = startup::create_new_account_address();
+
+    let config = ConfigurationBuilder::new()
+        .with_slots_per_epoch(20)
+        .with_slot_duration(2)
+        .with_block0_consensus(ConsensusType::Bft)
+        .with_funds(vec![
+            InitialUTxO {
+                address: sender.address(),
+                value: 100_000.into(),
+            },
+            InitialUTxO {
+                address: receiver.address(),
+                value: 100_000.into(),
+            },
+        ])
+        .build(&temp_dir);
+
+    let jormungandr = Starter::new()
+        .config(config)
+        .temp_dir(temp_dir)
+        .start()
+        .unwrap();
+
+    let mut fragment_sender = FragmentSender::new(
+        jormungandr.genesis_block_hash(),
+        jormungandr.fees(),
+        jormungandr.default_block_date_generator(),
+        FragmentSenderSetup::no_verify(),
+    );
+
+    let mut checks = vec![];
+
+    fragment_sender = fragment_sender.witness_mode(WitnessMode::Account { lane: 1 });
+    checks.push(
+        fragment_sender
+            .send_transaction(&mut sender, &receiver, &jormungandr, 1.into())
+            .unwrap(),
+    );
+
+    fragment_sender = fragment_sender.witness_mode(WitnessMode::Account { lane: 2 });
+    checks.push(
+        fragment_sender
+            .send_transaction(&mut sender, &receiver, &jormungandr, 1.into())
+            .unwrap(),
+    );
+
+    fragment_sender = fragment_sender.witness_mode(WitnessMode::Account { lane: 3 });
+    checks.push(
+        fragment_sender
+            .send_transaction(&mut sender, &receiver, &jormungandr, 1.into())
+            .unwrap(),
+    );
+
+    fragment_sender = fragment_sender.witness_mode(WitnessMode::Account { lane: 4 });
+    checks.push(
+        fragment_sender
+            .send_transaction(&mut sender, &receiver, &jormungandr, 1.into())
+            .unwrap(),
+    );
+
+    jormungandr_testing_utils::testing::FragmentVerifier::wait_and_verify_all_are_in_block(
+        std::time::Duration::from_secs(10),
+        checks,
+        &jormungandr,
+    )
+    .unwrap();
+}
--- testing/jormungandr-integration-tests/src/networking/communication/mod.rs
+++ testing/jormungandr-integration-tests/src/networking/communication/mod.rs
@@ -1,2 +1,3 @@
 pub mod leader_leader;
+pub mod parallel_spending_counter;
 pub mod passive_leader;
