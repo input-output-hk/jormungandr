d910eaa9263fde85e1ae65576aefe089c56df257
dkijania <dariusz.kijania@iohk.io>
2021-12-16 21:38:26 +0100

    printout to stdout

--- testing/hersir/src/controller/mod.rs
+++ testing/hersir/src/controller/mod.rs
@@ -261,14 +261,13 @@ impl Controller {
             peer.id = None;
         }
 
-        let log_file_path = dir.child("node.log");
         config.log = Some(Log(LogEntry {
             format: "json".into(),
             level: spawn_params
                 .get_log_level()
                 .unwrap_or(&LogLevel::DEBUG)
                 .to_string(),
-            output: LogOutput::File(log_file_path.path().to_path_buf()),
+            output: LogOutput::Stdout,
         }));
 
         if let PersistenceMode::Persistent = spawn_params.get_persistence_mode() {
