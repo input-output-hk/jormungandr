b7f9e62418ffc361c9dbc76c7c1a9e3e085a34de
Mr-Leshiy <leshiy12345678@gmail.com>
2021-12-29 15:09:32 +0200

    Update with the latest chain-libs

--- testing/jormungandr-testing-utils/src/wallet/account.rs
+++ testing/jormungandr-testing-utils/src/wallet/account.rs
@@ -92,6 +92,10 @@ impl Wallet {
         ))
     }
 
+    pub fn internal_counters_increasing(&self) -> &SpendingCounterIncreasing {
+        &self.internal_counters
+    }
+
     /// Use the default counter
     pub fn internal_counter(&self) -> SpendingCounter {
         self.internal_counters.get_valid_counter()
