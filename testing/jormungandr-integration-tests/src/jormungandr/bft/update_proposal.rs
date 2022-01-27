146dcb20755add28b588eff4e550d33fb4b6ee3b
dkijania <dariusz.kijania@iohk.io>
2022-01-27 10:18:51 +0100

    adjust test proposal_changes_immutable_setting - alter assertion, so it expect fragment to be rejected immediately

--- testing/jormungandr-integration-tests/src/jormungandr/bft/update_proposal.rs
+++ testing/jormungandr-integration-tests/src/jormungandr/bft/update_proposal.rs
@@ -9,7 +9,7 @@ use jormungandr_automation::testing::time::{get_current_date, wait_for_epoch};
 use jormungandr_lib::interfaces::{
     BlockContentMaxSize, ConfigParam, ConfigParams, ConsensusLeaderId,
 };
-use thor::FragmentSender;
+use thor::{FragmentSender, FragmentSenderSetup, FragmentVerifier};
 
 #[test]
 fn proposal_expired_after_proposal_expiration_deadline() {
@@ -143,11 +143,10 @@ fn proposal_changes_immutable_setting() {
         Discrimination::Production,
     )]);
 
-    let old_settings = jormungandr.rest().settings().unwrap();
-
-    let current_epoch = get_current_date(&mut jormungandr.rest()).epoch();
-
-    let fragment_sender = FragmentSender::from(jormungandr.block0_configuration());
+    let fragment_sender = FragmentSender::from_with_setup(
+        jormungandr.block0_configuration(),
+        FragmentSenderSetup::no_verify(),
+    );
 
     let update_proposal = UpdateProposal::new(
         change_params.into(),
@@ -161,22 +160,11 @@ fn proposal_changes_immutable_setting() {
             &jormungandr,
         )
         .unwrap();
-    let update_vote = UpdateVote::new(
-        *check.fragment_id(),
-        bft_secret.identifier().into_public_key().into(),
-    );
-    fragment_sender
-        .send_update_vote(
-            &mut alice,
-            &bft_secret.signing_key().into_secret_key(),
-            update_vote,
-            &jormungandr,
-        )
-        .unwrap();
 
-    wait_for_epoch(current_epoch + 2, jormungandr.rest());
-
-    let new_settings = jormungandr.rest().settings().unwrap();
-
-    assert_eq!(old_settings, new_settings);
+    FragmentVerifier::wait_and_verify_is_rejected(
+        std::time::Duration::from_secs(3),
+        check,
+        &jormungandr,
+    )
+    .unwrap();
 }
