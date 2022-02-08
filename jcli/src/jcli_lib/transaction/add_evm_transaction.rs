d34b50b344a65e7a51bee7d6faec7480b1ab1655
Joaquín Rosales <casabenita7@gmail.com>
2022-02-08 09:53:09 -0600

    Add tests evm transactions jcli (#3822)

--- jcli/src/jcli_lib/transaction/add_evm_transaction.rs
+++ jcli/src/jcli_lib/transaction/add_evm_transaction.rs
@@ -2,7 +2,7 @@ use crate::jcli_lib::transaction::{common, Error};
 use jormungandr_lib::interfaces::EvmTransaction;
 use structopt::StructOpt;
 
-#[derive(StructOpt)]
+#[derive(StructOpt, Debug)]
 #[structopt(rename_all = "kebab-case")]
 pub struct AddEvmTransaction {
     #[structopt(flatten)]
@@ -17,5 +17,43 @@ impl AddEvmTransaction {
         let mut transaction = self.common.load()?;
         transaction.set_evm_transaction(self.evm_transaction)?;
         self.common.store(&transaction)
+    }
+}
+
+#[cfg(all(test, feature = "evm"))]
+mod test {
+    use super::*;
+    use chain_impl_mockchain::evm;
+    use quickcheck::Arbitrary;
+
+    impl Arbitrary for AddEvmTransaction {
+        fn arbitrary<G: quickcheck::Gen>(g: &mut G) -> Self {
+            Self {
+                common: common::CommonTransaction { staging_file: None },
+                evm_transaction: EvmTransaction(evm::EvmTransaction::arbitrary(g)),
+            }
+        }
+    }
+
+    impl Clone for AddEvmTransaction {
+        fn clone(&self) -> Self {
+            Self {
+                common: common::CommonTransaction { staging_file: None },
+                evm_transaction: self.evm_transaction.clone(),
+            }
+        }
+    }
+
+    impl PartialEq for AddEvmTransaction {
+        fn eq(&self, other: &AddEvmTransaction) -> bool {
+            self.evm_transaction == other.evm_transaction
+        }
+    }
+
+    quickcheck! {
+        fn evm_transaction_encode(add_evm_tx: AddEvmTransaction) -> bool {
+            let hex_tx = format!("{}", add_evm_tx.evm_transaction);
+            AddEvmTransaction::from_iter(&["", &hex_tx]) == add_evm_tx
+        }
     }
 }
--- jcli/src/lib.rs
+++ jcli/src/lib.rs
@@ -1,2 +1,6 @@
+#[cfg(all(test, feature = "evm"))]
+#[macro_use]
+extern crate quickcheck;
+
 pub mod jcli_lib;
 pub use crate::jcli_lib::*;
