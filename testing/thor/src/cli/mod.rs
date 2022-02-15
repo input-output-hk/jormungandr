e1fcba89794c9b2b27611d3ace3ca81fb9d587a4
dkijania <dariusz.kijania@iohk.io>
2022-02-15 08:33:23 +0100

    expose WalletState struct

--- testing/thor/src/cli/mod.rs
+++ testing/thor/src/cli/mod.rs
@@ -3,7 +3,7 @@ mod controller;
 mod error;
 mod wallet_controller;
 
-pub use config::{Alias, Config, ConfigManager, Connection, Error as ConfigError};
+pub use config::{Alias, Config, ConfigManager, Connection, Error as ConfigError, WalletState};
 pub use controller::CliController;
 pub use error::Error;
 pub use wallet_controller::WalletController;
