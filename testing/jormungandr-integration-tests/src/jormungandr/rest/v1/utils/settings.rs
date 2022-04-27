fff71f2f3a71e32aa4c65093e51a187f7ad33b53
kukkok3 <93382903+kukkok3@users.noreply.github.com>
2022-04-27 17:54:06 +0200

    did some cleaning up

--- testing/jormungandr-integration-tests/src/jormungandr/rest/v1/utils/settings.rs
+++ testing/jormungandr-integration-tests/src/jormungandr/rest/v1/utils/settings.rs
@@ -1,26 +1,14 @@
+use std::num::NonZeroU64;
+
 use crate::startup;
-use chain_impl_mockchain::rewards::{self, CompoundingType, Parameters, Limit};
+use chain_impl_mockchain::chaintypes::ConsensusVersion;
+use chain_impl_mockchain::fee::{LinearFee, PerCertificateFee, PerVoteCertificateFee};
+use chain_impl_mockchain::rewards::{CompoundingType, Parameters, Limit};
 use jormungandr_automation::jormungandr::ConfigurationBuilder;
-use jormungandr_automation::jormungandr::JormungandrProcess;
 use jormungandr_lib::interfaces::Block0Configuration;
 use jormungandr_lib::interfaces::{SettingsDto, RewardParams, BlockchainConfiguration};
 use jormungandr_lib::time::SystemTime;
 use rstest::*;
-
-#[fixture]
-fn world() -> JormungandrProcess {
-    let alice = thor::Wallet::default();
-    let bob = thor::Wallet::default();
-    
-    let (jormungandr, _stake_pools) = startup::start_stake_pool(
-        &[alice.clone()],
-        &[bob.clone()],
-        &mut ConfigurationBuilder::new(),
-    )
-    .unwrap();
-
-    jormungandr
-}
 
 fn get_parameters_from_blockchain_config(blockchain_configuration: &BlockchainConfiguration) -> Option<Parameters> {   
     let reward_param = 
@@ -32,9 +20,9 @@ fn get_parameters_from_blockchain_config(blockchain_configuration: &BlockchainCo
     let reward_drawing = 
          match blockchain_configuration.reward_constraints.reward_drawing_limit_max{
             None => Limit::None,
-            Some(r) => Limit::ByStakeAbsolute(rewards::Ratio::from(r))
+            Some(r) => Limit::ByStakeAbsolute(r.into())
         };
-
+    //TODO use map?
     let pool_participation =
         match blockchain_configuration.reward_constraints.pool_participation_capping 
          {
@@ -50,7 +38,7 @@ fn get_parameters_from_blockchain_config(blockchain_configuration: &BlockchainCo
             epoch_rate,
         } => Some(Parameters {
             initial_value: constant,
-            compounding_ratio: rewards::Ratio::from(ratio),
+            compounding_ratio: ratio.into(),
             compounding_type: CompoundingType::Linear,
             epoch_rate,
             epoch_start,
@@ -64,7 +52,7 @@ fn get_parameters_from_blockchain_config(blockchain_configuration: &BlockchainCo
             epoch_rate,
         } => Some(Parameters {
             initial_value: constant,
-            compounding_ratio: rewards::Ratio::from(ratio),
+            compounding_ratio: ratio.into(),
             compounding_type: CompoundingType::Halvening,
             epoch_rate,
             epoch_start,
@@ -78,17 +66,17 @@ fn get_settings_from_block0_configuration(block0_configuration: &Block0Configura
     let blockchain_configuration = &block0_configuration.blockchain_configuration;
     SettingsDto {
         block0_hash: block0_configuration.to_block().header().id().to_string(),
-        block0_time: SystemTime::from(blockchain_configuration.block0_date),
+        block0_time: blockchain_configuration.block0_date.into(),
         //TODO use map?
         curr_slot_start_time: Some(SystemTime::from(blockchain_configuration.block0_date)),
         consensus_version: blockchain_configuration.block0_consensus.to_string(),
         fees: blockchain_configuration.linear_fees,
-        block_content_max_size: u32::from(blockchain_configuration.block_content_max_size),
-        epoch_stability_depth: u32::from(blockchain_configuration.epoch_stability_depth),
-        slot_duration: u8::from(blockchain_configuration.slot_duration) as u64,
-        slots_per_epoch: u32::from(blockchain_configuration.slots_per_epoch),
+        block_content_max_size: blockchain_configuration.block_content_max_size.into(),
+        epoch_stability_depth: blockchain_configuration.epoch_stability_depth.into(),
+        slot_duration: u8::from(blockchain_configuration.slot_duration).into(),
+        slots_per_epoch: blockchain_configuration.slots_per_epoch.into(),
         //TODO handle the unwarp
-        treasury_tax: rewards::TaxType::from(blockchain_configuration.treasury_parameters.unwrap()),
+        treasury_tax: blockchain_configuration.treasury_parameters.unwrap().into(),
         reward_params: get_parameters_from_blockchain_config(blockchain_configuration).unwrap(),
         discrimination: blockchain_configuration.discrimination,
         tx_max_expiry_epochs: blockchain_configuration.tx_max_expiry_epochs.unwrap(),
@@ -97,11 +85,22 @@ fn get_settings_from_block0_configuration(block0_configuration: &Block0Configura
 }
 
 #[rstest]
-pub fn test_valid_settings (world: JormungandrProcess) {
-    let jormungandr = world;
-    let rest_settings = jormungandr.rest().settings().expect("Rest settings error");
-    let block0_configuration = jormungandr.block0_configuration();
-    let block0_settings = get_settings_from_block0_configuration(block0_configuration);
-    assert_eq!(rest_settings,block0_settings);
+pub fn test_default_settings () {
+    let alice = thor::Wallet::default();
+    let bob = thor::Wallet::default();   
+    let (jormungandr, _stake_pools) = startup::start_stake_pool(
+        &[alice.clone()],
+        &[bob.clone()],
+        &mut ConfigurationBuilder::new(),
+    ).expect("Startup stake pool error");
 
+    let rest_settings = jormungandr.rest().settings().expect("Rest settings error");
+    let block0_settings = get_settings_from_block0_configuration(jormungandr.block0_configuration());
+    println!("REST {:#?}",rest_settings);
+    println!("===================");
+    println!("BLOCK0 {:#?}",block0_settings);
+    assert_eq!(rest_settings,block0_settings);
 }
+
+
+
