3c4ff5c35ad95bd88feeb1c30ae6fb970d2bc2e5
Mr-Leshiy <leshiy12345678@gmail.com>
2022-04-28 15:38:25 +0300

    add another RPC endpoints

--- jormungandr/src/rpc/eth_block_info/logic.rs
+++ jormungandr/src/rpc/eth_block_info/logic.rs
@@ -1,8 +1,54 @@
 use super::Error;
 use crate::{context::Context, rpc::eth_types::block::Block};
-use chain_evm::ethereum_types::H256;
+use chain_evm::ethereum_types::{H256, U256};
 
-pub fn get_block_by_hash(hash: H256, full: bool, context: &Context) -> Result<Option<Block>, Error> {
+pub fn get_block_by_hash(
+    hash: H256,
+    full: bool,
+    context: &Context,
+) -> Result<Option<Block>, Error> {
     let block = context.blockchain()?.storage().get(hash.0.into())?;
     Ok(block.map(|block| Block::build(block, full)))
+}
+
+pub fn get_block_by_number(
+    _number: u64,
+    _full: bool,
+    _context: &Context,
+) -> Result<Option<Block>, Error> {
+    // TODO implement
+    Ok(None)
+}
+
+pub fn get_transaction_count_by_hash(hash: H256, context: &Context) -> Result<Option<U256>, Error> {
+    let block = context.blockchain()?.storage().get(hash.0.into())?;
+
+    let count = block.map_or(0, |block| {
+        let mut count = 0;
+        block.fragments().for_each(|_| count += 1);
+        count
+    });
+
+    Ok(Some(count.into()))
+}
+
+pub fn get_transaction_count_by_number(
+    _number: u64,
+    _context: &Context,
+) -> Result<Option<U256>, Error> {
+    // TODO implement
+    Ok(None)
+}
+
+pub fn get_uncle_count_by_hash(_: H256, _: &Context) -> Result<Option<U256>, Error> {
+    Ok(Some(0.into()))
+}
+
+pub fn get_uncle_count_by_number(_: u64, _: &Context) -> Result<Option<U256>, Error> {
+    Ok(Some(0.into()))
+}
+
+pub fn get_block_number(_: &Context) -> Result<U256, Error> {
+    // TODO implement
+    Ok(0.into())
 }
--- jormungandr/src/rpc/eth_block_info/mod.rs
+++ jormungandr/src/rpc/eth_block_info/mod.rs
@@ -24,4 +24,70 @@ pub fn eth_get_blocks_info_module(context: ContextLock) -> RpcModule<ContextLock
         .unwrap();
 
     module
+        .register_async_method("eth_getBlockByNumber", |params, context| async move {
+            let context = context.read().await;
+            let (block_number, full) = params.parse()?;
+            logic::get_block_by_number(block_number, full, &context)
+                .map_err(|err| jsonrpsee_core::Error::Custom(err.to_string()))
+        })
+        .unwrap();
+
+    module
+        .register_async_method(
+            "eth_getBlockTransactionCountByHash",
+            |params, context| async move {
+                let context = context.read().await;
+                let block_hash = params.parse()?;
+                logic::get_transaction_count_by_hash(block_hash, &context)
+                    .map_err(|err| jsonrpsee_core::Error::Custom(err.to_string()))
+            },
+        )
+        .unwrap();
+
+    module
+        .register_async_method(
+            "eth_getBlockTransactionCountByNumber",
+            |params, context| async move {
+                let context = context.read().await;
+                let block_number = params.parse()?;
+                logic::get_transaction_count_by_number(block_number, &context)
+                    .map_err(|err| jsonrpsee_core::Error::Custom(err.to_string()))
+            },
+        )
+        .unwrap();
+
+    module
+        .register_async_method(
+            "eth_getUncleCountByBlockHash",
+            |params, context| async move {
+                let context = context.read().await;
+                let block_hash = params.parse()?;
+                logic::get_uncle_count_by_hash(block_hash, &context)
+                    .map_err(|err| jsonrpsee_core::Error::Custom(err.to_string()))
+            },
+        )
+        .unwrap();
+
+    module
+        .register_async_method(
+            "eth_getUncleCountByBlockNumber",
+            |params, context| async move {
+                let context = context.read().await;
+                let block_number = params.parse()?;
+                logic::get_uncle_count_by_number(block_number, &context)
+                    .map_err(|err| jsonrpsee_core::Error::Custom(err.to_string()))
+            },
+        )
+        .unwrap();
+
+    module
+        .register_async_method("eth_blockNumber", |_, context| async move {
+            let context = context.read().await;
+
+            logic::get_block_number(&context)
+                .map_err(|err| jsonrpsee_core::Error::Custom(err.to_string()))
+        })
+        .unwrap();
+
+    module
 }
