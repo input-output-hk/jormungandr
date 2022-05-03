4ff6beb8111302d6c189d7be5966197e9c1e406d
Alex Pozhylenkov <leshiy12345678@gmail.com>
2022-05-03 15:07:51 +0300

    Add get chain info RPC endpoints (#3973)
    * add new rpc endpoints
    
    * remove uneeded fields
    
    * fix suggestion

---
+++ jormungandr/src/jrpc/eth_types/sync.rs
@@ -0,0 +1,67 @@
+use chain_evm::ethereum_types::U256;
+use serde::{Serialize, Serializer};
+
+/// Sync info
+#[derive(Default, Debug, Serialize, PartialEq, Eq)]
+#[serde(rename_all = "camelCase")]
+pub struct SyncInfo {
+    /// Starting block
+    pub starting_block: U256,
+    /// Current block
+    pub current_block: U256,
+    /// Highest block seen so far
+    pub highest_block: U256,
+}
+
+/// Sync status
+#[derive(Debug, PartialEq, Eq)]
+pub enum SyncStatus {
+    /// Info when syncing
+    Info(SyncInfo),
+    /// Not syncing
+    #[allow(dead_code)]
+    None,
+}
+
+impl SyncStatus {
+    pub fn build() -> Self {
+        Self::Info(SyncInfo {
+            starting_block: U256::zero(),
+            current_block: U256::zero(),
+            highest_block: U256::zero(),
+        })
+    }
+}
+
+impl Serialize for SyncStatus {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        match *self {
+            SyncStatus::Info(ref info) => info.serialize(serializer),
+            SyncStatus::None => false.serialize(serializer),
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn sync_status_json_deserialize() {
+        let ss_none = SyncStatus::None;
+        let ss_info = SyncStatus::Info(SyncInfo {
+            starting_block: U256::zero(),
+            current_block: U256::zero(),
+            highest_block: U256::zero(),
+        });
+
+        assert_eq!(serde_json::to_string(&ss_none).unwrap(), "false");
+        assert_eq!(
+            serde_json::to_string(&ss_info).unwrap(),
+            "{\"startingBlock\":\"0x0\",\"currentBlock\":\"0x0\",\"highestBlock\":\"0x0\"}"
+        );
+    }
+}
--- jormungandr/src/jrpc/mod.rs
+++ jormungandr/src/jrpc/mod.rs
@@ -1,6 +1,8 @@
 #[cfg(feature = "evm")]
 mod eth_block_info;
 #[cfg(feature = "evm")]
+mod eth_chain_info;
+#[cfg(feature = "evm")]
 mod eth_types;
 
 use crate::context::ContextLock;
@@ -21,9 +23,15 @@ pub async fn start_jrpc_server(config: Config, _context: ContextLock) {
     let mut modules = RpcModule::new(());
 
     #[cfg(feature = "evm")]
-    modules
-        .merge(eth_block_info::eth_get_blocks_info_module(_context))
-        .unwrap();
+    {
+        modules
+            .merge(eth_block_info::eth_get_blocks_info_module(_context.clone()))
+            .unwrap();
+
+        modules
+            .merge(eth_chain_info::eth_get_blocks_info_module(_context))
+            .unwrap();
+    }
 
     server.start(modules).unwrap().await
 }
