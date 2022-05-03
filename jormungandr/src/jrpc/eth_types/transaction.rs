be465d82c65fb067686fa26ea3ac982890d49a1b
Mr-Leshiy <leshiy12345678@gmail.com>
2022-05-03 16:47:40 +0300

    update transaction type

--- jormungandr/src/jrpc/eth_types/transaction.rs
+++ jormungandr/src/jrpc/eth_types/transaction.rs
@@ -17,8 +17,8 @@ pub struct Transaction {
     pub gas: U256,
     /// Data
     pub input: Bytes,
-    #[serde(rename = "gasPrice", skip_serializing_if = "Option::is_none")]
-    pub gas_price: Option<U256>,
+    /// Gas price
+    pub gas_price: U256,
     /// The network id of the transaction, if any.
     pub chain_id: Option<U64>,
     /// The standardised V field of the signature.
@@ -41,12 +41,41 @@ impl Transaction {
             value: U256::one(),
             gas: U256::one(),
             input: Default::default(),
-            gas_price: Some(U256::one()),
+            gas_price: U256::one(),
             chain_id: Some(U64::one()),
             v: U256::one(),
             r: U256::one(),
             s: U256::one(),
             transaction_type: U256::one(),
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn transaction_json_serde() {
+        let transaction = Transaction {
+            nonce: U256::zero(),
+            from: H160::zero(),
+            to: Some(H160::zero()),
+            value: U256::zero(),
+            gas: U256::zero(),
+            input: Default::default(),
+            gas_price: U256::zero(),
+            chain_id: Some(U64::zero()),
+            v: U256::zero(),
+            r: U256::zero(),
+            s: U256::zero(),
+            transaction_type: U256::zero(),
+        };
+        assert_eq!(
+            serde_json::to_string(&transaction).unwrap(),
+            "{\"nonce\":\"0x0\",\"from\":\"0x0000000000000000000000000000000000000000\",\"to\":\"0x0000000000000000000000000000000000000000\",\"value\":\"0x0\",\"gas\":\"0x0\",\"input\":\"0x\",\"gasPrice\":\"0x0\",\"chainId\":\"0x0\",\"v\":\"0x0\",\"r\":\"0x0\",\"s\":\"0x0\",\"type\":\"0x0\"}"
+        );
+        let decoded: Transaction = serde_json::from_str("{\"nonce\":\"0x0\",\"from\":\"0x0000000000000000000000000000000000000000\",\"to\":\"0x0000000000000000000000000000000000000000\",\"value\":\"0x0\",\"gas\":\"0x0\",\"input\":\"0x\",\"gasPrice\":\"0x0\",\"chainId\":\"0x0\",\"v\":\"0x0\",\"r\":\"0x0\",\"s\":\"0x0\",\"type\":\"0x0\"}").unwrap();
+        assert_eq!(decoded, transaction);
     }
 }
