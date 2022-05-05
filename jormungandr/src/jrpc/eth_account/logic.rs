14af2cbb4793c3d4a099a9c0545adb5cbe27e4f1
Mr-Leshiy <leshiy12345678@gmail.com>
2022-05-05 12:02:40 +0300

    update arguments

--- jormungandr/src/jrpc/eth_account/logic.rs
+++ jormungandr/src/jrpc/eth_account/logic.rs
@@ -1,18 +1,44 @@
+use chain_evm::ethereum_types::{H160, H256, U256};
+
 use super::Error;
-use crate::context::Context;
+use crate::{
+    context::Context,
+    jrpc::eth_types::{block::Bytes, block_number::BlockNumber},
+};
 
-pub fn get_transaction_count(_context: &Context) -> Result<(), Error> {
-    Ok(())
+pub fn get_transaction_count(
+    _address: H160,
+    _block_number: BlockNumber,
+    _context: &Context,
+) -> Result<U256, Error> {
+    // TODO implement
+    Ok(0.into())
 }
 
-pub fn get_balance(_context: &Context) -> Result<(), Error> {
-    Ok(())
+pub fn get_balance(
+    _address: H160,
+    _block_number: BlockNumber,
+    _context: &Context,
+) -> Result<U256, Error> {
+    // TODO implement
+    Ok(0.into())
 }
 
-pub fn get_code(_context: &Context) -> Result<(), Error> {
-    Ok(())
+pub fn get_code(
+    _address: H160,
+    _block_number: BlockNumber,
+    _context: &Context,
+) -> Result<Bytes, Error> {
+    // TODO implement
+    Ok(Default::default())
 }
 
-pub fn get_storage_at(_context: &Context) -> Result<(), Error> {
-    Ok(())
+pub fn get_storage_at(
+    _address: H160,
+    _key: H256,
+    _block_number: BlockNumber,
+    _context: &Context,
+) -> Result<H256, Error> {
+    // TODO implement
+    Ok(H256::zero())
 }
--- jormungandr/src/jrpc/eth_account/mod.rs
+++ jormungandr/src/jrpc/eth_account/mod.rs
@@ -11,36 +11,37 @@ pub fn eth_account_module(context: ContextLock) -> RpcModule<ContextLock> {
     let mut module = RpcModule::new(context);
 
     module
-        .register_async_method("eth_getTransactionCount", |_params, context| async move {
+        .register_async_method("eth_getTransactionCount", |params, context| async move {
             let context = context.read().await;
-            // let (block_hash, full) = params.parse()?;
-            logic::get_transaction_count(&context)
+            let (address, block_number) = params.parse()?;
+            logic::get_transaction_count(address, block_number, &context)
                 .map_err(|err| jsonrpsee_core::Error::Custom(err.to_string()))
         })
         .unwrap();
 
     module
-        .register_async_method("eth_getBalance", |_params, context| async move {
+        .register_async_method("eth_getBalance", |params, context| async move {
             let context = context.read().await;
-            // let (block_hash, full) = params.parse()?;
-            logic::get_balance(&context)
+            let (address, block_number) = params.parse()?;
+            logic::get_balance(address, block_number, &context)
                 .map_err(|err| jsonrpsee_core::Error::Custom(err.to_string()))
         })
         .unwrap();
 
     module
-        .register_async_method("eth_getCode", |_params, context| async move {
+        .register_async_method("eth_getCode", |params, context| async move {
             let context = context.read().await;
-            // let (block_hash, full) = params.parse()?;
-            logic::get_code(&context).map_err(|err| jsonrpsee_core::Error::Custom(err.to_string()))
+            let (address, block_number) = params.parse()?;
+            logic::get_code(address, block_number, &context)
+                .map_err(|err| jsonrpsee_core::Error::Custom(err.to_string()))
         })
         .unwrap();
 
     module
-        .register_async_method("eth_getStorageAt", |_params, context| async move {
+        .register_async_method("eth_getStorageAt", |params, context| async move {
             let context = context.read().await;
-            // let (block_hash, full) = params.parse()?;
-            logic::get_storage_at(&context)
+            let (address, key, block_number) = params.parse()?;
+            logic::get_storage_at(address, key, block_number, &context)
                 .map_err(|err| jsonrpsee_core::Error::Custom(err.to_string()))
         })
         .unwrap();
