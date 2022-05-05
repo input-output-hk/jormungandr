a793bbca4c0ecf8f2afac4fee8f7cbfec6b63b90
Mr-Leshiy <leshiy12345678@gmail.com>
2022-05-05 13:38:09 +0300

    add rpc endpoints

--- jormungandr/src/jrpc/eth_filter/logic.rs
+++ jormungandr/src/jrpc/eth_filter/logic.rs
@@ -1,1 +1,31 @@
+use super::Error;
+use crate::context::Context;
+use chain_evm::ethereum_types::U256;
 
+pub fn new_filter(_context: &Context) -> Result<U256, Error> {
+    // TODO implement
+    Ok(0.into())
+}
+
+pub fn new_block_filter(_context: &Context) -> Result<U256, Error> {
+    // TODO implement
+    Ok(0.into())
+}
+
+pub fn new_pending_transaction_filter(_context: &Context) -> Result<U256, Error> {
+    // TODO implement
+    Ok(0.into())
+}
+
+pub fn uninstall_filter(_context: &Context) -> Result<bool, Error> {
+    // TODO implement
+    Ok(true)
+}
+
+pub fn get_filter_changes(_context: &Context) -> Result<(), Error> {
+    Ok(())
+}
+
+pub fn get_filter_logs(_context: &Context) -> Result<(), Error> {
+    Ok(())
+}
--- jormungandr/src/jrpc/eth_filter/mod.rs
+++ jormungandr/src/jrpc/eth_filter/mod.rs
@@ -7,7 +7,54 @@ mod logic;
 pub enum Error {}
 
 pub fn eth_filter_module(context: ContextLock) -> RpcModule<ContextLock> {
-    let module = RpcModule::new(context);
+    let mut module = RpcModule::new(context);
 
+    module
+        .register_async_method("eth_newFilter", |_, context| async move {
+            let context = context.read().await;
+            logic::new_filter(&context)
+                .map_err(|err| jsonrpsee_core::Error::Custom(err.to_string()))
+        })
+        .unwrap();
+
+    module
+        .register_async_method("eth_newBlockFilter", |_, context| async move {
+            let context = context.read().await;
+            logic::new_block_filter(&context)
+                .map_err(|err| jsonrpsee_core::Error::Custom(err.to_string()))
+        })
+        .unwrap();
+
+    module
+        .register_async_method("eth_newPendingTransactionFilter", |_, context| async move {
+            let context = context.read().await;
+            logic::new_pending_transaction_filter(&context)
+                .map_err(|err| jsonrpsee_core::Error::Custom(err.to_string()))
+        })
+        .unwrap();
+
+    module
+        .register_async_method("eth_uninstallFilter", |_, context| async move {
+            let context = context.read().await;
+            logic::uninstall_filter(&context)
+                .map_err(|err| jsonrpsee_core::Error::Custom(err.to_string()))
+        })
+        .unwrap();
+
+    module
+        .register_async_method("eth_getFilterChanges", |_, context| async move {
+            let context = context.read().await;
+            logic::get_filter_changes(&context)
+                .map_err(|err| jsonrpsee_core::Error::Custom(err.to_string()))
+        })
+        .unwrap();
+
+    module
+        .register_async_method("eth_getFilterLogs", |_, context| async move {
+            let context = context.read().await;
+            logic::get_filter_logs(&context)
+                .map_err(|err| jsonrpsee_core::Error::Custom(err.to_string()))
+        })
+        .unwrap();
     module
 }
