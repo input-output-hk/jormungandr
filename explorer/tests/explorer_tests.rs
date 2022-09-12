f43ce6fc13bc222707a055b6ea6c18294169ed7e
cong-or <conchubhar.gannon@gmail.com>
2022-09-12 09:55:10 +0100

    reformat test file

--- explorer/tests/explorer_tests.rs
+++ explorer/tests/explorer_tests.rs
@@ -11,26 +11,6 @@ mod tests {
     use jormungandr_lib::crypto::hash::Hash;
     use jormungandr_lib::interfaces::ActiveSlotCoefficient;
 
-    #[test]
-    pub fn explorer_tests() {
-        let faucet = thor::Wallet::default();
-
-        let query_complexity_limit = 70;
-
-        let mut config = ConfigurationBuilder::new();
-        config.with_consensus_genesis_praos_active_slot_coeff(ActiveSlotCoefficient::MAXIMUM);
-
-        let (jormungandr, _initial_stake_pools) =
-            startup::start_stake_pool(&[faucet.clone()], &[], &mut config).unwrap();
-
-        let params = ExplorerParams::new(query_complexity_limit, None, None);
-        let explorer_process = jormungandr.explorer(params);
-        let explorer = explorer_process.client();
-
-        get_invalid_block(explorer);
-        get_valid_block(explorer, jormungandr.genesis_block_hash());
-    }
-
     pub fn get_invalid_block(explorer: &Explorer) {
         let hash = Hash::from_str(
             &"000000000019d6689c085ae165831e934ff763ae46a2a6c172b3f1b60a8ce26f".to_string(),
@@ -52,5 +32,25 @@ mod tests {
         if let Some(data) = explorer.block(genesis_block).unwrap().data {
             assert_eq!(data.block.id, genesis_block.to_string());
         }
+    }
+
+    #[test]
+    pub fn explorer_tests() {
+        let faucet = thor::Wallet::default();
+
+        let query_complexity_limit = 70;
+
+        let mut config = ConfigurationBuilder::new();
+        config.with_consensus_genesis_praos_active_slot_coeff(ActiveSlotCoefficient::MAXIMUM);
+
+        let (jormungandr, _initial_stake_pools) =
+            startup::start_stake_pool(&[faucet.clone()], &[], &mut config).unwrap();
+
+        let params = ExplorerParams::new(query_complexity_limit, None, None);
+        let explorer_process = jormungandr.explorer(params);
+        let explorer = explorer_process.client();
+
+        get_invalid_block(explorer);
+        get_valid_block(explorer, jormungandr.genesis_block_hash());
     }
 }
