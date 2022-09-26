112db5dc06edfbf1d278d3d719166ed2ee698eca
kukkok3 <93382903+kukkok3@users.noreply.github.com>
2022-09-26 12:04:14 +0200

    add block by chain length test

--- testing/jormungandr-integration-tests/src/jormungandr/explorer/block.rs
+++ testing/jormungandr-integration-tests/src/jormungandr/explorer/block.rs
@@ -1,10 +1,6 @@
 use crate::startup;
-use chain_core::{
-    packer::Codec,
-    property::{Deserialize, FromStr},
-};
+use chain_core::property::FromStr;
 use chain_impl_mockchain::{
-    block::Block,
     chaintypes::ConsensusType,
     fee::LinearFee,
     tokens::{identifier::TokenIdentifier, minting_policy::MintingPolicy},
@@ -401,4 +397,112 @@ pub fn explorer_all_blocks_test() {
         let decoded_block = ExplorerVerifier::decode_block(encoded_block);
         ExplorerVerifier::assert_all_blocks(decoded_block, &explorer_block.node).unwrap();
     }
+}
+
+#[test]
+pub fn explorer_block_by_chain_lenght_test() {
+    let jcli: JCli = Default::default();
+    let sender = thor::Wallet::default();
+    let receiver = thor::Wallet::default();
+
+    let (jormungandr, _) = startup::start_stake_pool(
+        &[sender.clone()],
+        &[receiver.clone()],
+        ConfigurationBuilder::new()
+            .with_block0_consensus(ConsensusType::GenesisPraos)
+            .with_slots_per_epoch(20)
+            .with_block_content_max_size(100000.into())
+            .with_consensus_genesis_praos_active_slot_coeff(ActiveSlotCoefficient::MAXIMUM)
+            .with_slot_duration(3)
+            .with_linear_fees(LinearFee::new(1, 1, 1))
+            .with_mempool(Mempool {
+                pool_max_entries: 1_000_000usize.into(),
+                log_max_entries: 1_000_000usize.into(),
+                persistent_log: None,
+            })
+            .with_token(InitialToken {
+                // FIXME: this works because I know it's the VotePlanBuilder's default, but
+                // probably should me more explicit.
+                token_id: TokenIdentifier::from_str(
+                    "00000000000000000000000000000000000000000000000000000000.00000000",
+                )
+                .unwrap()
+                .into(),
+                policy: MintingPolicy::new().into(),
+                to: vec![sender.to_initial_token(1_000_000)],
+            }),
+    )
+    .unwrap();
+
+    let fragment_sender = FragmentSender::from_with_setup(
+        jormungandr.block0_configuration(),
+        FragmentSenderSetup::no_verify(),
+    );
+
+    let time_era = jormungandr.time_era();
+
+    let mut fragment_generator = FragmentGenerator::new(
+        sender,
+        receiver,
+        jormungandr.to_remote(),
+        time_era.slots_per_epoch(),
+        2,
+        2,
+        2,
+        fragment_sender,
+    );
+
+    fragment_generator.prepare(BlockDate::new(1, 0));
+
+    time::wait_for_epoch(2, jormungandr.rest());
+
+    let mem_checks: Vec<MemPoolCheck> = fragment_generator.send_all().unwrap();
+    FragmentVerifier::wait_and_verify_all_are_in_block(
+        Duration::from_secs(2),
+        mem_checks.clone(),
+        &jormungandr,
+    )
+    .unwrap();
+
+    let fragments_log = jcli.rest().v0().message().logs(jormungandr.rest_uri());
+    let fragment_log = fragments_log
+        .iter()
+        .find(|x| {
+            *x.fragment_id().to_string() == mem_checks.last().unwrap().fragment_id().to_string()
+        })
+        .unwrap();
+
+    let fragment_block_id =
+        if let &FragmentStatus::InABlock { date: _, block } = fragment_log.status() {
+            block
+        } else {
+            panic!("Fragment not in block")
+        };
+
+    let encoded_block = jcli
+        .rest()
+        .v0()
+        .block()
+        .get(fragment_block_id.to_string(), jormungandr.rest_uri());
+
+    let decoded_block = ExplorerVerifier::decode_block(encoded_block);
+
+    let params = ExplorerParams::new(BLOCK_QUERY_COMPLEXITY_LIMIT, BLOCK_QUERY_DEPTH_LIMIT, None);
+    let explorer_process = jormungandr.explorer(params);
+    let explorer = explorer_process.client();
+
+    let explorer_block_response = explorer
+        .blocks_at_chain_length(decoded_block.header().chain_length().into())
+        .unwrap();
+
+    assert!(
+        explorer_block_response.errors.is_none(),
+        "{:?}",
+        explorer_block_response.errors.unwrap()
+    );
+
+    let explorer_blocks_data = explorer_block_response.data.unwrap();
+    let explorer_block = explorer_blocks_data.blocks_by_chain_length.first().unwrap();
+
+    ExplorerVerifier::assert_block_by_chain_length(decoded_block, explorer_block).unwrap();
 }
